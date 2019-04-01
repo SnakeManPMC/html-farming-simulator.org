59d4bd4248c0ff3f9727a2453aad0fd5388037ab
Snake Man <snakeman@phnet.fi>
2019-04-01 20:02:52 +0300

    Us ned heightmap header added

--- 19/map-making-tutorial.php
+++ 19/map-making-tutorial.php
@@ -1219,6 +1219,47 @@ Idea: why not screenshot GE top-down view with field rectangles visible, then ju
 </p>
 
 
+	<h2>US NED Heightmap</h2>
+
+<p>
+Howto use US National Elevation Data (NED) heightmap as map_dem.png so giants engine understands how it should look.
+</p>
+
+<p>
+Global mapper -> L3DT -> grayscale PNG doesnt work, elevations are really high / rough, you need to darken the image, a lot. This is because GE recognizes 0 meters to 255 meters as RGB black to white color range only. You can do this with photoshop or gimp but we use <a href="http://www.graphicsmagick.org/" target="_blank">graphicsmagick</a> as its automatic one click method.
+</p>
+
+<p>
+Graphicsmagick values explained:<br>
+70 way too rough<br>
+35 looks nice in-game but is just <i>slightly</i> too rough when comparing to google/maps street view photos<br>
+20 still can see some roughness, but looks already very nice<br>
+15 smooth but still elevations, this looks realistic
+</p>
+
+<p>
+_run_graphicsmagick_heightmap_just_right.bat:
+</p>
+<pre>
+@echo off
+md temp
+set MAGICK_TMPDIR=%CD%\temp
+
+copy C:\FS19_Projects\USA_Texas_Rowena_8km\map_dem.png .
+gm convert -modulate 15,100 map_dem.png PMC_Texas_Rowena_8km_dem.png
+
+rd temp
+del map_dem.png
+echo All done :)
+pause
+exit
+</pre>
+
+<p>
+You need to replace the C:\FS19_Projects\USA_Texas_Rowena_8km\ path to where you placed your grayscale heightmap DEM image, if its in the current dir then remove that line completely. Also edit the PMC_Texas_Rowena_8km_dem.png to whatever file name you use. Once you have darkened the heightmap png just load the terrain up in GE and inspect how it looks, it might require in-game check where you get better sense of the elevations when you drive tractor or just walk around.
+</p>
+
+
 <!--
 	<h2></h2>
 
