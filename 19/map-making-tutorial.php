555e22554a6796eb0d0325293cd9b7dbd33edf20
Snake Man <snakeman@phnet.fi>
2019-04-01 19:48:30 +0300

    Gimp paint fields header added

--- 19/map-making-tutorial.php
+++ 19/map-making-tutorial.php
@@ -1196,6 +1196,29 @@ All done ;)
 </p>
 
 
+	<h2>Gimp Paint Fields</h2>
+
+<p>
+Paint fields in GIMP using satellite texture as base. Paint cultivated or plowed fields. GE is extremely bad software to use so painting fields using it is a torture, it is far better to paint them in gimp.
+</p>
+
+<p>
+This image assumes you have high resolution satellite image for real world data terrain available, use 8192 x 8192 resolution for 8km and 4096 x 4096 resolution for 4km terrains.
+</p>
+
+<p>
+- open satellite image in gimp, create fields layer and paint fields as some bright color like red<br>
+- resize to cultivator_density resolution (usually smaller), background RGB 0,0,0 and red color to RGB 1,0,0 export to cultivator_density.png<br>
+- replace RGB 0,0,0 with RGB 130,0,0 and RGB 1,0,0 with RGB 0,0,0 export to fruit_density.png<br>
+- edit terrainname.i3d GDM to PNG on these two density images<br>
+- GE load terrainname.i3d and verify that fields are painted without grass in them, save<br>
+</p>
+
+<p>
+Idea: why not screenshot GE top-down view with field rectangles visible, then just gimp resize it to like 4096 res, color replace the blue with RGB 1,0,0 to create cultivated fields almost automatically? Hmm :)
+</p>
+
+
 <!--
 	<h2></h2>
 
