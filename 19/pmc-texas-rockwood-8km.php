0122fc19603f80e4128319f4e88281ae127032bd
Snake Man <snakeman@phnet.fi>
2019-06-02 07:21:30 +0300

    Field sizes added

--- 19/pmc-texas-rockwood-8km.php
+++ 19/pmc-texas-rockwood-8km.php
@@ -32,11 +32,144 @@ USA Texas Rockwood 8km <a href="https://www.google.com/maps/@31.5217821,-99.3755
 </p>
 
 <p>
-This terrain was the second we did right after learning the basics with north dakota 4km. Not all of the terrain is covered with fields as many of them do look kind of not field looking, we will have to research it more and figure out later.
+This terrain was the second we did right after learning the basics with North Dakota Greendale 4km. Not all of the terrain is covered with fields as many of them do look kind of not field looking, we will have to research it more and figure out later.
 </p>
 
+<p>
+Smallest field: 0.270 ha, largest field 65.777 ha.
+</p>
 <pre>
-Field size info to be added upon terrain re-creation.
+Field 1 (field01) : 42.024 ha
+Field 2 (field02) : 40.252 ha
+Field 3 (field03) : 1.505 ha
+Field 4 (field04) : 11.820 ha
+Field 5 (field05) : 6.056 ha
+Field 6 (field06) : 3.169 ha
+Field 7 (field07) : 9.721 ha
+Field 8 (field08) : 15.325 ha
+Field 9 (field09) : 8.875 ha
+Field 10 (field10) : 1.256 ha
+Field 11 (field11) : 5.219 ha
+Field 12 (field12) : 17.050 ha
+Field 13 (field13) : 42.508 ha
+Field 14 (field14) : 3.547 ha
+Field 15 (field15) : 21.141 ha
+Field 16 (field16) : 6.408 ha
+Field 17 (field17) : 34.075 ha
+Field 18 (field18) : 15.605 ha
+Field 19 (field19) : 17.056 ha
+Field 20 (field20) : 12.298 ha
+Field 21 (field21) : 26.089 ha
+Field 22 (field22) : 2.254 ha
+Field 23 (field23) : 4.855 ha
+Field 24 (field24) : 5.069 ha
+Field 25 (field25) : 5.367 ha
+Field 26 (field26) : 9.978 ha
+Field 27 (field27) : 34.866 ha
+Field 28 (field28) : 32.525 ha
+Field 29 (field29) : 24.533 ha
+Field 30 (field30) : 2.167 ha
+Field 31 (field31) : 5.895 ha
+Field 32 (field32) : 3.403 ha
+Field 33 (field33) : 23.338 ha
+Field 34 (field34) : 2.406 ha
+Field 35 (field35) : 1.388 ha
+Field 36 (field36) : 2.136 ha
+Field 37 (field37) : 5.036 ha
+Field 38 (field38) : 0.270 ha
+Field 39 (field39) : 18.562 ha
+Field 40 (field40) : 10.024 ha
+Field 41 (field41) : 10.530 ha
+Field 42 (field42) : 3.879 ha
+Field 43 (field43) : 1.971 ha
+Field 44 (field44) : 4.775 ha
+Field 45 (field45) : 3.144 ha
+Field 46 (field46) : 11.485 ha
+Field 47 (field47) : 36.564 ha
+Field 48 (field48) : 46.176 ha
+Field 49 (field49) : 4.180 ha
+Field 50 (field50) : 6.631 ha
+Field 51 (field51) : 0.794 ha
+Field 52 (field52) : 1.145 ha
+Field 53 (field53) : 16.163 ha
+Field 54 (field54) : 25.549 ha
+Field 55 (field55) : 40.140 ha
+Field 56 (field56) : 65.777 ha
+Field 57 (field57) : 23.499 ha
+Field 58 (field58) : 0.742 ha
+Field 59 (field59) : 14.078 ha
+Field 60 (field60) : 11.322 ha
+Field 61 (field61) : 9.746 ha
+Field 62 (field62) : 5.583 ha
+Field 63 (field63) : 5.491 ha
+Field 64 (field64) : 18.226 ha
+Field 65 (field65) : 26.395 ha
+Field 66 (field66) : 11.422 ha
+Field 67 (field67) : 3.713 ha
+Field 68 (field68) : 9.317 ha
+Field 69 (field69) : 1.455 ha
+Field 70 (field70) : 0.475 ha
+Field 71 (field71) : 2.083 ha
+Field 72 (field72) : 1.531 ha
+Field 73 (field73) : 6.094 ha
+Field 74 (field74) : 17.227 ha
+Field 75 (field75) : 2.408 ha
+Field 76 (field76) : 16.329 ha
+Field 77 (field77) : 3.430 ha
+Field 78 (field78) : 25.012 ha
+Field 79 (field79) : 3.962 ha
+Field 80 (field80) : 14.325 ha
+Field 81 (field81) : 1.600 ha
+Field 82 (field82) : 1.513 ha
+Field 83 (field83) : 4.259 ha
+Field 84 (field84) : 0.696 ha
+Field 85 (field85) : 1.625 ha
+Field 86 (field86) : 6.118 ha
+Field 87 (field87) : 3.959 ha
+Field 88 (field88) : 3.039 ha
+Field 89 (field89) : 15.716 ha
+Field 90 (field90) : 3.519 ha
+Field 91 (field91) : 5.892 ha
+Field 92 (field92) : 2.766 ha
+Field 93 (field93) : 4.703 ha
+Field 94 (field94) : 2.273 ha
+Field 95 (field95) : 23.152 ha
+Field 96 (field96) : 1.157 ha
+Field 97 (field97) : 4.459 ha
+Field 98 (field98) : 19.807 ha
+Field 99 (field99) : 6.551 ha
+Field 100 (field100) : 19.466 ha
+Field 101 (field101) : 9.841 ha
+Field 102 (field102) : 61.769 ha
+Field 103 (field103) : 17.525 ha
+Field 104 (field104) : 1.598 ha
+Field 105 (field105) : 6.415 ha
+Field 106 (field106) : 1.753 ha
+Field 107 (field107) : 10.026 ha
+Field 108 (field108) : 29.627 ha
+Field 109 (field109) : 6.581 ha
+Field 110 (field110) : 17.409 ha
+Field 111 (field111) : 8.276 ha
+Field 112 (field112) : 1.128 ha
+Field 113 (field113) : 2.541 ha
+Field 114 (field114) : 1.053 ha
+Field 115 (field115) : 1.497 ha
+Field 116 (field116) : 4.656 ha
+Field 117 (field117) : 46.433 ha
+Field 118 (field118) : 9.748 ha
+Field 119 (field119) : 57.700 ha
+Field 120 (field120) : 49.575 ha
+Field 121 (field121) : 38.277 ha
+Field 122 (field122) : 3.565 ha
+Field 123 (field123) : 29.297 ha
+Field 124 (field124) : 11.480 ha
+Field 125 (field125) : 13.370 ha
+Field 126 (field126) : 22.002 ha
+Field 127 (field127) : 6.459 ha
+Field 128 (field128) : 6.150 ha
+Field 129 (field129) : 3.885 ha
+Field 130 (field130) : 2.723 ha
+Field 131 (field131) : 40.337 ha
 </pre>
 
 </section>
