c4f3263952d9e39a09a550350e2c53f4548eb253
Snake Man <snakeman@phnet.fi>
2019-06-07 05:42:41 +0300

    North dakota sonora fields and farmland added

--- 19/pmc-north-dakota-sonora-8km.php
+++ 19/pmc-north-dakota-sonora-8km.php
@@ -25,18 +25,173 @@
 
 	<h2>PMC North Dakota Sonora 8km</h2>
 
-<a href="images/USA-North-Dakota-Sonora-Satellite-PDA.png" target="_blank"><img src="images/thumbs/USA-North-Dakota-Sonora-Satellite-PDA.jpg" alt="Farming Simulator 19 Terrain - Sonora, North Dakota, USA" style="float:right;width:256px;height:256px;"></a>
+<div>
+<p style="font-size: 10px;">
+Click for larger image
+</p>
+<a href="images/USA-North-Dakota-Sonora-Satellite-PDA.png" target="_blank"><img src="images/thumbs/USA-North-Dakota-Sonora-Satellite-PDA.jpg" alt="Farming Simulator 19 Terrain - Sonora, North Dakota, USA" style="padding: 3px 15px;width:256px;height:256px;"></a>
+
+<a href="images/USA-North-Dakota-Sonora-8km-Fields.jpg" target="_blank"><img src="images/thumbs/USA-North-Dakota-Sonora-8km-Fields.jpg" alt="Farming Simulator 19 Terrain - Sonora, North Dakota, USA. Fields" style="padding: 3px 15px;width:256px;height:256px;"></a>
+
+<a href="images/USA-North-Dakota-Sonora-8km-Farmland.jpg" target="_blank"><img src="images/thumbs/USA-North-Dakota-Sonora-8km-Farmland.jpg" alt="Farming Simulator 19 Terrain - Sonora, North Dakota, USA. Farmland" style="padding: 3px 15px;width:256px;height:256px;"></a>
+</div>
 
 <p>
 PMC North Dakota Sonora 8km <a href="https://www.google.com/maps/@46.0955368,-96.6994824,8192m/data=!3m1!1e3" target="_blank">google/maps</a>
 </p>
 
 <p>
-Nice big square fields.
+Nice big square fields for that pure farming fun.
 </p>
 
+<p>
+Fields smallest: 0.144 ha, largest 119.989 ha.
+</p>
 <pre>
-Field size info to be added upon terrain re-creation.
+Field 1 (field) : 58.642 ha
+Field 2 (field) : 15.827 ha
+Field 3 (field) : 35.401 ha
+Field 4 (field) : 22.171 ha
+Field 5 (field) : 21.166 ha
+Field 6 (field) : 59.956 ha
+Field 7 (field) : 59.876 ha
+Field 8 (field) : 20.202 ha
+Field 9 (field) : 4.103 ha
+Field 10 (field) : 28.088 ha
+Field 11 (field) : 28.058 ha
+Field 12 (field) : 27.055 ha
+Field 13 (field) : 9.206 ha
+Field 14 (field) : 3.784 ha
+Field 15 (field) : 89.396 ha
+Field 16 (field) : 1.286 ha
+Field 17 (field) : 19.984 ha
+Field 18 (field) : 30.257 ha
+Field 19 (field) : 17.777 ha
+Field 20 (field) : 38.092 ha
+Field 21 (field) : 51.035 ha
+Field 22 (field) : 34.980 ha
+Field 23 (field) : 59.131 ha
+Field 24 (field) : 30.574 ha
+Field 25 (field) : 34.533 ha
+Field 26 (field) : 6.151 ha
+Field 27 (field) : 22.853 ha
+Field 28 (field) : 22.123 ha
+Field 29 (field) : 21.294 ha
+Field 30 (field) : 21.552 ha
+Field 31 (field) : 32.269 ha
+Field 32 (field) : 57.826 ha
+Field 33 (field) : 42.548 ha
+Field 34 (field) : 20.435 ha
+Field 35 (field) : 18.883 ha
+Field 36 (field) : 47.425 ha
+Field 37 (field) : 59.798 ha
+Field 38 (field) : 38.616 ha
+Field 39 (field) : 17.309 ha
+Field 40 (field) : 74.091 ha
+Field 41 (field) : 20.669 ha
+Field 42 (field) : 14.189 ha
+Field 43 (field) : 44.916 ha
+Field 44 (field) : 3.332 ha
+Field 45 (field) : 3.744 ha
+Field 46 (field) : 63.455 ha
+Field 47 (field) : 60.218 ha
+Field 48 (field) : 1.687 ha
+Field 49 (field) : 59.137 ha
+Field 50 (field) : 43.819 ha
+Field 51 (field) : 42.086 ha
+Field 52 (field) : 58.258 ha
+Field 53 (field) : 42.130 ha
+Field 54 (field) : 44.762 ha
+Field 55 (field) : 57.215 ha
+Field 56 (field) : 57.970 ha
+Field 57 (field) : 32.418 ha
+Field 58 (field) : 1.083 ha
+Field 59 (field) : 55.433 ha
+Field 60 (field) : 43.349 ha
+Field 61 (field) : 43.198 ha
+Field 62 (field) : 55.537 ha
+Field 63 (field) : 59.700 ha
+Field 64 (field) : 29.209 ha
+Field 65 (field) : 59.926 ha
+Field 66 (field) : 59.817 ha
+Field 67 (field) : 59.447 ha
+Field 68 (field) : 56.810 ha
+Field 69 (field) : 52.872 ha
+Field 70 (field) : 57.974 ha
+Field 71 (field) : 43.847 ha
+Field 72 (field) : 10.959 ha
+Field 73 (field) : 0.587 ha
+Field 74 (field) : 33.699 ha
+Field 75 (field) : 59.255 ha
+Field 76 (field) : 26.434 ha
+Field 77 (field) : 19.686 ha
+Field 78 (field) : 28.297 ha
+Field 79 (field) : 29.045 ha
+Field 80 (field) : 55.895 ha
+Field 81 (field) : 26.575 ha
+Field 82 (field) : 27.866 ha
+Field 83 (field) : 60.552 ha
+Field 84 (field) : 55.193 ha
+Field 85 (field) : 55.750 ha
+Field 86 (field) : 59.329 ha
+Field 87 (field) : 41.847 ha
+Field 88 (field) : 26.605 ha
+Field 89 (field) : 20.484 ha
+Field 90 (field) : 41.568 ha
+Field 91 (field) : 35.983 ha
+Field 92 (field) : 20.806 ha
+Field 93 (field) : 28.177 ha
+Field 94 (field) : 28.040 ha
+Field 95 (field) : 37.812 ha
+Field 96 (field) : 36.486 ha
+Field 97 (field) : 17.421 ha
+Field 98 (field) : 16.629 ha
+Field 99 (field) : 35.048 ha
+Field 100 (field) : 36.844 ha
+Field 101 (field) : 17.785 ha
+Field 102 (field) : 17.785 ha
+Field 103 (field) : 35.890 ha
+Field 104 (field) : 119.989 ha
+Field 105 (field) : 52.005 ha
+Field 106 (field) : 49.222 ha
+Field 107 (field) : 54.353 ha
+Field 108 (field) : 57.770 ha
+Field 109 (field) : 14.186 ha
+Field 110 (field) : 48.913 ha
+Field 111 (field) : 14.089 ha
+Field 112 (field) : 56.861 ha
+Field 113 (field) : 55.619 ha
+Field 114 (field) : 54.149 ha
+Field 115 (field) : 50.551 ha
+Field 116 (field) : 55.030 ha
+Field 117 (field) : 56.083 ha
+Field 118 (field) : 13.216 ha
+Field 119 (field) : 12.514 ha
+Field 120 (field) : 55.651 ha
+Field 121 (field) : 55.613 ha
+Field 122 (field) : 57.075 ha
+Field 123 (field) : 39.406 ha
+Field 124 (field) : 28.030 ha
+Field 125 (field) : 25.094 ha
+Field 126 (field) : 33.128 ha
+Field 127 (field) : 45.985 ha
+Field 128 (field) : 37.129 ha
+Field 129 (field) : 39.620 ha
+Field 130 (field) : 44.582 ha
+Field 131 (field) : 51.453 ha
+Field 132 (field) : 28.044 ha
+Field 133 (field) : 0.719 ha
+Field 134 (field) : 0.144 ha
+Field 135 (field) : 23.873 ha
+Field 136 (field) : 26.207 ha
+Field 137 (field) : 24.112 ha
+Field 138 (field) : 24.118 ha
+Field 139 (field) : 22.115 ha
+Field 140 (field) : 14.264 ha
+Field 141 (field) : 3.241 ha
+Field 142 (field) : 19.240 ha
+Field 143 (field) : 33.492 ha
+Field 144 (field) : 15.026 ha
 </pre>
 
 </section>
