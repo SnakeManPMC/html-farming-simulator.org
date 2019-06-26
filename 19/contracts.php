fc0fb064c0aab232c5ba701d4535d1c22b730684
Snake Man <snakeman@phnet.fi>
2019-06-27 02:45:35 +0300

    Contracts large terrain info added

--- 19/contracts.php
+++ 19/contracts.php
@@ -31,27 +31,95 @@ In the easy mode (left most career start option) the contracts don't pay much, y
 </p>
 
 <p>
-There are some contracts which pay few hundreds and you got to be thinking that the balance is way off on these prices. Sure if you compare it to real life, a farmer would not pay you thousands to drive a tractor for a couple of hours but then again the money balance is off in farm sim games in general so you can't compare it to real life.
+There are some contracts which pay few hundreds and you got to be thinking that the balance is way off on these prices. Sure if you compare it to real life, a farmer would not pay you thousands to drive a tractor for a couple of hours but then again the money balance is off in farm sim games in general so you can't compare it to real life. Note: contract rewards scale up with the task, small task small reward, big task ... well big reward. This is especially true with large terrains and huge 50 or even 100+ hectare sized fields.
 </p>
 
 <p>
-Contracts have no time limit, take as long as you want. World around you will not freeze anymore, so if you have fields with crops planted they do grow while you are doing contracts. You can use own vehicles / equipment or lease, if you lease the vehicles will de-spawn once the contract is completed.
+Contracts have no time limit, take as long as you want. World around you will not freeze anymore, so if you have fields with crops planted they do grow while you are doing contracts. You can use own vehicles / equipment or lease, if you lease the vehicles will de-spawn once the contract is completed. You can stop doing a contract and leave the field, contract will not abort until you complete or cancel it.
 </p>
 
 <p>
-In multiplayer you can work on contracts with other players. I've heard you can also contract jobs out for other players in the server (haven't tried this myself).
+In multiplayer you can work on contracts with other players. You cannot contract out tasks for other players in MP, you have to do it virtually like asking your friend to work your field and then you just send him money when its done.
 </p>
 
 <p>
-They do feel much better now than missions in FS17 mostly because no time limit and the world will not freeze around you.
+Contracts do feel much better now than missions in FS17 mostly because no time limit, the world will not freeze around you and you can leave the field like stop doing the contract for a while and then continue.
 </p>
 
 <p>
-It is best to own your vehicles or lease them by yourself and not from the contracts menu because when your vehicle gets maintenance wear down you cannot repair the contracts-leased vehicle, it just doesn't show up on your garage leased menu. This was on v1.2 game engine, perhaps giants will fix this in future upgrades who knows.
+It is best to own your vehicles or lease them by yourself, not to borrow from contracts menu because when your vehicle gets maintenance wear down you cannot repair the contracts borrowed vehicle, it just doesn't show up on your garage leased menu. This was on v1.2 game engine, perhaps giants will fix this in future upgrades who knows.
 </p>
 
 <p>
 Original game design is to allow only one active contract, however there is <a href="mods.php" target="_blank">a "multiple missions" mod which allows you to take many contracts</a> at the same time.
+</p>
+
+	<h2>8km Terrain Contract Experiences</h2>
+
+<p>
+<b>2019-04-17</b> These are thoughts while playing PMC Iowa Garden City 8km start from zero. See background info for large terrains from <a href="pmc-terrains.php">PMC Terrains</a> page.
+</p>
+
+<p>
+Harvest contracts take forever and potatoes & sugar beet stuff is no brainer (meaning, dont do then as it takes forever). Harvesting 25ha field with Case IH 9240 with 3162 TerraFlex Draper 45ft header takes HOURS.
+</p>
+
+<p>
+Check distances. If you need to drive your vehicle to the other side of 8km terrain, its no fun. If you choose to take harvest contact with first the field being in the other side of the terrain and sale point to the opposite... imagine how much driving you'll be doing. This is obviously no issue if you love trucking :)
+</p>
+
+<p>
+Harvest contract you get the 100% complete notification and option to collect the money and therefore end the mission and automatically return the leased vehicles. Do not collect until you have sold on your own, to your own pocket, each and every liter of the fields crop you where harvesting.
+</p>
+
+<p>
+Cheat from neighbors field. I was doing harvest contract on F105, I accidentally clipped F107 which is right next to it and to my surprise the combine did harvest that small snippet. You can harvest PARTS of the other field which is on the same land lot as your assigned field. Just harvest your assigned field and then the next one as far as you reach and take the crops to the best selling sale point, or the one you have been assigned to. You'll get the money from the sale. This is somewhat cheat but its within the giants field definition guidelines and you could just consider yourself lucky if you have contract to a field that has similar crop type field next to it (header can cut).
+</p>
+
+<p>
+Fertilizing contracts pay extremely well, they are borderline cheating they are such a money makers.
+</p>
+
+<p>
+Start working on fields longest edge which allows you to make more progress without constantly turning your vehicle. This is because at the narrow etc edge of the field where you have to turn many times might not be even required to work on before you reach 100% contract completion. Why waste time to something which isn't required.
+</p>
+
+<p>
+There is no point leasing BIG equipment for a SMALL field. Like if you have contract that pays 15k do not lease 12k worth of equipment. But if your contract pays 50k or more, then go for it. In general it is very bad idea to lease vehicles for contract work, also do not hire worker for contract work. Try to calculate the profit from the contract pay minus expenses for vehicle leases and dont forget to include the hourly lease rate approximating how many hours it takes to complete the field. This is somewhat moot point with fertilizing contracts which pay insane amount of money, for example lease rubicon 9000 sprayer and take all fertilizing contracts and you get huge amounts of money.
+</p>
+
+<p>
+In singleplayer you can use courseplay (no MP support in 2019-06-26), which means you can run as many contracts at the same time as you want long as you have the vehicles. Or obviously you could use hired workers but its adviced against doing this as they cost money.
+</p>
+
+<p>
+When you lease big vehicles when you have none or no money, you can sometimes benefit from it like if you need to plow a big field but you only have a small plow, you can take contract for ploughing with big vehicles and once the contract is done go plow your own field before returning the leased vehicles. Not really contract related exactly, but nice little trick.
+</p>
+
+<p>
+Seeding (sowing) contracts in general are quite bad, not recommended. Especially horrendous is the corn planting using horsch mastro 12sw with the inferior width and tank size which makes you need to refill way too often. Try to avoid corn planting with this planter.
+</p>
+
+<p>
+Not really contracts guide more like terrain info, but on 8km x 8km terrain the contracts "lag behind" the field stages, for example you may be offered a seeding contract but when you look at the field in the map it is already growing a crop, if you choose such contract it just disappears. Same happens often times with fertilizing contracts.
+</p>
+
+	<h2>Large Terrains</h2>
+
+<p>
+In general I dont think giants fully planned how contracts scale up in large terrains, they optimized them for 2km sized felsbrunn and ravenport with those tiny garden fields, when you start doing contracts on 4km, 8km or the monster 16km terrains with realistic / huge fields... its definitely a different situation.
+</p>
+
+<p>
+This video shows me fertilizing absolutely massive gigantic 2800 hectare field <a href="https://www.youtube.com/watch?v=AslQMBvsCYw" target="_blank">PMC Grande Gardens 16km terrain 2019-06-21</a>. The reward for that contract was insane <a href="images/PMC-Grande-Gardens-16km-2019-06-22T0900.png" target="_blank">5.6 million</a> heh.
+</p>
+
+<p>
+Now obviously 2800 hectare field is more like an spectacle circus freak that anything normal you would work on, even in real world data wise unless your terrain is located in australia you rarely see fields this massive.
+</p>
+
+<p>
+Even when I played on Midwest Horizons by CBJ and Txzar, which is 4km terrain it was really easy to make money doing fertilizing contracts using small tractor and bredal K165 spreader in the beginning. And once you upgrade (or even lease) rubicon 9000, oh wow the bank is open heh.
 </p>
 
 </section>
