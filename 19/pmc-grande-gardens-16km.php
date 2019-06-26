22bbd03817fa1a8184b805a9c310c09407819e12
Snake Man <snakeman@phnet.fi>
2019-06-27 00:26:32 +0300

    Grande gardens page created

---
+++ 19/pmc-grande-gardens-16km.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PMC Grande Gardens 16km Farming Simulator 19 - PMC Tactical</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="PMC Grande Gardens 16km Farming Simulator 19 - PMC Tactical">
+<META name="keywords" content="Terrain, Grande, Gardens, 16km, PMC, Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>PMC Tactical Farming Simulator Terrain PMC Grande Gardens 16km</b></h1>
+</header>
+
+<section>
+<p>Please read <a href="pmc-terrains.php">PMC Terrains</a> page for background information.</p>
+
+	<h2>PMC Grande Gardens 16km</h2>
+
+<p>
+Fictional terrain, no reference to any real world location or anything, this is completely man made custom terrain. It will have however that feeling of american style farming as everything is large scale and buildings will be american. This terrain was previously known as "MyMap_16km".
+</p>
+
+<div>
+<p style="font-size: 10px;">
+Click for larger image
+</p>
+<a href="images/PMC-Grande-Gardens-16km-PDA.png" target="_blank"><img src="images/thumbs/PMC-Grande-Gardens-16km-PDA.jpg" alt="Farming Simulator 19 Terrain - PMC Grande Gardens 16km" style="padding: 3px 15px;width:256px;height:256px;"></a>
+
+<a href="images/PMC-Grande-Gardens-16km-Fields.jpg" target="_blank"><img src="images/thumbs/PMC-Grande-Gardens-16km-Fields.jpg" alt="Farming Simulator 19 Terrain - PMC Grande Gardens 16km. Fields" style="padding: 3px 15px;width:256px;height:256px;"></a>
+
+<a href="images/PMC-Grande-Gardens-16km-Farmland.jpg" target="_blank"><img src="images/thumbs/PMC-Grande-Gardens-16km-Farmland.jpg" alt="Farming Simulator 19 Terrain - PMC Grande Gardens 16km. Farmland" style="padding: 3px 15px;width:256px;height:256px;"></a>
+</div>
+
+<p>
+This terrain was ported from FS17, you might have seen videos from july 2018 where we demoed this terrain in action. This is the mega terrain with 2800 hectare field. I recall in FS17 I created the nice animal areas in this one (obviously not included in FS19 version). FS17 videos: <a href="https://www.youtube.com/watch?v=1V9m8Pm439M" target="_blank">PMC 16km Terrain Development 2018-07-10 Farming Simulator 17</a>. This was our most developed terrain in FS17 back in late summer of 2018. Would be nice to finish this for FS19.
+</p>
+
+<p>
+Heightmap is brand new L3DT generated nice smooth rolling hills type with plenty of different elevations but still not too rough that farming vehicles and equipment can operate nice and easy (or mostly easy heh).
+</p>
+
+<p>
+Fields smallest: 3.901 ha, largest 2864.922 ha.
+</p>
+<pre>
+Field 1 (field01) : 50.300 ha
+Field 2 (field02) : 91.282 ha
+Field 3 (field03) : 136.982 ha
+Field 4 (field04) : 876.200 ha
+Field 5 (field05) : 1201.400 ha
+Field 6 (field06) : 1201.400 ha
+Field 7 (field07) : 300.700 ha
+Field 8 (field08) : 50.300 ha
+Field 9 (field09) : 70.340 ha
+Field 10 (field10) : 12.650 ha
+Field 11 (field11) : 37.750 ha
+Field 12 (field12) : 50.300 ha
+Field 13 (field13) : 103.406 ha
+Field 14 (field14) : 50.300 ha
+Field 15 (field15) : 129.394 ha
+Field 16 (field16) : 100.500 ha
+Field 17 (field17) : 50.300 ha
+Field 18 (field18) : 50.300 ha
+Field 19 (field19) : 33.483 ha
+Field 20 (field20) : 300.700 ha
+Field 21 (field21) : 300.700 ha
+Field 22 (field22) : 611.811 ha
+Field 23 (field23) : 50.300 ha
+Field 24 (field24) : 73.146 ha
+Field 25 (field25) : 73.146 ha
+Field 26 (field26) : 135.571 ha
+Field 27 (field27) : 50.300 ha
+Field 28 (field28) : 104.516 ha
+Field 29 (field29) : 104.516 ha
+Field 30 (field30) : 1475.146 ha
+Field 31 (field31) : 46.535 ha
+Field 32 (field32) : 43.021 ha
+Field 33 (field33) : 218.068 ha
+Field 34 (field34) : 54.919 ha
+Field 35 (field35) : 54.919 ha
+Field 36 (field36) : 54.919 ha
+Field 37 (field37) : 54.919 ha
+Field 38 (field38) : 98.069 ha
+Field 39 (field39) : 131.058 ha
+Field 40 (field40) : 2864.922 ha
+Field 41 (field41) : 45.280 ha
+Field 42 (field42) : 45.280 ha
+Field 43 (field43) : 45.280 ha
+Field 44 (field44) : 142.245 ha
+Field 45 (field45) : 87.839 ha
+Field 46 (field46) : 131.499 ha
+Field 47 (field47) : 139.143 ha
+Field 48 (field48) : 219.219 ha
+Field 49 (field49) : 219.019 ha
+Field 50 (field50) : 219.219 ha
+Field 51 (field51) : 360.760 ha
+Field 52 (field52) : 219.019 ha
+Field 53 (field53) : 443.289 ha
+Field 54 (field54) : 219.019 ha
+Field 55 (field55) : 340.740 ha
+Field 56 (field56) : 527.960 ha
+Field 57 (field57) : 71.870 ha
+Field 58 (field58) : 62.506 ha
+Field 59 (field59) : 527.960 ha
+Field 60 (field60) : 970.410 ha
+Field 61 (field61) : 20.430 ha
+Field 62 (field62) : 20.430 ha
+Field 63 (field63) : 20.430 ha
+Field 64 (field64) : 20.430 ha
+Field 65 (field65) : 68.026 ha
+Field 66 (field66) : 54.014 ha
+Field 67 (field67) : 54.014 ha
+Field 68 (field68) : 55.310 ha
+Field 69 (field69) : 91.984 ha
+Field 70 (field70) : 180.540 ha
+Field 71 (field71) : 240.640 ha
+Field 72 (field72) : 54.014 ha
+Field 73 (field73) : 54.014 ha
+Field 74 (field74) : 54.014 ha
+Field 75 (field75) : 157.157 ha
+Field 76 (field76) : 308.237 ha
+Field 77 (field77) : 55.310 ha
+Field 78 (field78) : 91.984 ha
+Field 79 (field79) : 54.014 ha
+Field 80 (field80) : 180.540 ha
+Field 81 (field81) : 165.520 ha
+Field 82 (field82) : 636.495 ha
+Field 83 (field83) : 3.920 ha
+Field 84 (field84) : 3.901 ha
+Field 85 (field85) : 3.901 ha
+Field 86 (field86) : 3.921 ha
+Field 87 (field87) : 3.901 ha
+Field 88 (field88) : 3.901 ha
+Field 89 (field89) : 3.901 ha
+Field 90 (field90) : 3.920 ha
+Field 91 (field91) : 3.901 ha
+Field 92 (field92) : 3.920 ha
+Field 93 (field93) : 3.901 ha
+Field 94 (field94) : 3.920 ha
+Field 95 (field95) : 3.901 ha
+Field 96 (field96) : 3.920 ha
+Field 97 (field97) : 3.920 ha
+Field 98 (field98) : 3.901 ha
+</pre>
+
+<p>
+Includes vehicle shop and ten sale points.
+</p>
+
+</section>
+
+<footer>
+<p><a href="pmc-terrains.php" class="button">PMC Farming Simulator Terrains page</a></p>
+<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
