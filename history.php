9adf8664c45dc2bb838d38ac2ce7bebd1f79c52c
Snake Man <snakeman@phnet.fi>
2019-11-19 05:07:13 +0200

    Expansion vs Edition typo fix

--- history.php
+++ history.php
@@ -46,9 +46,9 @@
 2019-06-18 Farming Simulator 19 - v1.4 update <a href="https://www.farming-simulator.com/changelog-fs19.php?lang=en&country=us" target="_blank">changelog</a><br>
 2019-07-11 Farming Simulator 19 - v1.4.1 update <a href="https://www.farming-simulator.com/changelog-fs19.php?lang=en&country=us" target="_blank">changelog</a><br>
 2019-07-23 Farming Simulator 19 - John Deere Cotton DLC released<br>
-2019-07-29 Farming Simulator 19 - Platinum Expansion (Claas DLC) announced (release date 2019-10-22)<br>
+2019-07-29 Farming Simulator 19 - Platinum Edition (Claas DLC) announced (release date 2019-10-22)<br>
 2019-10-15 Farming Simulator 19 - v1.5.1 update <a href="https://www.farming-simulator.com/changelog-fs19.php?lang=en&country=us" target="_blank">changelog</a><br>
-2019-10-22 Farming Simulator 19 - Platinum Expansion (Claas DLC) released
+2019-10-22 Farming Simulator 19 - Platinum Edition (Claas DLC) released
 </p>
 
 </section>
