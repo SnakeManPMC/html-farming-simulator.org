32288bc42d8b8e391461496c1a572f3e2d4168ab
Snake Man <snakeman@phnet.fi>
2020-02-22 03:16:20 +0200

    FS19 bourgault dlc announce history added

--- history.php
+++ history.php
@@ -48,7 +48,12 @@
 2019-07-23 Farming Simulator 19 - John Deere Cotton DLC released<br>
 2019-07-29 Farming Simulator 19 - Platinum Edition (Claas DLC) announced (release date 2019-10-22)<br>
 2019-10-15 Farming Simulator 19 - v1.5.1 update <a href="https://www.farming-simulator.com/changelog-fs19.php?lang=en&country=us" target="_blank">changelog</a><br>
-2019-10-22 Farming Simulator 19 - Platinum Edition (Claas DLC) released
+2019-10-22 Farming Simulator 19 - Platinum Edition (Claas DLC) released<br>
+2020-02-20 Farming Simulator 19 - Bourgault DLC announced
+<!--
+2020-02-27 Farming Simulator 19 - v1.2 patch<br>
+2020-03-10 Farming Simulator 19 - Bourgault DLC <a href="https://www.farming-simulator.com/dlc-detail.php?lang=en&country=us&dlc_id=fs19bourgault" target="_blank">Bourgault DLC</a>
+-->
 </p>
 
 </section>
