19bf79d9349f3e5e0c08e93f4c60776ff8b172a5
Snake Man <snakeman@phnet.fi>
2020-04-26 16:06:25 +0300

    Alphabetical order

--- 19/map-making-tutorial.php
+++ 19/map-making-tutorial.php
@@ -24,45 +24,45 @@
 	<h2>Overview</h2>
 
 <p>
-<b>2019-05-04</b> This page is work in progress, will be updated later.
+<b>2020-04-26</b> This page is work in progress, will be updated as we go along.
 </p>
 
 <p>
 Please read <a href="../map-making-tutorial.php" target="_blank">Farming Simulator 17 (FS17) Map Making Tutorial</a> first because what you see below assumes you know all of that FS17 stuff. Also <a href="https://www.youtube.com/playlist?list=PLKt0kt4BAeflG1eKIxMzWk01TM6swzpum" target="_blank">PMC youtube FS19 playlist</a> has some Giants Editor (GE) videos.
 </p>
 
-<p><a href="terrain-file-structure.php">File Structure</a></p>
-<p><a href="terrain-create-sample.php">Create Sample Terrain</a></p>
-<p><a href="terrain-porting-fs17.php">Porting Farming Simulator 17 Terrain</a></p>
-<p><a href="terrain-farmland.php">Farmland</a> and <a href="terrain-farmland-painting-gimp.php">Farmland Painting Gimp</a></p>
-<p><a href="terrain-moddesc-xml.php">modDesc.xml</a></p>
-<p><a href="terrain-field-definitions.php">Field Definitions</a></p>
-<p><a href="terrain-general-editing-tips.php">General Editing Tips</a></p>
-<p><a href="terrain-sale-points.php">Sale Points</a></p>
-<p><a href="terrain-user-interface.php">User Interface</a></p>
-<p><a href="terrain-common-errors.php">Common Errors</a></p>
-<p><a href="terrain-forests.php">Forests</a></p>
-<p><a href="terrain-giants-editor.php">Giants Editor (GE)</a></p>
-<p><a href="terrain-real-world-data.php">Real World Data Terrain</a></p>
-<p><a href="terrain-density-weight-images.php">Density and Weight Images</a></p>
-<p><a href="terrain-object-view-distance.php">Object View Distance</a></p>
-<p><a href="terrain-heightmap.php">Heightmap</a></p>
-<p><a href="terrain-vehicle-shop.php">Vehicle Shop</a></p>
-<p><a href="terrain-lua-scripts.php">LUA Scripts</a></p>
-<p><a href="terrain-environmental-sounds.php">Environmental Sounds</a></p>
-<p><a href="terrain-global-mapper-painted-roads.php">Global Mapper Painted Roads</a></p>
-<p><a href="terrain-fruit_density-gimp.php">Gimp Paint fruit_density Image</a></p>
-<p><a href="terrain-cultivator_density-gimp.php">Gimp Paint cultivator_density Image</a></p>
-<p><a href="terrain-heightmap-smoothing.php">Heightmap Smoothing</a></p>
 <p><a href="terrain-ai-planted-crop-types.php">AI Planted Crop Types</a></p>
-<p><a href="terrain-edge-block.php">Terrain Edge Block</a></p>
+<p><a href="terrain-common-errors.php">Common Errors</a></p>
+<p><a href="terrain-create-sample.php">Create Sample Terrain</a></p>
+<p><a href="terrain-cultivator_density-gimp.php">Gimp Paint cultivator_density Image</a></p>
+<p><a href="terrain-custom-crop-icons.php">Custom Crop Icons</a></p>
+<p><a href="terrain-density-weight-images.php">Density and Weight Images</a></p>
+<p><a href="terrain-edge-block.php">Edge Block</a></p>
+<p><a href="terrain-environmental-sounds.php">Environmental Sounds</a></p>
+<p><a href="terrain-farmland.php">Farmland</a> and <a href="terrain-farmland-painting-gimp.php">Farmland Painting Gimp</a></p>
+<p><a href="terrain-field-definitions.php">Field Definitions</a></p>
+<p><a href="terrain-file-structure.php">File Structure</a></p>
+<p><a href="terrain-forests.php">Forests</a></p>
+<p><a href="terrain-fruit_density-gimp.php">Gimp Paint fruit_density Image</a></p>
+<p><a href="terrain-general-editing-tips.php">General Editing Tips</a></p>
+<p><a href="terrain-giants-editor.php">Giants Editor (GE)</a></p>
+<p><a href="terrain-giants-game-mode-review.php">Giants Game Mode Review</a></p>
+<p><a href="terrain-global-mapper-painted-roads.php">Global Mapper Painted Roads</a></p>
+<p><a href="terrain-heightmap.php">Heightmap</a></p>
+<p><a href="terrain-heightmap-smoothing.php">Heightmap Smoothing</a></p>
+<p><a href="terrain-howto-create-16km.php">HOWTO Create 16.3km Terrain</a></p>
+<p><a href="terrain-lua-scripts.php">LUA Scripts</a></p>
+<p><a href="terrain-moddesc-xml.php">modDesc.xml</a></p>
+<p><a href="terrain-object-view-distance.php">Object View Distance</a></p>
+<p><a href="terrain-placeable-objects.php">Placeable Objects</a></p>
+<p><a href="terrain-porting-fs17.php">Porting Farming Simulator 17 Terrain</a></p>
+<p><a href="terrain-real-world-data.php">Real World Data Terrain</a></p>
+<p><a href="terrain-sale-points.php">Sale Points</a></p>
+<p><a href="terrain-savegame-update.php">Savegame Update</a></p>
 <p><a href="terrain-traffic-system.php">Traffic System</a></p>
 <p><a href="terrain-train-system.php">Train System</a></p>
-<p><a href="terrain-placeable-objects.php">Placeable Objects</a></p>
-<p><a href="terrain-custom-crop-icons.php">Custom Crop Icons</a></p>
-<p><a href="terrain-giants-game-mode-review.php">Giants Game Mode Review</a></p>
-<p><a href="terrain-savegame-update.php">Savegame Update</a></p>
-<p><a href="terrain-howto-create-16km.php">HOWTO Create 16.3km Terrain</a></p>
+<p><a href="terrain-user-interface.php">User Interface</a></p>
+<p><a href="terrain-vehicle-shop.php">Vehicle Shop</a></p>
 <!--
 <p><a href="terrain-.php"></a></p>
 -->
