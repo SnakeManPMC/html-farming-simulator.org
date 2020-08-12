c3d36f56bbfc91526046ce931036378bcd847497
Snake Man <snakeman@phnet.fi>
2020-08-12 18:57:24 +0000

    heightScale step added

--- 19/terrain-real-world-data.php
+++ 19/terrain-real-world-data.php
@@ -24,11 +24,15 @@
 	<h2>Real World Data Terrain</h2>
 
 <p>
+<b>2020-08-12</b> Updated.
+</p>
+
+<p>
 The tools we use are Global Mapper, Terra Incognita and L3DT. Unfortunately global mapper is a commercial software about $500. Please read <a href="https://pmc.editing.wiki/doku.php?id=arma3:terrain:real-world-data-tutorial" target="_blank">PMC Editing Wiki: ArmA 3 Real World Data Terrain Tutorial</a> how to use these tools to get satellite texture and SRTM heightmap.
 </p>
 
 <p>
-FS19 terrains use 2 meters per pixel heightmap resolution. SRTM heightmap resolution is 30 meters which is awfully low for FS19, but for world wide coverage its best and easiest option to use. For american terrains you should use the United States National Elevation Data (NED) 10m resolution which gives great results for FS19.
+FS19 default terrains use 2 meters per pixel heightmap resolution. SRTM heightmap resolution is 30 meters which is awfully low for FS19, but for world wide coverage its best and easiest option to use. For american terrains you should use the United States National Elevation Data (NED) 10m resolution which gives great results for FS19.
 </p>
 
 <p>
@@ -44,14 +48,15 @@ This is the steps I use to generate real world data terrain source files:<br>
 - global mapper export kmz<br>
 - terra incognita map source -> openstreetmap -> zoom level 500km<br>
 - terra incognita file -> load waypoints,tracks -> kmz<br>
-- terra incognita full screen, zoom into kmz black rectangle (8km is ok with openstreetmap 250m 4m/px but 16km is 500m 8m/px), draw selection rectangle<br>
+- terra incognita full screen, zoom into kmz black rectangle (8km is ok with openstreetmap 500m 8m/px but 16km is 1km 16m/px), draw selection rectangle<br>
 - terra incognita waypoints,tracks -> visible to disable kmz black rectangle<br>
 - terra incognita map source -> esri arcgis, zoom level 17<br>
 - terra incognita download oziexplorer map format<br>
 - global mapper open -> oziexplorer .map/.jpg satellite imagery<br>
 - global mapper file -> export -> export raster/image format -> png -> file type 24-bit rgb, sample spacing 1, export to bounds -> crop to selected area feature(s)<br>
 - l3dt file -> import -> heightfield, choose your heightmap.asc<br>
-- l3dt CTRL-E -> file format -> PNG -> map_dem.png
+- l3dt operations -> heightfield -> change vertical range, write down altitude range (m) this is heightScale<br>
+- l3dt CTRL-E -> file format -> PNG -> heightmap_L3DT.png
 </p>
 
 </section>
