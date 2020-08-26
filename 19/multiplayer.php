f58c59e8198872bbb31595d8412808d9875efd3b
Snake Man <snakeman@phnet.fi>
2020-08-26 10:31:02 +0000

    PMC MP server info page added

--- 19/multiplayer.php
+++ 19/multiplayer.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Farming Simulator 19 Multiplayer - PMC Farming</title>
+<title>PMC Farming Simulator 19 Multiplayer</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator 19 Multiplayer - PMC Farming">
+<META name="description" content="PMC Farming Simulator 19 Multiplayer">
 <META name="keywords" content="Farming, Simulator, Multiplayer, PMC, Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,14 +18,86 @@
 <!-- google ads -->
 <?php include("../include/google-ads.php"); ?>
 <h1><b>PMC Farming Simulator 19 Multiplayer</b></h1>
+<p>
+<b>2020-08-26T10:16:00Z</b> Updated.
+</p>
 </header>
 
 <section>
-	<h2>Dedicated Server</h2>
+	<h2>PMC Terrains Server</h2>
 
 <p>
-<b>2018-11-25</b> This page is work in progress.
+PMC runs server for testing our own terrains, its name is "PMC Terrains". We never run giants or anyone elses terrains as there are plenty of servers available for them, we only game-play test our own terrains in this server.
 </p>
+
+
+	<h2>Mods Download</h2>
+
+<p>
+This is not private link, its a convenience location for all the mods used in PMC Terrains server. Some of the mods like courseplay.zip and guidanceSteering.zip are updated frequenty and most likely have different hash than yours so you could not join the server, download them here and then you can. You are fine to use mods like CBJ Midwest Buildings Pack, Seasons etc from elsewhere as such stable mods do not change hash etc, however to reduce frustration of searching mods, they are located all here.
+</p>
+
+<p>
+Download <a href="https://drive.google.com/drive/folders/1HwronaE_vZw9m-TqCQjxJaGhAAwgf4it?usp=sharing" target="_blank">drive.google</a>.
+</p>
+
+<p>
+Server is running PMC North Dakota Greendale 4km <b>v0.2.1</b>. Note that this is NOT the latest 7zip currently in google/drive. Make sure you load FS19 up with v0.2.1 first. This terrain is not public, so you have to get it from the private link given to you in discord.
+</p>
+
+
+	<h2>Joining</h2>
+
+<p>
+Multiplayer -> Join -> International -> PMC Terrains
+</p>
+
+<p>
+Check on PMC discord #farming_simulator channel if the server is up or down. If its down but you want to play / test, just ask there.
+</p>
+
+
+	<h2>Guide</h2>
+
+<p>
+<b>Chat</b>. To type into chat use T key. You can scroll chat log up/down by arrow keys. If you decide not to type in text, just hit ESC and chat dialog closes.
+</p>
+
+<p>
+<b>My Ping</b>. Your ping is listed as milliseconds (ms), you can find it from the top right corner of mini map (hotkey 9). Average US to EU ping is about 100ms, this is perfectly fine for FS19 gaming.
+</p>
+
+<p>
+<b>Being away</b>. If you go away like kitchen to get a beverage, restroom to take a leak or kitchen for a lunch, always exit any vehicle you're in. If you stay in a vehicle and are away, nobody else can use this vehicle. If you stay away for a very long time, this can be a real problem.
+</p>
+
+<p>
+<b>Cruise Control</b>. Cruise control on vehicles keep a set speed until cruise control is disabled. Hotkeys 1 decrease speed, 2 increase speed, 3 engage and disengage cruise control.
+</p>
+
+<p>
+<b>Crop Destruction</b>. Do not drive into growing crops as PMC server has crop destruction on. You can drive on fields that have no plants growing, you could even drive on them when plants grow on FIRST STAGE of growth, but on second stage I believe you start to destroy crops, so no driving on crops. Remember when you are combining that if you type into chat you cannot hit S or 3 keys to stop your combine, you need to ESC or enter out of the chat dialog first. Its very common to see combine becoming full and driving into crop destroying it while operator is typing into chat. Good rule of thumb is; stop typing into chat when you are 90 percent full.
+</p>
+
+<p>
+<b>GPS</b>. Very often new players have hard time using Wopster's GuidanceSteering mod aka GPS mod. Here is a very quick rundown how it works.<br>
+<b>ALT-C</b> on / off. See the satellite icon in right side of the screen turn green, now GPS is on, white is off.<br>
+<b>CTRL-S</b> menu. Menu only works when GPS is on.<br>
+<b>ALT-E</b> start / end track. Use ALT-E first time to start recording track, drive forward the longer the better then use ALT-E second time to end the track.<br>
+<b>ALT-HOME</b> center track. Centers current track along your vehicle.<br>
+<b>ALT-END</b> rotate track 90 degrees.<br>
+<b>ALT-X</b> enable / disable. This is how to actually drive with GPS, you hear a beep sound when its enabled/disabled. You can disabled GPS driving by steering left or right (A or D key, or steering wheel etc).
+</p>
+
+<p>
+<b>Courseplay</b>. Weeell.. if you have to read a guide about how to use courseplay especially in MP server, then please do not use it, let someone else do it. Sorry but there is no simple quickie-guide to get you up to speed with courseplay.
+</p>
+
+<!--
+<p>
+<b></b>.
+</p>
+-->
 
 </section>
 
