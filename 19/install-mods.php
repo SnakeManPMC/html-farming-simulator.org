8d45c43f04e7ffccf8a51bcad01ab10fc4f90676
Snake Man <snakeman@phnet.fi>
2021-01-04 21:49:29 +0000

    New page created install mods FS19

---
+++ 19/install-mods.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>How To Install Mods Farming Simulator 19 Tutorial - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="How To Install Mods Farming Simulator 19 Tutorial - PMC Farming">
+<META name="keywords" content="Farming, Simulator, FS19, Install, Download, Mods, Tutorial, PMC, Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>How To Install Mods Farming Simulator 19 Tutorial - PMC Farming</b></h1>
+</header>
+
+<section>
+	<h2>Locating Mod Directory</h2>
+
+<p>
+By default MOD directory is located in "C:\Users\YOURNAME\Documents\My Games\FarmingSimulator2019\mods\".
+</p>
+
+<p>
+You can configure where mod directly is located by editing gameSettings.xml file which is located in "C:\Users\YOURNAME\Documents\My Games\FarmingSimulator2019\" directory.
+</p>
+
+<p>
+Below you see example gameSettings.xml file pointing mods directory to C:\FS19.Mods\ and please note that path by default is set with / instead of standard windows \ character.
+</p>
+<pre>
+&lt;?xml version="1.0" encoding="utf-8" standalone="no" ?&gt;
+&lt;gameSettings revision="15"&gt;
+    &lt;modsDirectoryOverride active="true" directory="C:/FS19.Mods"/&gt;
+</pre>
+<p>
+modsDirectoryOverride needs to be set to TRUE.
+</p>
+
+	<h2>Downloading Mods With In-Game Mods Menu</h2>
+
+<p>
+Easiest way to download limited number of mods is Giants ModHub from in-game "Mods" menu. Farming Simulator 19 (FS19) will automatically download mods what you select, then you only need to restart game to get the mods copied into mods directory, FS19 will do this automatically for you (don't worry it asks first).
+</p>
+
+<p>
+Limited? Yes Giants ModHub only has more or less error free tested and hand selected mods. ModHub has hundreds of nice mods but that list cannot compare to other mods available on the internet.
+</p>
+
+<p>
+<a href="https://www.farming-simulator.com/mods.php" target="_blank">Giants ModHub</a> can be accessed from web browser as well, its a normal website to download FS19 mods.
+</p>
+
+<p>
+Here is HOWTO download mods using FS19 mods menu. Start FS19 and right there in the main menu you see "Mods" option.
+</p>
+
+<p>
+Click images for large size.
+</p>
+<a href="images/FS19-Giants-ModHub-2021-01-04T192403.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192403.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+Select this Mods menu option and then you get to the main mods menu screen. On the top most bar you see various odd looking icons, use your game controller being it keyboard or whatever to navigate those icons.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192416.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192416.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+Installed mods icon tab lists all the mods currently in your mods directory. Note the small &gt; arrow right side of the screen, that means you can scroll this list forward, click the arrow or keyboard arrow right/left key to scroll through installed mods list.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192447.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192447.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+Update mods icon tab lists all mods that have upgrade available on Giants ModHub. Note that this upgrade lists mods that are in modhub without actual upgrade but because you edited them in some way they get listed of having upgrade available.
+</p>
+
+<p>
+In this screenshot you see Precision Farming DLC mod having upgrade available (because I edited it).
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192501.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192501.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+Many players are interested of the "LATEST MODS!!1" ;)
+</p>
+
+<p>
+Latest mods icon tab lists the latest mods in Giants ModHub. This can also be scrolled with arrow keys.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192516.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192516.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+Here we clicked John Deere 6030 mod, it brings up detailed mod description screen with two screenshots.
+</p>
+
+<p>
+On bottom left corner is Install option, when you hit enter key or click install text the install process will start. With smaller mods and decent internet speed this process is very fast.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192602.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192602.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+You get dialog saying "Mod NAME was added to the download list!", click OK to exit this dialog.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192611.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192611.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+Now on Installed mods icon tab you find this john deere 6030 mod and it looks like its already 100% download complete. This gray bar on the bottom which says 100% on our screenshot will show the download progress. Depending on your internet speed and possible connection problems, this can take a long time... or like said be real fast.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192630.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192630.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+When you press ESC key to back out from installed mods tab you get a dialog saying "Mods downloaded. Mods were downloaded successfully. Please restart the game to install them. Restart now?", if you answer yes FS19 will restart and the newly downloaded mod is ready to play, or you can answer No and restart later.
+</p>
+
+<a href="images/FS19-Giants-ModHub-2021-01-04T192637.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Giants-ModHub-2021-01-04T192637.jpg" alt="Farming Simulator 19 - Giants ModHub" loading="lazy"></a>
+
+<p>
+This is how you download mods from Giants ModHub using the in-game mods menu.
+</p>
+
+<p>
+When you start a new career or later load a savegame, you get to choose which mods to use (activate).
+</p>
+
+<a href="images/FS19-Mods-DLC-Selection-2021-01-04T2056.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/FS19-Mods-DLC-Selection-2021-01-04T2056.jpg" alt="Farming Simulator 19 - Mods/DLC Selection" loading="lazy"></a>
+
+<p>
+In this Mods/DLC selection screen you can see john deere DB60 planter mod is NOT selected. Scroll through the list and select mods using SPACE and BACKSPACE keys, or mouse click on them.
+</p>
+
+	<h2>Downloading Mods Normally</h2>
+
+<p>
+Use your web browser to find a good FS19 mods website and download mods from there normally. Not sure what tutorial we can write about using world wide web ;)
+</p>
+
+<p>
+Be aware that Farming Simulator community has many bad websites which use scams to clickbait you into their pages which contain nothing but ads and no actual mod or whatever content you were looking for. Also many mod developers have only facebook pages for unknown reasons.
+</p>
+
+<p>
+If the website looks okay and you found the mod you were looking for... be very aware of the download link, being it button or link text. If you cant see which site the download is coming from, hover mouse over the link and hopefully the link is shown in your browsers bottom left corner, sometimes not (these scam sites want to hide their intentions). Some file hosting sites are notoriously bad with "click this click that ads popup here" and you end up with several new browser tabs opened to shady advertisements or worse. You could try to see which file host site the link goes to and google it, over time you learn from experience which file hosts to avoid.
+</p>
+
+<p>
+We do know several bad FS19 websites which should never be visited, but this list can change quickly, what is up to date today most likely is outdated couple of months from now as new scam sites are created. Its impossible to list "all safe websites" as bad ones can pop up in any moment.
+</p>
+
+<p>
+Howto search FS19 mods from the web, use <a href="https://www.google.com/search?&q=farming+simulator+19+mods" target="_blank">google.com/search?&q=farming+simulator+19+mods</a> and you get plenty of hits, good and bad.
+</p>
+
+<p>
+Once you have successfully downloaded mod ZIP file, place it on your mods directory as explained in beginning of this tutorial page. If you cannot find the download you should configure your web browser to download into directory you want, but most likely file is on "C:\Users\USERNAME\Downloads\" directory.
+</p>
+
+	<h2>What Are _UNZIP Files?</h2>
+
+<p>
+Sometimes mod developers especially terrain developers release "Filename_UNZIP.zip" packets, perhaps its RAR and unfortunately very rarely 7-zip. This is done wrong in almost all cases as terrain devs include required mods or "good stuff" which they think you should use to play in the downloaded release packet instead of a link, you probably already have them or they simply should be downloaded individually to save bandwidth and hard disk space.
+</p>
+
+<p>
+"Filename_UNZIP.zip" can be opened with WinZip (windows 7/8/10 built in nowadays), WinRAR or 7-Zip.
+</p>
+
+<p>
+"Filename_UNZIP.rar" and "Filename_UNZIP.7z" can be opened with WinRAR or 7-Zip.
+</p>
+
+<p>
+If you already have WinRAR use it, but if you don't then download <a href="https://www.7-zip.org/" target="_blank">7-Zip</a> which is the best file compress software available in 2021-01-04.
+</p>
+
+<p>
+Once you unpack zip/rar/7z archive you downloaded, bare minimum there should be mod ZIP files inside, also there might be readme or other files. Move only the FS19 mod zip files into your game mods directory and you are ready to play. Easiest way to identify actual mod zip file is to open it and if there is "modDesc.xml" file in the packet root directory, its a mod.
+</p>
+
+</section>
+
+<footer>
+<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
