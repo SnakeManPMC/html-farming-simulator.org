c9528f9f2d6c9c08a83d6eb903c4868796c5ba2c
Snake Man <snakeman@phnet.fi>
2021-01-04 23:00:25 +0000

    New page created install PMC terrains

---
+++ 19/install-pmc-terrain-mods.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Install PMC Terrain Mods Tutorial - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Install PMC Terrain Mods Tutorial - PMC Farming">
+<META name="keywords" content="Install, Terrain, Mods, Tutorial, PMC, Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>Install PMC Terrain Mods Tutorial - PMC Farming</b></h1>
+</header>
+
+<section>
+	<h2>PMC Terrain Releases</h2>
+
+<p>
+PMC terrains are released in 7-zip packets which include three (3) files inside: LICENSE.txt, FS19 terrain mod zip and readme.
+</p>
+
+<p>
+PMC terrain license allows you to share <i>the original release packet</i> to anyone and any site as long as that distribution is non commercial use.
+</p>
+
+<p>
+If you don't know how to install FS19 mods in general please read <a href="install-mods.php">install FS19 mods tutorial</a> before continuing.
+</p>
+
+<p>
+When you download one of PMC terrain releases you see 7-zip file for example "PMC_Texas_Rowena_8km_v1.2.3.7z", if you do not have windows show file extensions enabled (<a href="https://www.google.com/search?&q=windows+show+file+extensions" target="_blank">you really should do this</a>) then all you see is "PMC_Texas_Rowena_8km_v1.2.3" file name (it has .7z extension, you just dont see it).
+</p>
+
+<p>
+In windows explorer select the downloaded terrain 7-zip packet, right mouse button click it and then you see something like this:
+</p>
+
+<img class="terrainimg" src="images/FS19-HOWTO-Install-PMC-Terrain-Mods-2021-01-04T215606.png" alt="Farming Simulator 19 - PMC Terrain Mods Install Tutorial Open 7-Zip Archive" loading="lazy" style="max-width:768px;">
+
+<p>
+Now just select either open archive or extract here, depending on your need. "Extract to" is nice and clean way to unpack any archive to one dir, then you can see what came out of that packet and can easily delete the dir once you're done, instead of "extract here" which depending on the directory you are in, could get many files all over.
+</p>
+
+<p>
+If you chose open archive then you should see something like this, file names obviously change depending which terrain you downloaded:
+</p>
+
+<img class="terrainimg" src="images/FS19-HOWTO-Install-PMC-Terrain-Mods-2021-01-04T215640.png" alt="Farming Simulator 19 - PMC Terrain Mods Install Tutorial 7-Zip Contents" loading="lazy" style="max-width:768px;">
+
+<p>
+In the above example image file name "PMC_Texas_Rowena_8km.zip" is the Farming Simulator 19 ready to play mod zip.
+</p>
+
+<p>
+Remember to read LICENSE.txt so you know what is allowed with PMC terrain releases and also check out readme. Once you have read the license and readme, you can move the terrain mod zip into your FS19 mods directory (unless you extracted it there directly).
+</p>
+
+<p>
+All done, enjoy large scale American real world data farming with PMC terrain :)
+</p>
+
+</section>
+
+<footer>
+<p>
+<a href="pmc-terrains.php" class="button">PMC Terrains page</a>
+<a href="../index.php" class="button">PMC Farming Simulator root page</a>
+</p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
