28ae8fb55584ccc8e8951d813df00c99accff464
Snake Man <snakeman@phnet.fi>
2021-01-22 09:22:46 +0000

    Grimme announcement added

--- history.php
+++ history.php
@@ -60,6 +60,10 @@
 2020-11-05 Farming Simulator 19 - v1.7.1 update <a href="https://farming-simulator.com/newsArticle.php?news_id=193" target="_blank">news</a><br>
 2020-11-12 Farming Simulator 19 - Alpine Terrain DLC release <a href="https://www.farming-simulator.com/newsArticle.php?lang=en&country=us&news_id=191" target="_blank">news</a><br>
 2020-12-08 Farming Simulator 19 - Precision Farming mod release <a href="https://www.farming-simulator.com/newsArticle.php?lang=en&country=us&news_id=196" target="_blank">news</a>, <a href="https://www.farming-simulator.com/mod.php?&mod_id=188882&title=fs2019" target="_blank">mod download</a><br>
+2021-01-19 Farming Simulator 19 - GRIMME Equipment Pack announced <a href="https://www.farming-simulator.com/newsArticle.php?news_id=204" target="_blank">news</a><br>
+<!--
+2021-01-26 Farming Simulator 19 - GRIMME Equipment Pack release <a href="https://www.farming-simulator.com/dlc-detail.php?&dlc_id=fs19grimme" target="_blank">details</a><br>
+-->
 </p>
 
 </section>
