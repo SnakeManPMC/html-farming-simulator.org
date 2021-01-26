27f9f6c1eb5b90c9e41ee2ebdad4258387b26797
Snake Man <snakeman@phnet.fi>
2021-01-26 03:48:22 +0000

    Video updated

--- 19/pmc-grande-gardens-16km.php
+++ 19/pmc-grande-gardens-16km.php
@@ -45,7 +45,7 @@ Click for larger image
 Latest PMC Grande Gardens 16km Start From Zero video.
 </p>
 <iframe width="480" height="270"
-src="https://www.youtube.com/embed/r1NFyMOXLjs">
+src="https://www.youtube.com/embed/w3zBybXSE6g">
 </iframe>
 </div>
 
