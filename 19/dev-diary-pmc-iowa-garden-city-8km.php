819018fd878d9c17cb4d0805db5314b104ba8890
Snake Man <snakeman@phnet.fi>
2021-06-20 17:02:46 +0000

    Dev diary updated

--- 19/dev-diary-pmc-iowa-garden-city-8km.php
+++ 19/dev-diary-pmc-iowa-garden-city-8km.php
@@ -1154,6 +1154,224 @@ And with that edit I'm ready to start put together next internal testing version
 2021-06-05T06:40:00Z Compiled PMC_Iowa_Garden_City_8km_v1.1.7.7z for internal testing.
 </p>
 
+<p>
+<b>2021-06-12</b> Seasons GEO iowa garden city increased winter clouds, rain chance and amount of rain. Gotta make winters HARD <img src="../images/smileys/icon_wink.gif" alt="Smiley ;)" loading="lazy">
+</p>
+
+<p>
+<b>2021-06-13</b> Floydno9 had reported that with our seasons GEO iowa garden city sugar beets cannot be harvested. So I started new savegame and.. well let me copy paste my play-by-play report here below, I didn't write it dev diary in mind so be aware of that.
+</p>
+
+<p>
+Started new savegame, 9 day seasons. 01/early-spring in-game 0835hrs, weather -2C/+4C. Lets start.
+</p>
+
+<p>
+Had to time accelerate until soil temperature reached +5C which is sugar beet planting temperature. I'm using my standard 6000 speed time acceleration.
+</p>
+
+<p>
+02/early-spring in-game 0817hrs, weather -0C/+2C.<br>
+03/early-spring in-game 0746hrs, weather -2C/+2C.<br>
+04/mid-spring in-game 0748hrs, weather +6C/+3C.<br>
+05/mid-spring in-game 0803hrs, weather +9C/+6C. Its now sugar beet planting time.
+</p>
+
+<p>
+Got sugar beets planted, next was just to time accelerate until they are harvest ready... or wither?
+</p>
+
+<p>
+06/mid-spring in-game 0742hrs, weather +7C. After few real life minutes sugar beets got germinated.<br>
+07/late-spring in-game 0747hrs, weather +12C/+10C. After few real life minutes sugar beets are growing (green plants appeared).<br>
+08/late-spring in-game 0724hrs, weather +12C/+11C.<br>
+09/late-spring in-game 0713hrs, weather +13C.<br>
+01/early-summer in-game 0655hrs, weather +13C/+14C.<br>
+02/early-summer in-game 0710hrs, weather +14C/+16C. Second growth stage kicked in, green plants increased in size.<br>
+03/early-summer in-game 0650hrs, weather +14C/+16C.<br>
+04/mid-summer in-game 0649hrs, weather +17C.<br>
+05/mid-summer in-game 0709hrs, weather +19C. Another growth stage kicked in.<br>
+06/mid-summer in-game 0710hrs, weather +16C/+20C.<br>
+07/late-summer in-game 0731hrs, weather +17C/+20C.<br>
+08/late-summer in-game 0739hrs, weather +16C/+20C.<br>
+09/late-summer in-game 0747hrs, weather +16C/+19C.<br>
+01/early-autumn in-game 0754hrs, weather +15C/+19C.<br>
+02/early-autumn in-game 0841hrs, weather +12C/+17C.<br>
+03/early-autumn in-game 0810hrs, weather +14C/+16C.<br>
+04/mid-autumn in-game 0814hrs, weather +6C/+13C. First day of sugar beets harvest season.<br>
+05/mid-autumn in-game 0859hrs, weather +8C/+12C. Growth stage is: remove tops. This is before half way to harvest season.
+</p>
+
+<p>
+Conclusion is that at least for me with the latest seasons GEO, I can get into "remove tops" stage just fine, I assume that is the harvest ready stage.
+</p>
+
+<p>
+2021-06-13T11:21:00Z Painted tipcol with RGB 0,0,0 black, tipping allowed, for sure this time heh.
+</p>
+
+<p>
+Seasons mask created.
+</p>
+
+<p>
+<b>2021-06-15T01:40:00Z</b> Tipping collision fixed, REAL PROPER this time haha!
+</p>
+
+<p>
+Yeah it was odd, I painted tipCol info layer in GE with BLACK color meaning it was erased, but no dice there was still tipping collision when playing in-game. I painted and checked in-game for like 5 times and no go, still tipping collision.
+</p>
+
+<p>
+Well then I ran GE script update ground collision map which I always thought was for PLACEABLES, guess not... first in-game test after that and boom tipping collision fixed.
+</p>
+
+<p>
+<b>2021-06-18T14:54:00Z</b> Editing continues, took backups to have another restore point to fall back into if I mess up.
+</p>
+
+<p>
+Added storeItems into modDesc.xml so that placeables work properly without causing errors or even a game freeze.
+</p>
+
+<p>
+2021-06-18T15:46:00Z Added animal dealer 3D model and associated trigger with warning stripes.
+</p>
+
+<p>
+Was frustrated with the lack of animal hog/pig barns as we don't have any that would match these iowa ones. Didn't want to get stuck dwelling over this problem and decided to do new farmer game-mode vehicles next.
+</p>
+
+<p>
+Iowa garden city player farm 1 driveway grid coordinates 2820,3320 and vehicle shop coords 3630,4690.
+</p>
+
+<p>
+Purchased Lemken Titan 18 plow (width 4.9m, speed 12km/h, required horsepower 300hp). John Deere 6R with 6250R engine upgrade (300hp).
+</p>
+
+<p>
+Used GtX easy dev controls to teleport tractor and plow into players farm 1 driveway, then parked plow into one of the CBJ vehicle sheds. Used easy dev control to throw tractor back to vehicle shop ready to pickup new purchases.
+</p>
+
+<p>
+Purchased Horsch Tiger 6 DT cultivator (width 6m, speed 15km/h, required horsepower 300hp). Then did the same parking shuffle, next following vehicles/implements are done the same way but I don't spam every itchy pitchy detail here other than brand/model/specs.
+</p>
+
+<p>
+Purchased Lemken Solitair 23 (capacity 2000 liters) and Lemken Azurit 9 (capacity 600 liters, width 6m, speed 15km/h, required horsepower 110hp). Filled with fertilizer and seeds.
+</p>
+
+<p>
+Purchased Vaderstad Rapid A 600S (capacity 3100 liters, width 6m, speed 18km/h, required horsepower 180hp). Filled with fertilizer and seeds.
+</p>
+
+<p>
+Purchased Agroliner TAW 30 (capacity 45,000 liters).
+</p>
+
+<p>
+Purchased Kuhn PF 1500 (capacity 1500 liters) and Kuhn Deltis 1302 MTA3 (capacity 1300 liters, with 21m, speed 12km/h).
+</p>
+
+<p>
+Purchased Kuhn Axis 40.2 M-EMC-W (capacity 3200 liters, width 24m, speed 20km/h).
+</p>
+
+<p>
+Purchased Rostelmash Nova 330 combine, Ziegler Corn Champion 5R header (width 3.4m, speed 10km/h), Rostelmash PowerStream500 header (width 5m, speed 10km/h).
+</p>
+
+<p>
+Purchased product pallets 4 seed, 4 fertilizer, 4 lime and 2 herbicide tanks. Had to use fliegl autoload trailer to transfer these pallets into one of the CBJ midwest buildings vehicle sheds in the farm, I could not figure out any way to teleport them.
+</p>
+
+<p>
+Then opened savegame vehicles.xml and edited operating time and wear to zero, then copy pasted everything to terrains defaultVehicles.xml and added defaultFarmProperty="true" property.
+</p>
+
+<p>
+2021-06-18T18:22:00Z Tested and stuff appears okay in New-farmer game-mode, they do not appear in Farm manager. All good.
+</p>
+
+<p>
+2021-06-18T19:22:00Z Grass cleared off from farm 1 placeable buildings, didn't paint the ground to gravel or anything yet, at least now it looks a bit better than just grass growing inside your vehicle sheds.
+</p>
+
+<p>
+Then added terrain edge block invisible geometry's so vehicles wont fall off the edge. This was super easy, just import pre-made terrain_boundaries_8km.i3d and boom done <img src="../images/smileys/icon_wink.gif" alt="Smiley ;)" loading="lazy">
+</p>
+
+<a href="screenshots/PMC-Iowa-Garden-City-8km-2021-06-18T1927.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Iowa-Garden-City-8km-2021-06-18T1927.jpg" alt="PMC Iowa Garden City 8km Terrain, Developer Diary" loading="lazy" class="terrainimg"></a>
+
+<p>
+2021-06-18T20:00:00Z Transferred Location-06 farm yard buildings to player farm 2. Its on farmId="2" now which I actually cant test without starting multiplayer hosted server as far as I know. Buildings show up fine in farm manager game-mode but door animations wont work (because I'm on farmId="1" in singleplayer).
+</p>
+
+<p>
+Hmm I think farmId="0" which is tied to the farmland purchase is better option here.
+</p>
+
+<p>
+In multiplayer server players create farms and then wont get any buildings right away, like whoever creates farm 2 doesn't necessarily want to buy this farm yard, so with farmId="0" they could buy any land they wish along with the buildings located there.
+</p>
+
+<p>
+Need to figure that out and document it properly, I think my local notes have something written down but have already forgotten them heh.
+</p>
+
+<p>
+<b>2021-06-19T02:10:00Z</b> Transferred Location-09 into player farms.
+</p>
+
+<p>
+Then transferred Location-10, 13, 19, 20, 24 and 25 into player farms.
+</p>
+
+<p>
+With Location-24 I had to create farmSiloSmall for PMC Farm Silos mod as it was not there yet, just never had any need for this smaller silo.
+</p>
+
+<p>
+2021-06-19T03:11:00Z Transferred Location-26 into player farms.
+</p>
+
+<p>
+Quite nice bunch of farms transferred into players ownership, all in farmId="0" which should work okay, needs testing.
+</p>
+
+<p>
+Started FS19 for in-game test and got this error:
+</p>
+
+<pre>
+2021-06-19 03:13 Error: C:/FS19.Mods/PMC_Iowa_Garden_City_8km/defaultItems.xml. items.
+</pre>
+
+<p>
+Umm "items", thats it? Can you elaborate on that a bit? HEHE
+</p>
+
+<p>
+First time I've seen such "items" error, time to go check what notepad++ copy pasting messed up in there. Discovered that farmSiloLarge.xml ending line was /&gt; instead of just &gt; heh
+</p>
+
+<p>
+2021-06-19T06:16:00Z Just got all the new player farm placeable objects in so that on new savegame you don't own any of them but they are in the terrain and animations work (doors open/close), then once you buy the land you get ownership of the buildings/silos as well.
+</p>
+
+<p>
+<b>2021-06-20T00:09:00Z</b> PMC Farm Silo Extension gave some lua index error when you attempted to sell it, well there was a bug in defaultItems.xml where className was SiloPlaceable which was wrong, it needs to be SiloExtensionPlaceable, its now finally fixed.
+</p>
+
+<p>
+2021-06-20T15:17:00Z New farmer game-mode vehicles, Kuhn axis 40.2 m-emc-w spreader cant do lime so you have now NO lime spreading ability, had to fix this by selling kuhn and buying Bredal K105 with 6m spreading unit.
+</p>
+
+<p>
+Then updated defaultVehicles.xml with operating time and wear set to zero, also added defaultFarmProperty="true" as usual.
+</p>
+
 </section>
 
 <footer>
