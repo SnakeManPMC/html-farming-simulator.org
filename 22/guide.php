27a384d147474fd1c3881507644d88cbafa6158f
Snake Man <snakeman@phnet.fi>
2021-11-20 15:46:52 +0000

    FS22 rock picking guide added

--- 22/guide.php
+++ 22/guide.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Farming Simulator 22 Guide - PMC Farming</title>
+<title>Farming Simulator 22 Playing Guide - PMC Farming</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator 22 Guide - PMC Farming">
-<META name="keywords" content="Farming, Simulator, FS22, Guide, PMC, Farming">
+<META name="description" content="Farming Simulator 22 Playing Guide - PMC Farming">
+<META name="keywords" content="Farming, Simulator, FS22, Guide, Tutorial, PMC, Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -17,7 +17,7 @@
 <header>
 <!-- google ads -->
 <?php include("../include/google-ads.php"); ?>
-<h1><b>PMC Farming Simulator 22 Guide</b></h1>
+<h1><b>PMC Farming Simulator 22 Playing Guide</b></h1>
 </header>
 
 <section>
@@ -25,18 +25,29 @@
 	<h2>Playing Guide</h2>
 
 <p>
-<b>2021-11-20T14:20:00Z</b> updated. This page is SPECULATION until 2021-11-21 sunday UTC 2300hrs when we get to download the game and try this info ourselves.
+<b>2021-11-20T15:43:00Z</b> updated. This page is SPECULATION until 2021-11-21 sunday UTC 2300hrs when we get to download the game and try this info ourselves.
 </p>
 
 <p>
-Please read <a href="../guide.php">FS17 Guide</a> and <a href="../19/guide.php">FS19 Guide</a> first as we do not repeat the same stuff here unless it has been changed.
+Please read <a href="../guide.php">Farming Simulator 17 Guide</a> and <a href="../19/guide.php">Farming Simulator 19 (FS19) Guide</a> first as we do not repeat the same stuff in this tutorial unless it has been changed.
+</p>
+
+
+	<h2>Rock Picking</h2>
+
+<p>
+Back in FS19 ThundR created a mod for rock picking, now this is an in-game Farming Simulator 22 (FS22) feature. Giants call this "stone picking" but all real life youtube farming videos use rock picking terminology.
+</p>
+
+<p>
+When you work the fields small patches of rocks appear. If you do not pick up these rocks they will damage your vehicles which drive over them, seeder, planter, combine harvester etc.
+</p>
+
+<p>
+Purchase rock picker (stone picker) from the store, use your tractor to pull it and work the field to collect these rocks. You can sell or dump these rocks to get rid of them. Money you get from selling rocks is very little, you do not get rich by selling rocks.
 </p>
 
 </section>
-<!--
-<p>
-</p>
--->
 
 <footer>
 <p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
