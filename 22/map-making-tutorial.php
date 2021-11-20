3dfab056d16112bb51d22d690566c5ad60020bf7
Snake Man <snakeman@phnet.fi>
2021-11-20 15:33:00 +0000

    FS22 map making tutorial created - file structure three terrains created

---
+++ 22/map-making-tutorial.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Farming Simulator 22 Map Making Tutorial - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Farming Simulator 22 Map Making Tutorial - PMC Farming">
+<META name="keywords" content="Farming, Simulator, FS22, Map, Making, Tutorial, Terrain, PMC, Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>PMC Farming Simulator 22 Map Making Tutorial</b></h1>
+</header>
+
+<section>
+	<h2>Overview</h2>
+
+<p>
+<b>2021-11-20T14:59:00Z</b> updated. This page is SPECULATION until after 2021-11-21 sunday UTC 2300hrs when we get to download the game and try this info ourselves.
+</p>
+
+<p>
+Please read <a href="../map-making-tutorial.php" target="_blank">Farming Simulator 17 (FS17) Map Making Tutorial</a> and <a href="../19/map-making-tutorial.php" target="_blank">Farming Simulator 19 (FS19) Map Making Tutorial</a> first because what you see below assumes you know all of that FS17 and FS19 stuff. If you have any corrections or questions please <a href="../contact.php">contact us</a>.
+</p>
+
+<p><a href="terrain-file-structure-mapalpine.php">File Structure mapAlpine (Erlengrat)</a>, <a href="terrain-file-structure-mapfr.php">mapFR (Haut-Beyleron)</a> and <a href="terrain-file-structure-mapus.php">mapUS (Elmcreek)</a></p>
+</section>
+
+<footer>
+<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 <a href="22/contracts.php" class="button">Contracts</a>
 <a href="22/guide.php" class="button">Guide</a>
 <a href="22/equipment-vehicle-prices.php" class="button">Equipment Vehicle Prices</a>
+<a href="22/map-making-tutorial.php" class="button">Map Making Tutorial</a>
 </p>
 
 
