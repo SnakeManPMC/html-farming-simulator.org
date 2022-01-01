7b14528e9135616429bfbe2120574e72957217a8
Snake Man <snakeman@phnet.fi>
2022-01-01 05:25:07 +0000

    FS22 test farm 10km page added

---
+++ 22/pmc-test-farm-10km.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PMC Test Farm 10km Terrain Farming Simulator 22 - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="PMC Test Farm 10km Terrain Farming Simulator 22 - PMC Farming">
+<META name="keywords" content="Terrain, Test, 10km, Farming, Simulator, FS22, Download, Big, Map, PMC">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>PMC Test Farm 10km Terrain Farming Simulator 22</b></h1>
+</header>
+
+<section>
+	<h2>PMC Test Farm 10km</h2>
+
+<p>
+<b>2022-01-01T05:16:00Z</b>
+</p>
+
+<p>
+Terrain size: 10240 meters x 10240 meters, ie 10.2 kilometers x 10.2 kilometers (elmcreek is 2km). Terrain type: fictional.
+</p>
+
+<div>
+<p style="font-size: 10px;">
+Click for larger image
+</p>
+<a href="screenshots/PMC-Test-Farm-10km-2021-12-24T0113.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Test-Farm-10km-2021-12-24T0113.jpg" class="terrainimg" loading="lazy" alt="PMC Farming Simulator 22 Screenshot"></a>
+
+<a href="screenshots/PMC-Test-Farm-10km-2021-12-24T0408.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Test-Farm-10km-2021-12-24T0408.jpg" class="terrainimg" loading="lazy" alt="PMC Farming Simulator 22 Screenshot"></a>
+
+<a href="screenshots/PMC-Test-Farm-10km-2021-12-24T0454.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Test-Farm-10km-2021-12-24T0454.jpg" class="terrainimg" loading="lazy" alt="PMC Farming Simulator 22 Screenshot"></a>
+
+<a href="screenshots/PMC-Test-Farm-10km-2021-12-24T0941.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Test-Farm-10km-2021-12-24T0941.jpg" class="terrainimg" loading="lazy" alt="PMC Farming Simulator 22 Screenshot"></a>
+
+<a href="screenshots/PMC-Test-Farm-10km-2021-12-24T2149.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Test-Farm-10km-2021-12-24T2149.jpg" class="terrainimg" loading="lazy" alt="PMC Farming Simulator 22 Screenshot"></a>
+</div>
+
+<p>
+This mod has errors, don't download. A very bad big map, with big square fields :)
+</p>
+
+<p>
+This terrain is not meant for playing as its a simply testing version for 10.2km x 10.2km terrain size. However I was asked to share example files to test some things, so I uploaded this terrain and thought why not share with everyone.
+</p>
+
+<p>
+This is empty object-less terrain, there are no buildings, no trees and no selling points. Heightmap is flat 0 meter elevation, no hills no nothing. There are many square fields all the same size, these fields are painted in cultivator_density and there are no field definitions so contracts will not work.
+</p>
+
+<p>
+There is a vehicle shop triggers only (no 3D building), no vehicle repair trigger either. Farmland image is one ID covers all style.
+</p>
+
+	<h2>License</h2>
+
+<p>
+License: <b><a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY NC ND</a></b>, which means you are free to distribute/share the original 7-zip packet but NO commercial use and NO editing, see LICENSE.txt for details.
+</p>
+
+	<h2>Required Mods</h2>
+
+<p>
+None.
+</p>
+
+	<h2>Download</h2>
+
+<p>
+PMC_Test_Farm_10km_v0.1.7z 1.78mb <a href="downloads/PMC_Test_Farm_10km_v0.1.7z">farming-simulator.org</a>
+</p>
+
+<p>
+For installing please read <a href="../19/install-mods.php">how to install FS19 mods</a> and <a href="../19/install-pmc-terrain-mods.php">how to install PMC terrain mods</a>. Yes these FS19 tutorials still work with FS22 with the obvious "19" versus "22" changes.
+</p>
+
+</section>
+
+<footer>
+<p><a href="pmc-terrains.php" class="button">PMC Terrains FS22 page</a>
+<a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
