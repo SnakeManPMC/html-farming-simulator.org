866c7a4c4d709a40c842085544de90737316dcc5
Snake Man <snakeman@phnet.fi>
2022-01-04 08:43:32 +0000

    FS22 modDesc page added

---
+++ 22/terrain-moddesc-xml.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>modDesc.xml Farming Simulator 22 - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="modDesc.xml Farming Simulator 22 - PMC Farming">
+<META name="keywords" content="Terrain, modDesc.xml, FS22, PMC, Farming, Simulator">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>Terrain modDesc.xml PMC Farming Simulator 22</b></h1>
+</header>
+
+<section>
+	<h2>modDesc.xml</h2>
+
+<p>
+<b>2022-01-04T08:32:00Z</b> Updated. Game engine v1.2.0.2 used.
+</p>
+
+<p>
+First please read <a href="../19/terrain-moddesc-xml.php">FS19 modDesc.xml</a> page.
+</p>
+
+	<h2>FS22 modDesc Version</h2>
+
+<p>
+You can now find modDesc version from v1.2.0.2 game log. Search for "ModDesc Version:" string in the log, the number is the current game engine modDesc version number.
+</p>
+<pre>
+2021-12-24 22:23   ModDesc Version: 63
+</pre>
+
+<p>
+Keep your modDesc.xml files first line always up to date against game engines version. Obviously this doesn't mean you need to release "re-issue" of your terrain just because new game update increments modDesc Version number, but when ever you are releasing new update make sure modDesc is up to date as well.
+</p>
+
+</section>
+
+<footer>
+<p><a href="map-making-tutorial.php" class="button">PMC Farming Simulator 22 Map Making Tutorial page</a></p>
+<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
