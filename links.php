d4582c6304122cff7f8ed8191fb29a54bc22866b
Snake Man <snakeman@phnet.fi>
2022-05-01 13:15:21 +0000

    Links updated, obsolete removed etc

--- links.php
+++ links.php
@@ -23,34 +23,12 @@
 <section>
 	<h2>PMC Site Links</h2>
 <p>
-<a href="http://www.pmctactical.org" target="_blank">PMC Tactical</a><br>
-<a href="http://pmc.editing.wiki" target="_blank">PMC Editing Wiki</a><br>
-<a href="http://www.pmcnews.net" target="_blank">PMC News</a><br>
-<a href="http://www.scenariohq.com" target="_blank">Scenario HeadQuarters</a><br>
-<a href="http://www.elitedangerous.net" target="_blank">EliteDangerous.net</a><br>
-<a href="http://www.silenthunter.info" target="_blank">SilentHunter.info</a><br>
-</p>
-	<h2>Farming Simulator Links</h2>
-
-<p>PMC does not endorse these sites, some of them may be shady ads ridden hellholes, greatest sites on the web or down or whatever, so click on your own risk. We just list them here for reference.
-</p>
-
-<p>
-<a href="http://www.modhub.us/category/farming-simulator-2017-mods/maps/" target="_blank">modhub.us, FS17 terrains</a><br>
-<a href="https://www.modhoster.com/" target="_blank">modhoster.com</a><br>
-<a href="https://www.modland.net/farming-simulator-2017-mods/maps/" target="_blank">modland.net, FS17 terrains</a><br>
-<a href="https://github.com/DeckerMMIV?tab=repositories" target="_blank">DeckerMMIV github</a><br>
-<a href="http://modcentral.co.uk/resources/categories/farming-simulator-2017.187/" target="_blank">modcentral.co.uk/fs17</a><br>
-<a href="http://pvmods.enjin.com/home/m/31719950" target="_blank">pvmods</a><br>
-<a href="http://www.blacksheep-modding.com/" target="_blank">blacksheep-modding.com</a><br>
-<a href="https://nld-farmers.nl/" target="_blank">nld-farmers.nl</a><br>
-<a href="https://www.facebook.com/StevieFSMods/" target="_blank">StevieFSMods (facebook)</a><br>
-</p>
-
-<p>
-<a href="https://www.twitch.tv/dajnet" target="_blank">Twitch - pleasant valley dev</a><br>
-<a href="https://www.twitch.tv/jbmods" target="_blank">Twitch - jbmods</a><br>
-<a href="https://www.youtube.com/channel/UCIsWsALNJN7mK_NIjh-ne7A" target="_blank">Youtube - Kentucky Farmer</a><br>
+<a href="https://www.elitedangerous.net" target="_blank">EliteDangerous.net</a><br>
+<a href="https://www.pmctactical.org" target="_blank">PMC Tactical</a><br>
+<a href="https://pmc.editing.wiki" target="_blank">PMC Editing Wiki</a><br>
+<a href="https://www.scenariohq.com" target="_blank">Scenario HeadQuarters</a><br>
+<a href="https://www.silenthunter.info" target="_blank">SilentHunter.info</a><br>
+<a href="https://www.vtemod.com" target="_blank">Vietnam: The Experience</a><br>
 </p>
 
 </section>
