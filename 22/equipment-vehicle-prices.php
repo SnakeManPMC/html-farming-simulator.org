68a292da0a6cb66b3733b7ecb282e219d46688cd
Snake Man <snakeman@phnet.fi>
2022-09-17 13:56:50 +0000

    FS22 equipment and vehicle prices updated

--- 22/equipment-vehicle-prices.php
+++ 22/equipment-vehicle-prices.php
@@ -4,7 +4,6 @@
 <title>Equipment and Vehicle Prices FS22 - PMC Farming Simulator</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
 <META name="description" content="Equipment and Vehicle Prices FS22 - PMC Farming Simulator">
-<META name="keywords" content="Equipment, Vehicles, Prices, FS22, PMC, Farming, Simulator">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -24,31 +23,31 @@
 	<h2>Equipment and Vehicle Prices FS22</h2>
 
 <p>
-<b>2021-11-20T15:51:00Z</b> updated. This page is SPECULATION until after 2021-11-21T23:00:00Z when we get to activate the game and try this info ourselves.
+<b>2022-09-17T13:49:00Z</b> updated.
 </p>
 
 
 	<h2>Cheapest</h2>
 
 <p>
-Cheapest vehicles list for Start From Zero or otherwise money limited careers (I don't do from zero anymore btw, hah). Make sure you understand that these are the cheapest, smallest and slowest, the most painful vehicles you could ever buy heh.
+Cheapest vehicles list for Start From Zero or otherwise money limited careers. Make sure you understand that these are the cheapest, smallest and slowest, the most painful vehicles you could ever buy heh.
 </p>
 
 <p>
 Cheapest:
 </p>
 <pre>
-- plow PMCTODO
-- cultivator PMCTODO
-- tractor PMCTODO
-- seeder PMCTODO
-- sprayer PMCTODO
-- spreader PMCTODO
-- harvester PMCTODO
-- header PMCTODO
-- trailer PMCTODO
+- plow 160hp 2.5m 12km/h agro masz pov 5 xl 16k
+- cultivator 160hp 4m 15km/h amazone cenio 4000 super 19.5k
+- tractor 175hp 50km/h new holland t6.175 series with engine upgrade 127k
+- seeder 165hp 3m 12km/h amazone kg 3001 + centaya 3000 42k
+- sprayer 1.1k liter 24m 12km/h hardi mega 1200l 34.5k + 1.5k liter hardi mega 1200l tank 5k == 39.5k
+- spreader 9k liters 12m 18km/h bredal k105 45k
+- harvester 4.9k liters 20km/h rostselmash nova 330 75k
+- header 5m 10km/h rostselmash power stream 500 25k + 3.4m 10km/h ziegler corn champion 5r 19k == 44k
+- trailer 8k liters welger dk 115 13k
 ===
-k
+421k
 </pre>
 
 	<h2>Medium</h2>
@@ -61,17 +60,17 @@ Medium vehicle fleet is something you would upgrade to or perhaps buy with farm-
 Medium:
 </p>
 <pre>
-- plow PMCTODO
-- cultivator PMCTODO
-- tractor PMCTODO
-- seeder PMCTODO
-- sprayer PMCTODO
-- spreader PMCTODO
-- harvester PMCTODO
-- header PMCTODO
-- trailer PMCTODO
+- plow 220hp 3.5m 12km/h kverneland ecomat 23.5k
+- cultivator 225hp 5m 15km/h lemken karat 12/500 kua 47.5k
+- tractor 246hp 50km/h fendt 700 vario with 724 engine upgrade 209.5k
+- seeder 240hp 8m 18km/h vaderstad rapid a 800s 159.5k
+- sprayer 4k liters 27m 12km/h hardi navigator 4000 delta force 54.5k
+- spreader 14k liters 18m 18km/h bredal k105 with big extension and 6m spreading unit 54.3k
+- harvester 9.3k liters 30km/h new holland ch7.70 267.5k
+- header 8.5m 10km/h new holland varifeed 28ft 48k + 6m 10km/h new holland 980cr 8-30 53k == 101k
+- trailer 12.6k liters claas carat 140 td 17.5k
 ===
-m
+934.8k
 </pre>
 
 	<h2>Expensive</h2>
@@ -81,29 +80,34 @@ Now this is where we start to have some fun and do large scale farming with bigg
 </p>
 
 <p>
-When you got to do big boy large scale farming you absolutely should get some mods which increase your farming capability big time.
+When you got to do big boy large scale farming you absolutely should get some addons / mods which increase your farming capability big time.
 </p>
 
 <p>
 Expensive:
 </p>
 <pre>
-- plow PMCTODO
-- cultivator PMCTODO
-- tractor PMCTODO
-- seeder PMCTODO
-- sprayer PMCTODO
-- spreader PMCTODO
-- harvester PMCTODO
-- header PMCTODO
-- trailer PMCTODO
-- truck PMCTODO
+- plow 360hp 6m 12km/h kverneland pw 100-12 75k
+- cultivator 500hp 15.7m 15km/h farmet kompaktomat 1570 max 129k
+- tractor 517hp 60km/h fendt 1000 vario with 1050 engine upgrade and twin wheels 378.5k
+- seeder 7k liters 350hp 18.2m 18km/h kinze 4905 blue drive 249.5k
+- sprayer 9k liters 48.5m 25km/h hardi rubicon 9000 489k
+- spreader 18.9k liters 18m 18km/h bredal k165 with extension and 6m spreading unit 88.5k
+- harvester 18k liters 40km/h claas lexion 8900 with big configuration 496.3k
+- header 13.8m 10km/h claas convio flex 1380 90k + 9m 10km/h claas corio 1275 c 79.5k == 169.5k
+- trailer 400hp 77.5k liters demco 2200 dual auger grain cart 115k
+- truck 500hp 80km/h mack super-liner 6x4 110k
+- truck trailer 40k liters wilson pacesetter 59.5k
 ===
-m
+2359.8m
 </pre>
 
 <p>
-Do yourself a favor and download mods (once they are released hehe). Then various free user made mods for plows, tractors, trucks, trailers etc you name it. With mods possibilities are endless.
+Do yourself a favor and download <a href="mods.php">addons / mods</a>. There are various free user made mods for plows, tractors, trucks, trailers etc you name it. With mods possibilities are endless.
+</p>
+
+<p>
+Most useful mod equipment on the expensive list would definitely be Midwest Durus 60ft combine header, then perhaps a proper american plow / ripper / field cultivator, also several higher grain tank capacity combines with american configuration has been released. So many options.
 </p>
 
 </section>
