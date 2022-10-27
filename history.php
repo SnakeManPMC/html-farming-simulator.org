bb338a50fbe863ac723d2a3eb441abfcc97968ca
Snake Man <snakeman@phnet.fi>
2022-10-27 01:58:05 +0000

    FS22 history v1.8.1 update added

--- history.php
+++ history.php
@@ -80,6 +80,7 @@
 2022-08-23 Farming Simulator 22 - Vermeer Pack released <a href="https://www.farming-simulator.com/newsArticle.php?news_id=374" target="_blank">news</a>, <a href="https://www.farming-simulator.com/dlc-detail.php?dlc_id=fs22vermeer target="_blank">details</a><br>
 2022-09-06 Farming Simulator 22 - v1.7.1 update released <a href="https://www.farming-simulator.com/newsArticle.php?news_id=408" target="_blank">news</a>, <a href="https://www.farming-simulator.com/changelog-fs22.php" target="_blank">changelog</a><br>
 2022-09-27 Farming Simulator 22 - Pumps and Hoses Pack released <a href="https://www.farming-simulator.com/newsArticle.php?news_id=389" target="_blank">news</a><br>
+2022-10-26 Farming Simulator 22 - v1.8.1 update released <a href="https://www.farming-simulator.com/newsArticle.php?news_id=421" target="_blank">news</a>, <a href="https://www.farming-simulator.com/changelog-fs22.php" target="_blank">changelog</a><br>
 </p>
 
 </section>
