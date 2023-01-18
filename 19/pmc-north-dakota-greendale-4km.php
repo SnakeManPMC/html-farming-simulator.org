221c026e4ab3948da14baac0fa4d43ffdc284dfe
Snake Man <snakeman@phnet.fi>
2023-01-18 06:07:57 +0000

    North dakota greendale 4km screenshots page created

--- 19/pmc-north-dakota-greendale-4km.php
+++ 19/pmc-north-dakota-greendale-4km.php
@@ -4,7 +4,6 @@
 <title>North Dakota Greendale 4km Farming Simulator 19 - PMC Farming</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
 <META name="description" content="North Dakota Greendale 4km Farming Simulator 19 - PMC Farming">
-<META name="keywords" content="Terrain, North Dakota Greendale 4km, PMC, Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -37,58 +36,11 @@ PMC North Dakota Greendale 4km <a href="https://www.google.com/maps/@46.0182883,
 </p>
 
 <div>
-<p style="font-size: 10px;">
-Click for larger image
+<p>
+Click map image for more <a href="screenshots-pmc-north-dakota-greendale-4km.php" target="_blank">screenshots</a>.
 </p>
-<a href="images/PMC-North-Dakota-Greendale-Satellite-PDA.jpg" target="_blank"><img src="images/thumbs/PMC-North-Dakota-Greendale-Satellite-PDA.jpg" class="terrainimg" loading="lazy" alt="Farming Simulator 19 PMC Terrain Screenshot"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-Fields.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Fields.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Fields"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-Farmland.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Farmland.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Farmland"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-Soil-Types-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-Soil-Types-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming Soil Types"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-pH-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-pH-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming pH"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-Nitrogen-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-Nitrogen-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming Nitrogen"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-Economic-Analysis-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-Economic-Analysis-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming Economic Analysis"></a>
-
-<a href="images/PMC-North-Dakota-Greendale-4km-TestRunner-2021-01-10T0007.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-TestRunner-2021-01-10T0007.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. TestRunner Results" style="padding: 3px 15px;width:auto;height:128px;"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2339.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2339.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2340.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2340.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2341.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2341.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234221.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234221.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234242.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234242.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234313.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234313.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234345.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234345.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
-
-<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2344.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2344.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+<a href="screenshots-pmc-north-dakota-greendale-4km.php" target="_blank"><img src="images/thumbs/PMC-North-Dakota-Greendale-Satellite-PDA.jpg" class="terrainimg" loading="lazy" alt="Farming Simulator 19 PMC Terrain Screenshot"></a>
 </div>
-
-<div>
-<p>
-Latest PMC North Dakota Greendale 4km video.
-</p>
-<iframe width="480" height="270"
-src="https://www.youtube.com/embed/iOeoS5fdR08">
-</iframe>
-</div>
-
-<p>
-Screenshots from development 2019-02-20 (now deleted terrain): fields rough foliage layer outline done <a href="https://i.imgur.com/KvM6Njq.png" target="_blank">KvM6Njq.png</a>, first look of real world terrain 4km <a href="https://i.imgur.com/BtFuP80.jpg" target="_blank">BtFuP80.jpg</a> and some game-play <a href="https://i.imgur.com/SI5BzW2.jpg" target="_blank">SI5BzW2.jpg</a>
-</p>
-
-<p>
-2019-05-07 Re-created terrain in-game <a href="https://cdn.discordapp.com/attachments/557685453801914379/575061093836128310/FS19-North-Dakota-Greendale-4km-2019-05-06T2045.png" target="_blank">FS19-North-Dakota-Greendale-4km-2019-05-06T2045.png</a>. Gimp painting cultivator_density image <a href="https://cdn.discordapp.com/attachments/557685453801914379/575080387064102912/FS19-North-Dakota-Greendate-4km-2019-05-06T2201.jpg" target="_blank">FS19-North-Dakota-Greendate-4km-2019-05-06T2201.jpg</a>.
-</p>
 
 	<h2>Background</h2>
 
@@ -315,6 +267,7 @@ For installing please read <a href="install-mods.php">how to install FS19 mods</
 <footer>
 <p>
 <a href="pmc-terrains.php" class="button">PMC Terrains FS19</a>
+<a href="screenshots-pmc-north-dakota-greendale-4km.php" class="button">Screenshots PMC North Dakota Greendale 4km</a>
 <a href="../index.php" class="button">PMC Farming Simulator root page</a>
 </p>
 <p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
---
+++ 19/screenshots-pmc-north-dakota-greendale-4km.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Screenshots PMC North Dakota Greendale 4km Farming Simulator 19 - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Screenshots PMC North Dakota Greendale 4km Farming Simulator 19 - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<!-- google ads -->
+<?php include("../include/google-ads.php"); ?>
+<h1><b>Screenshots PMC North Dakota Greendale 4km - Farming Simulator 19 Terrain</b></h1>
+</header>
+
+<section>
+<div class="terrain">
+<p><b>2023-01-18T05:45:00Z</b> Updated. Please read <a href="pmc-terrains.php">PMC Terrains</a> page for background information.</p>
+
+	<h2>Screenshots PMC North Dakota Greendale 4km</h2>
+
+<div>
+<p style="font-size: 10px;">
+Click for larger image
+</p>
+<a href="images/PMC-North-Dakota-Greendale-Satellite-PDA.jpg" target="_blank"><img src="images/thumbs/PMC-North-Dakota-Greendale-Satellite-PDA.jpg" class="terrainimg" loading="lazy" alt="Farming Simulator 19 PMC Terrain Screenshot"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-Fields.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Fields.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Fields"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-Farmland.jpg" target="_blank"><img class="terrainimg" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Farmland.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Farmland"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-Soil-Types-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-Soil-Types-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming Soil Types"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-pH-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-pH-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming pH"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-Nitrogen-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-Nitrogen-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming Nitrogen"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-Precision-Farming-Economic-Analysis-2021-01-09.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-Precision-Farming-Economic-Analysis-2021-01-09.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Precision Farming Economic Analysis"></a>
+
+<a href="images/PMC-North-Dakota-Greendale-4km-TestRunner-2021-01-10T0007.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="images/thumbs/PMC-North-Dakota-Greendale-4km-TestRunner-2021-01-10T0007.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. TestRunner Results" style="padding: 3px 15px;width:auto;height:128px;"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2339.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2339.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2340.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2340.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2341.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2341.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234221.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234221.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234242.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234242.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234313.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234313.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T234345.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T234345.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+
+<a href="screenshots/PMC-North-Dakota-Greendale-4km-2021-01-09T2344.jpg" target="_blank"><img class="terrainimg" loading="lazy" src="screenshots/thumbs/PMC-North-Dakota-Greendale-4km-2021-01-09T2344.jpg" alt="Farming Simulator 19 Terrain - Greendale, North Dakota, USA. Landscape"></a>
+</div>
+
+<div>
+<p>
+Latest PMC North Dakota Greendale 4km video.
+</p>
+<iframe width="480" height="270"
+src="https://www.youtube.com/embed/iOeoS5fdR08">
+</iframe>
+</div>
+
+<p>
+Plenty of <a href="https://www.youtube.com/user/pmcsince1984/search?query=%22PMC%20North%20Dakota%20Greendale%204km%22" target="_blank">youtube PMC North Dakota Greendale 4km development videos</a> available.
+</p>
+
+<p>
+Screenshots from development 2019-02-20 (now deleted terrain): fields rough foliage layer outline done <a href="https://i.imgur.com/KvM6Njq.png" target="_blank">KvM6Njq.png</a>, first look of real world terrain 4km <a href="https://i.imgur.com/BtFuP80.jpg" target="_blank">BtFuP80.jpg</a> and some game-play <a href="https://i.imgur.com/SI5BzW2.jpg" target="_blank">SI5BzW2.jpg</a>
+</p>
+
+<p>
+2019-05-07 Re-created terrain in-game <a href="https://cdn.discordapp.com/attachments/557685453801914379/575061093836128310/FS19-North-Dakota-Greendale-4km-2019-05-06T2045.png" target="_blank">FS19-North-Dakota-Greendale-4km-2019-05-06T2045.png</a>. Gimp painting cultivator_density image <a href="https://cdn.discordapp.com/attachments/557685453801914379/575080387064102912/FS19-North-Dakota-Greendate-4km-2019-05-06T2201.jpg" target="_blank">FS19-North-Dakota-Greendate-4km-2019-05-06T2201.jpg</a>.
+</p>
+
+</div>
+
+</section>
+
+<footer>
+<p>
+<a href="pmc-terrains.php" class="button">PMC Terrains FS19</a>
+<a href="pmc-north-dakota-greendale-4km.php" class="button">PMC North Dakota Greendale 4km</a>
+<a href="../index.php" class="button">PMC Farming Simulator root page</a><br>
+<i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i>
+</p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
