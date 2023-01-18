f2065beda2f5e671f8110ec2129f842194acaf61
Snake Man <snakeman@phnet.fi>
2023-01-18 05:42:54 +0000

    Texas rowena screenshots page created

--- 19/pmc-texas-rowena-8km.php
+++ 19/pmc-texas-rowena-8km.php
@@ -38,34 +38,10 @@ PMC Texas Rowena 8km <a href="https://www.google.com/maps/@31.628138,-100.007363
 </p>
 
 <div>
-<p style="font-size: 10px;">
-Click for larger image
+<p>
+Click map image for more <a href="screenshots-pmc-texas-rowena-8km.php" target="_blank">screenshots</a>.
 </p>
-<a href="images/PMC-Texas-Rowena-Satellite-PDA.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-Satellite-PDA.jpg" class="terrainimg" loading="lazy" alt="Farming Simulator 19 PMC Terrain Screenshot"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-Fields.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-Fields.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Fields" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-Farmland.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-Farmland.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Farmland" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-Precision-Farming-Soil-Types-2020-12-31.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-Precision-Farming-Soil-Types-2020-12-31.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Precision Farming Soil Types" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-Precision-Farming-pH-2020-12-31.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-Precision-Farming-pH-2020-12-31.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Precision Farming pH" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-Precision-Farming-Nitrogen-2020-12-31.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-Precision-Farming-Nitrogen-2020-12-31.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Precision Farming Nitrogen" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-Precision-Farming-Economic-Analysis-2020-12-31.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-Precision-Farming-Economic-Analysis-2020-12-31.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Precision Farming Economic Analysis" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="images/PMC-Texas-Rowena-8km-TestRunner-2020-12-31T0039.jpg" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-8km-TestRunner-2020-12-31T0039.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. TestRunner Results" loading="lazy" style="padding: 3px 15px;width:auto;height:128px;"></a>
-
-<a href="screenshots/PMC-Texas-Rowena-8km-2020-12-31T2216.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Texas-Rowena-8km-2020-12-31T2216.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Landscape" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="screenshots/PMC-Texas-Rowena-8km-2020-12-31T2221.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Texas-Rowena-8km-2020-12-31T2221.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Landscape" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="screenshots/PMC-Texas-Rowena-8km-2020-12-31T2233.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Texas-Rowena-8km-2020-12-31T2233.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Landscape" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="screenshots/PMC-Texas-Rowena-8km-2020-12-31T2234.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Texas-Rowena-8km-2020-12-31T2234.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Landscape" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
-
-<a href="screenshots/PMC-Texas-Rowena-8km-2020-12-31T2241.jpg" target="_blank"><img src="screenshots/thumbs/PMC-Texas-Rowena-8km-2020-12-31T2241.jpg" alt="Farming Simulator 19 Terrain - Rowena, Texas, USA. Landscape" loading="lazy" style="padding: 3px 15px;width:auto;height:auto;"></a>
+<a href="screenshots-pmc-texas-rowena-8km.php" target="_blank"><img src="images/thumbs/PMC-Texas-Rowena-Satellite-PDA.jpg" class="terrainimg" loading="lazy" alt="Farming Simulator 19 PMC Terrain Screenshot"></a>
 </div>
 
 <div>
@@ -76,28 +52,6 @@ Latest PMC Texas Rowena 8km video.
 src="https://www.youtube.com/embed/37rGbHaz2lg">
 </iframe>
 </div>
-
-<p>
-Screenshots from development: <a href="https://cdn.discordapp.com/attachments/557685453801914379/576695192023728157/FS19-Texas-Rowena-8km-2019-05-11T0858.png" target="_blank">Gimp cultivator density</a>, <a href="https://cdn.discordapp.com/attachments/557685453801914379/576931181166329857/FS19-Texas-Rowena-8km-2019-05-12T0035.png" target="_blank">cultivated land in-game</a>, <a href="https://cdn.discordapp.com/attachments/557685453801914379/576981214041341965/FS19-Texas-Rowena-8km-2019-05-12T0355.png" target="_blank">fields states in-game</a>. <a href="https://cdn.discordapp.com/attachments/557685453801914379/695250080814596177/PMC-Texas-Rowena-8km-2020-04-02T1138.png" target="_blank">Objects placed on 2020-04-02T1138</a> and <a href="https://cdn.discordapp.com/attachments/557685453801914379/697164449022476328/PMC-Texas-Rowena-8km-2020-04-07T1920.png" target="_blank">Object placement 2020-04-07</a>.
-<br><br>
-Screenshots from game-play 2020:<br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/695278712739856415/PMC-Texas-Rowena-8km-2020-04-02T1424.png" target="_blank">Object placement inspection 2020-04-02T1424</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/697242052429217842/PMC-Texas-Rowena-8km-2020-04-08T0027.png" target="_blank">Farmlands painted in GIMP 2020-04-08</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/697252616173912084/PMC-Texas-Rowena-8km-2020-04-08T0110.jpg" target="_blank">LOD view distance settings (game.xml) of 5, increases how far objects are drawn, in this case trees</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/697300062253875230/PMC-Texas-Rowena-8km-2020-04-08T0408.png" target="_blank">Game.xml LOD view distance values 5, 2020-04-08</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/697928628800847912/PMC-Texas-Rowena-8km-2020-04-09T2134.png" target="_blank">Birds eye view of seeding 2020-04-09</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/697928681276047370/PMC-Texas-Rowena-8km-2020-04-09T2155.jpg" target="_blank">Lurker view of seeding 2020-04-09</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/698760589631029278/PMC-Texas-Rowena-8km-2020-04-12T0500.jpg" target="_blank">Big Bud 450 with John Deere 2623</a><br>
-<a href="https://cdn.discordapp.com/attachments/557685453801914379/698786425457606656/PMC-Texas-Rowena-8km-2020-04-12T0642.jpg" target="_blank">John Deere 9RX with 2680H High Performance disk</a>.
-</p>
-
-<p>
-Video: <a href="https://www.youtube.com/watch?v=uoPcKmqCrF4" target="_blank">Terrain WIP Tour 2020-04-08</a>.
-</p>
-
-<p>
-Fields 120, 123, 127, 141, 145, <a href="screenshots/PMC-Texas-Rowena-8km-2020-04-13T0424.jpg" target="_blank">148 and 149 have utility poles</a> going through, see <a href="https://www.google.com/maps/@31.5788321,-100.0020704,237m/data=!3m1!1e3" target="_blank">google/maps reference</a>. This is realistic feature, terrain has been built like it is in real world, utility poles have geometry so vehicles will collide with them however cultivator_density and field definitions have been edited to create empty spot around utility poles which courseplay avoids. See <a href="https://www.youtube.com/watch?v=-4xmqo6cXq4" target="_blank">Courseplay on PMC Texas Rowena 8km field 145, avoiding utility poles. 2020-07-31</a> video of how it works.
-</p>
 
 	<h2>Work In Progress</h2>
 
@@ -417,6 +371,7 @@ For installing please read <a href="install-mods.php">how to install FS19 mods</
 <footer>
 <p>
 <a href="pmc-terrains.php" class="button">PMC Terrains FS19</a>
+<a href="screenshots-pmc-texas-rowena-8km.php" class="button">Screenshots PMC Texas Rowena 8km</a>
 <a href="../index.php" class="button">PMC Farming Simulator root page</a><br>
 <i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i>
 </p>
