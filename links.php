af1dc6184b60cf1f7e9a93af7f840311869324ce
Snake Man <snakeman@phnet.fi>
2024-02-21 00:22:02 +0000

    Links page updated - descriptions of our sites added - fsasmc.com link and description added

--- links.php
+++ links.php
@@ -3,8 +3,7 @@
 <head>
 <title>Farming Simulator Links - PMC Farming</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator - PMC Farming">
-<META name="keywords" content="Farming Simulator, PMC, Farming">
+<META name="description" content="Farming Simulator Links - PMC Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,21 +14,23 @@
 <body>
 
 <header>
-<!-- google ads -->
-<?php include("include/google-ads.php"); ?>
 	<h1><b>PMC Farming Simulator Links</b></h1>
 </header>
 
 <section>
-	<h2>PMC Site Links</h2>
-<p>
-<a href="https://www.elitedangerous.net" target="_blank">EliteDangerous.net</a><br>
-<a href="https://www.pmctactical.org" target="_blank">PMC Tactical</a><br>
-<a href="https://pmc.editing.wiki" target="_blank">PMC Editing Wiki</a><br>
-<a href="https://www.scenariohq.com" target="_blank">Scenario HeadQuarters</a><br>
-<a href="https://www.silenthunter.info" target="_blank">SilentHunter.info</a><br>
-<a href="https://www.vtemod.com" target="_blank">Vietnam: The Experience</a><br>
-</p>
+	<h2>PMC Web Site Links</h2>
+
+<p><a href="https://www.elitedangerous.net" target="_blank">EliteDangerous.net</a>, Elite Dangerous space exploration game.</p>
+<p><a href="https://www.pmctactical.org" target="_blank">PMC Tactical</a>, Falcon 4.0, Operation Flashpoint, ArmA 1 (Armed Assault), ArmA 2, ArmA 3, addons/mods.</p>
+<p><a href="https://pmc.editing.wiki" target="_blank">PMC Editing Wiki</a>, ofp/arma and Falcon 4.0 editing tutorials.</p>
+<p><a href="https://www.scenariohq.com" target="_blank">Scenario HeadQuarters</a>, ofp/arma missions and campaigns.</p>
+<p><a href="https://www.silenthunter.info" target="_blank">SilentHunter.info</a>, Silent Hunter 3, Silent Hunter 4, Silent Hunter 5 (SH5) world world 2 submarine simulator.</p>
+<p><a href="https://www.vtemod.com" target="_blank">Vietnam: The Experience</a>, the leading and most detailed Vietnam War Modifications for PC games.</p>
+
+
+	<h2>Friends Of Ours Web Site Links</h2>
+
+<p><a href="https://www.fsasmc.com" target="_blank">fsasmc.com</a>, Farming Simulator American Style Community</p>
 
 </section>
 
