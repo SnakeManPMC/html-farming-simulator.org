e1b2289e9a1b741c300eae877fb854cf7e9a188e
Snake Man <snakeman@phnet.fi>
2024-03-03 14:28:47 +0000

    FS22 guide production chains updated

--- 22/guide-production-chains.php
+++ 22/guide-production-chains.php
@@ -4,7 +4,6 @@
 <title>Farming Simulator 22 Playing Guide Production Chains - PMC Farming</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
 <META name="description" content="Farming Simulator 22 Playing Guide Production Chains - PMC Farming">
-<META name="keywords" content="Farming, Simulator, FS22, Guide, Production, Chains, Tutorial, PMC, Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,8 +14,6 @@
 <body>
 
 <header>
-<!-- google ads -->
-<?php include("../include/google-ads.php"); ?>
 <h1><b>PMC Farming Simulator 22 Playing Guide Production Chains</b></h1>
 </header>
 
@@ -25,11 +22,11 @@
 	<h2>Playing Guide Production Chains</h2>
 
 <p>
-<b>2021-11-20T21:20:00Z</b> updated. This page is SPECULATION until after 2021-11-21T23:00:00Z when we get to activate the game and try this info ourselves.
+<b>2024-03-03</b> updated.
 </p>
 
 <p>
-Back to <a href="guide.php">Farming Simulator 22 Playing Guide</a> page.
+Join discussion in <a href="https://www.pmctactical.org/forum/viewtopic.php?f=79&t=22940" target="_blank">PMC Tactical Forum: Production Chains Playing Guide FS22</a>, post feedback if you see any errors in this guide. Back to <a href="guide.php">Farming Simulator 22 Playing Guide</a> page.
 </p>
 
 
@@ -40,20 +37,66 @@ Production chains are good old factories, where you produce your own goods. Ther
 </p>
 
 <p>
-<b>PMCTODO</b> Unfortunately this subject is very difficult to document just by looking at twitch live streams so more indepth writing happens as mentioned above when game activates for us.
+Buy cereal factory. Construction menu then production and create placeable factory.
 </p>
 
-<!--
-Buy cereal factory.
+<p>
+Buy grain mill. Dump grain to grain mill.
+</p>
 
-Construction menu then production and create placeable factory.
-
-Buy grain mill.
-
-Dump grain to grain mill.
-
+<p>
 Change output mode in bottom of the production menu. Storing, selling, distributing.
--->
+</p>
+
+<p>
+Distributing mode. On top of the hour any "distributing mode" materials are sent through the network towards other production points that use them (not sure if there is an order, or is everyone getting their share evenly).
+</p>
+
+<p>
+Only easy job production chains are grain mill, oil mill and bakery. Grain mill generates flour which can be sold or sent to bakery. Bakery takes flour and produces bread which can be sold with quite nice profit. Oil mill takes canola grain and produces canola oil which can be sold. All the other production buildings require fancy stuff like animals, grapes or sugar beets etc, not to mention logging for planks etc. Make no mistake, production chains are a grind, good way to generate game-play, something to do for those folks who are into factories. Production chains for making fertilizer, herbicide, seeds, flour, bread and canola oil are perfectly fine though.
+</p>
+
+<p>
+Cake is I believe the most expensive production chain product available, its done in bakery. Ingredients are: strawberries, eggs, sugar, butter and flour. Stuff you need for all these ingredients: chicken coop (eggs), water (greenhouse), greenhouse (strawberries), sugar mill (sugar), cows (milk for dairy), dairy (butter) and grain mill (flour), uuh wonder if I forgot any placeable hehe. So as you can see, its a convoluted mess, but certainly still within american farming realm as opposed to cereal factory which requires grapes and grape processing unit.
+</p>
+
+<p>
+Name of this game is "Farming Simulator", its not "production chains simulator", you have to consider these type placeables as bonus features, not the core game-play.
+</p>
+
+<p>
+Its important to understand that just because giants has provided players with all kinds of production chains placeable buildings, it doesn't mean you're missing out if you're not using them all, as said for example grapes and logging is not really that traditional large scale american farming, if you don't like particular production chain type then for petes sake don't use it, its that simple.
+</p>
+
+<p>
+Production chains: All products, production plants and connections <a href="https://www.farming-simulator.com/cms/uploads/news_61570ac9a8b10.jpg" target="_blank">Giants news_61570ac9a8b10.jpg</a> and icon overview <a href="https://www.farming-simulator.com/cms/uploads/news_616686689e68f.jpg" target="_blank">Giants news_616686689e68f.jpg</a>.
+</p>
+
+
+	<h2>Productions With Animals</h2>
+
+<p>
+If you have dairy cows, then hauling milk to production chains dairy is very easy, all you need is lizard MKS8 with tractor or MKS32 with semi-truck. Production chains dairy would then take milk, produce butter and cheese which you could just sell right away with nice profit. This is surprisingly simple, believable realistic and "fun" production chain. Wonder why I never thought of that before (perhaps the cows were the big hurtle).
+</p>
+
+<p>
+If you have sheep barn, haul wool to spinnery production chain building, it produces fabric which you can sell, or if you have tailor shop production chain building it can produce clothes which can be sold for a nice profit. This is surprisingly simple, believable realistic and "fun" production chain. However you do need a quite few sheep in the barn to shed enough wool to keep that spinnery running 24/7. I started with just a few 0 month olds, it took forever for them to reproduce and then with 32-50 sheep, not sure if there is enough wool source material for spinnery. If you want a sure way to keep spinnery and tailor shop printing money for you, buy full large sheep barn with 8 month or older sheeps (not 0 month lambs).
+</p>
+
+
+	<h2>Separate Products</h2>
+
+<p>
+HOWTO separate cheese and chocolate in dairy and bread and cake in bakery? You should separate these two products from each other on each production chain building because they are not sold at the highest price at the same time, most often you are using autoloading trailer to haul these pallets for sell and this way you get two different products in each trailer load. You could use forklift or telehandler to separate the pallets, but honestly having been done it just once I can already say that it gets old REAL fast, its just so annoying to work with telehandler to pick individual pallets on a big pile.
+</p>
+
+<p>
+Other more automated but expensive and farmland space requiring option is to download a pallet warehouse mod of some kind, put all the productions there using autoloading trailer (or perhaps it connects to production chains network and automatically "downloads" products), then just pick up the product type that is on highest market price when you need to. Most likely pallet warehouse is the best option considering the bigger picture, it also helps you to get rid of huge piles of product near production chain buildings or where ever you decide to store them temporarily.
+</p>
+
+<p>
+Remember; each pallet is considered to be a vehicle in farmsim engine, its not a good idea to store hundreds of pallets on the ground, in fact there was some pallet limit per savegame I believe. Another rather simplistic cold approach for this is to sell bread and cake when cake has the highest price as its the more expensive one, same with cheese and chocolate, sell when chocolate market price is high (there is not that much difference btw, not like bread and cake do).
+</p>
 
 </section>
 
