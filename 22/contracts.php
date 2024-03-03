a13471d9b66967be5f3ffede0e2f2b9041cb30e5
Snake Man <snakeman@phnet.fi>
2024-03-03 15:01:04 +0000

    FS22 contracts page updated

--- 22/contracts.php
+++ 22/contracts.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Farming Simulator 22 Contracts - PMC Farming</title>
+<title>Farming Simulator 22 Playing Guide Contracts - PMC Farming</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator 22 Contracts - PMC Farming">
-<META name="keywords" content="Farming, Simulator, FS22, Contracts, Missions, PMC, Farming">
+<META name="description" content="Farming Simulator 22 Playing Guide Contracts - PMC Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,34 +14,69 @@
 <body>
 
 <header>
-<!-- google ads -->
-<?php include("../include/google-ads.php"); ?>
-<h1><b>PMC Farming Simulator 22 Contracts</b></h1>
+<h1><b>PMC Farming Simulator 22 Playing Guide Contracts</b></h1>
 </header>
 
 <section>
 	<h2>Contracts Overview</h2>
 
 <p>
-<b>2021-11-20T15:51:00Z</b> updated. This page is SPECULATION until after 2021-11-21T23:00:00Z when we get to activate the game and try this info ourselves.
+<b>2024-03-03</b> updated.
 </p>
 
 <p>
-Contracts, or missions as they were called in FS17.
+Join discussion in <a href="https://www.pmctactical.org/forum/viewtopic.php?f=79&t=22941" target="_blank">PMC Tactical Forum: Contracts Playing Guide FS22</a>, feel free to post feedback about this page if you find any errors etc issue.
+</p>
+
+<p>
+Contracts, or missions as they were called in Farming Simulator 17 (FS17). See also <a href="../19/contracts.php">Farming Simulator 19 (FS19) Guide: Contracts</a> for basic information.
 </p>
 
 <p>
 In Farming Simulator 22 (FS22) you can now by default have three (3) contracts running at the same time (without mods).
 </p>
 
-</section>
-<!--
 <p>
+You should definitely get <a href="https://github.com/Mmtrx/FS22_BetterContracts" target="_blank">FS22 Better Contracts mod</a> which improves how contracts are handled, this is pretty much essential mod for anyone who does more than few contracts.
 </p>
--->
+
+<p>
+Better Contracts fixes the harvesting contracts which giants broke in some game engine patch, you have to adjust the percentage to deliver in order to accomplish harvest contracts.
+</p>
+
+<p>
+Better Contracts adds icons to the ESC mapview on fields that have contract, it even includes icon type so you will recognize immediately cultivating, harvesting and fertilizing etc contract types.
+</p>
+
+
+	<h2>Accepting Contract</h2>
+
+<p>
+Press ESC to get into main FS22 menu. Then browse the horizontal GUI menu down to the icon which looks like two pieces of paper tilted next to each other (ugh my descriptions).
+</p>
+
+<img src="screenshots/thumbs/PMC-King-Corn-45km-2022-01-29T1547.jpg" alt="PMC King Corn 45km Contracts Menu Screenshot Farming Simulator 22 (FS22)">
+
+<p>
+You can use keyboard arrow keys or mouse scroll wheel to browse contract list up and down.
+</p>
+
+<p>
+When you want to accept contract with borrowed vehicles press BACKSPACE button or click the "Borrow Items" button/icon/text at bottom of the screen. If you want to accept contract without borrowed vehicles then simply press SPACEBAR or click the "Accept Contract" thingy.
+</p>
+
+<p>
+If you took borrowed vehicles they will appear at the vehicle shop aka the dealership yard. Once you have finished the contract and collect contract reward the vehicles (and any product in their tanks) will disappear, so its a good idea to get out of the vehicle first and possibly empty seed, fertilizer etc tank from any product. If you don't empty tanks then you are reimbursed money for that product.
+</p>
+
+<p>
+Once you collect contract reward money is added to your account.
+</p>
+
+</section>
 
 <footer>
-<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><a href="guide.php" class="button">Playing Guides FS22 root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
 <p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
 <?php include("../include/w3-validator-logo.php"); ?>
 </footer>
