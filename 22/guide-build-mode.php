612aa20ad82bdc63eebe67c425e42ce40ee63ba9
Snake Man <snakeman@phnet.fi>
2024-03-03 16:55:59 +0000

    FS22 guide build-mode updated

--- 22/guide-build-mode.php
+++ 22/guide-build-mode.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Farming Simulator 22 Playing Guide Build Mode - PMC Farming</title>
+<title>Farming Simulator 22 (FS22) Playing Guide Build Mode - PMC Farming</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator 22 Playing Guide Build Mode - PMC Farming">
-<META name="keywords" content="Farming, Simulator, FS22, Guide, Build, Tutorial, PMC, Farming">
+<META name="description" content="Farming Simulator 22 (FS22) Playing Guide Build Mode - PMC Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,28 +14,30 @@
 <body>
 
 <header>
-<!-- google ads -->
-<?php include("../include/google-ads.php"); ?>
-<h1><b>PMC Farming Simulator 22 Playing Guide Build Mode</b></h1>
+<h1>PMC Farming Simulator 22 (FS22) Playing Guide Build Mode</h1>
 </header>
 
 <section>
 
-	<h2>Playing Guide Build Mode</h2>
+	<h2>FS22 Playing Guide Build Mode</h2>
 
 <p>
-<b>2021-11-20T21:28:00Z</b> updated. This page is SPECULATION until after 2021-11-21T23:00:00Z when we get to activate the game and try this info ourselves.
+<b>2024-03-03</b> updated.
 </p>
 
 <p>
-Back to <a href="guide.php">Farming Simulator 22 Playing Guide</a> page.
+Join discussion in <a href="https://www.pmctactical.org/forum/viewtopic.php?f=79&t=22943" target="_blank">PMC Tactical Forum FS22 Playing Guide: Build Mode topic</a>, where you can post any feedback like errors or other issues in this page, or you could post your own tips to share in this page.
+</p>
+
+<p>
+Back to <a href="guide.php">Farming Simulator 22 (FS22) Playing Guide</a> page.
 </p>
 
 
 	<h2>Build Mode</h2>
 
 <p>
-Build mode is feature under which placeables, landscaping and all related items are listed. Activate build mode by bringing up the SHOP menu, then bottom of the left side tool bar there are three horizontal (level) lines icon which is "Others" menu, from others menu select "Construction" page/icon thingy.
+Build mode is feature under which placeables, landscaping and all related items are listed. Activate build mode by bringing up the SHOP menu, then bottom of the left side tool bar there are three horizontal (level) lines icon which is "Others" menu, from others menu select "Construction" page/icon thingy. Keyboard shortcut is SHIFT-P.
 </p>
 
 <p>
@@ -67,8 +68,19 @@ In decoration tab are further selections for: fences, lights and others.
 In landscaping tab are further selections for: sculpting, painting, trees and plants.
 </p>
 
+
+	<h2>Recommended FS22 Mods For Build Mode</h2>
+
 <p>
-<b>PMCTODO</b> Unfortunately this subject is very difficult to document just by looking at twitch live streams so more indepth writing happens as mentioned above when game activates for us.
+Recommended mod for build mode is <b>FS22 Place Anywhere mod</b> which allows you to place objects, placeables, etc into anywhere regardless if you own the farmland or not. Also I believe there was some limitation in default game settings that you cannot place objects too close to existing terrain or placeable objects, this mod disables all such limitations.
+</p>
+
+<p>
+You should use PlaceAnywhere mod responsibly of course, don't just go spamming weird object types in middle of where-ever, make wise choices where to put your placeables and such objects with the freedom that this mod gives you, with great power comes great responsibility.
+</p>
+
+<p>
+If you like to landscape a lot, change terrain heightmap elevations, paint ground textures, place down shrubbery flowers, bushes and even trees, its highly recommended to get <b><a href="https://www.farming-simulator.com/mod.php?mod_id=225034" target="_blank">FS22 Free Landscaping Tools mod</a></b> as it will disable money being drained away from your account while landscaping. Landscaping should never cost money as game-play feature.
 </p>
 
 </section>
