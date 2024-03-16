ac24288340945c261fb7fcadd011784c78326dc0
Snake Man <snakeman@phnet.fi>
2024-03-16 18:06:19 +0000

    FS17 stories page update 3: - newlin texas all the fields page created - news page updated

---
+++ 17/stories-newlin-texas-all-fields.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Newlin Texas All The Fields Farming Simulator 17 - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Newlin Texas All The Fields Farming Simulator 17 - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- google analytics -->
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<h1>Newlin Texas All The Fields Farming Simulator 17 - PMC Farming</h1>
+</header>
+
+<section>
+	<h2>Newlin Texas All The Fields</h2>
+
+<p>
+2018-09-18 was super bored of <a href="https://www.pmctactical.org/arma3/" target="_blank">ArmA 3</a> editing as I achieved so much in the past few weeks that had to take a break. Wanted to continue Newlin Texas (beta) farming, but wasnt really committed and didnt even use Saitek FarmSim Bundle wheel.
+</p>
+
+<p>
+To recap the goals: purchase, plough, fertilize, seed and harvest every field in newlin texas. Today I own F15 through F22. I'm trying to puchase fields next to each other and if possible in sequential order (but that is often not possible as fields are scattered all over) however I want to save the massive F23 to last as its the crown jewel heh.
+</p>
+
+<p>
+Started by harvesting corn from F16.
+</p>
+
+<p>
+2019-09-18T0445 purchased F24, 20.65 hectares. It was cultivated state, need of ploughing so thats how I started.
+</p>
+
+<p>
+The field was cultivated and I was using john deere flex disk plough slash cultivator... so when working on F24 I could not see the parts I had already done and had to check them from map view heh. Should have got the traditional plough for this job.
+</p>
+
+<p>
+2019-09-18T0847 purchased F25, 6.01 hectares. This was all grass field and had bunch of trees in it which I was not interested of cutting down as you cant use scorpion king vehicle on them.
+</p>
+
+<p>
+2019-09-18T0848 purchased F14, 54.85 hectares. Same as above. But boy is it a huge field 54 hectares heh.
+</p>
+
+<p>
+2019-09-18T0850 purchased F31, 23.87 hectares. This field was cultivated so another annoying one to do with john deere flex disk.
+</p>
+
+<p>
+2019-09-18T1151 purchased F11, 55.54 hectares. This field was cultivated so another annoying one to do with john deere flex disk, but I let courseplay handle it so no problem.
+</p>
+
+<p>
+TODO
+</p>
+<ul>
+<li>clean out cutted trees from F24</li>
+<li>cut trees from F24 W edge and the central odd nudge, they just waste good field space</li>
+</ul>
+
+<p>
+Later afternoon got sick and tired of the fences, trees and telephone poles blocking my vehicles which made courseplay use impossible, so I saved and quit. Few days later got enough motivation to continue and I was still on arma3 break heh.
+</p>
+
+<p>
+F11:<br>
+2018-09-22 purchase<br>
+2018-09-22 plough<br>
+2018-09-22T1503 fertilize slurry<br>
+2018-09-22T1729 seed<br>
+* fertilize manure/spray?<br>
+</p>
+
+<p>
+F27:<br>
+2018-09-22T1200 purchase<br>
+2018-09-22T1200 plough<br>
+2018-09-22T1522 fertilize slurry<br>
+2018-09-22T1546 seed<br>
+</p>
+
+<p>
+F13:<br>
+2018-09-22T1200 purchase<br>
+2018-09-22T1200 plough<br>
+* fertilize slurry<br>
+</p>
+
+<p>
+F12:<br>
+2018-09-22T1711 purchase<br>
+2018-09-22T1747 plough<br>
+2018-09-22T1750 fertilize slurry &lt;- no need, it was fertilized already (plough over crops hehe)<br>
+2018-09-22T1832 seed<br>
+</p>
+
+<p>
+F28:<br>
+2018-09-22T1923 purchase<br>
+2018-09-22T2008 plough<br>
+2018-09-22T2008 fertilize slurry &lt;- no need, it was fertilized already (plough over crops hehe)<br>
+2018-09-22T seed<br>
+</p>
+
+<p>
+F9:<br>
+2018-09-22T2023 purchase<br>
+2018-09-22T2217 plough<br>
+2018-09-22T2217 fertilize slurry &lt;- no need, it was fertilized already (plough over crops hehe)<br>
+</p>
+
+<p>
+F32:<br>
+2018-09-22T2100 purchase<br>
+2018-09-22T2246 plough<br>
+2018-09-22T2246 fertilize slurry &lt;- no need, it was fertilized already (plough over crops hehe)<br>
+</p>
+
+<p>
+F29:<br>
+2018-09-22T2043 purchase<br>
+2018-09-22T2336 plough<br>
+2018-09-22T2336 fertilize slurry &lt;- no need, it was fertilized already (plough over crops hehe)<br>
+</p>
+
+<p>
+F41:<br>
+2018-09-23T1014 purchase<br>
+2018-09-23T1028 plough<br>
+</p>
+
+<p>
+F30:<br>
+2018-09-22T2254 purchase<br>
+2018-09-22T2336 plough<br>
+2018-09-22T2336 fertilize slurry &lt;- no need, it was fertilized already (plough over crops hehe)<br>
+</p>
+
+<p>
+Result: 2018-09-23T1030hrs discovered that the whole northern and NE edge of the map is cut off by fences, so decided to stop playing and deleted this savegame as it was so frustrating.
+</p>
+
+</section>
+
+<footer>
+<p>
+<a href="stories.php" class="button">Stories FS17 home page</a>
+<a href="../index.php" class="button">PMC Farming Simulator root page</a>
+</p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
