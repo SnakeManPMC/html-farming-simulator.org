90a8a09420916e4913cc3d0ef9a9098ab50e3e8e
Snake Man <snakeman@phnet.fi>
2024-03-18 23:04:50 +0000

    FS17 downloads migrated to torrent magnet

--- 17/downloads.php
+++ 17/downloads.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Farming Simulator Downloads - PMC Farming</title>
+<title>Farming Simulator 17 Downloads - PMC Farming</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator Downloads - PMC Farming">
-<META name="keywords" content="Farming Simulator Downloads, PMC, Farming">
+<META name="description" content="Farming Simulator 17 Downloads - PMC Farming">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,21 +14,36 @@
 <body>
 
 <header>
-<!-- google ads -->
-<?php include("../include/google-ads.php"); ?>
-<h1><b>PMC Farming Simulator Downloads</b></h1>
+<h1>PMC Farming Simulator 17 Downloads</h1>
 </header>
 
 <section>
-<h2>Farming Simulator 17 Maps</h2>
-<p>PMC favorite Farming Simulator 17 map downloads.
+	<h2>Farming Simulator 17 Terrains</h2>
+
+<p>
+PMC favorite Farming Simulator 17 (FS17) terrain aka "<b>FS17 Mod Map Downloads</b>".
 </p>
 
 <ul>
-<!-- <li><a href="downloads/FS17_Cornbelt.zip">FS17_Cornbelt.zip</a>, CornBelt v1.0.2</li> -->
-<li><a href="downloads/FS17_NWTexas.zip">FS17_NWTexas.zip</a>, North West Texas</li>
-<li><a href="downloads/Parkers_Prairie_v1_4_2.zip">Parkers_Prairie_v1_4_2.zip</a>, Parkers Prairie v1.4.2</li>
+<li>FS17_NWTexas.zip, North West Texas by TxZar - 711.2mb. <a href="magnet:?xt=urn:btih:d18bcb7672e047efb02c92700da553cef848cb23&xt=urn:btmh:122049a430d39a0f6fbdaf0b0a658f06f0eabfd04aa87047bf83083b275c1132fded&dn=FS17_NWTexas.zip&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent magnet</a></li>
+<li>Parkers_Prairie_v1_4_2.zip, Parkers Prairie v1.4.2 - 524.1mb. <a href="magnet:?xt=urn:btih:d87a851bbafb85f9a368b7b9b2db38222f2307b5&xt=urn:btmh:1220fa41e6da839c26d25ccbe51d828e4bc0adfdb02d7db5a7979128e91c6d21ae6c&dn=Parkers_Prairie_v1_4_2.zip&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent magnet</a></li>
 </ul>
+
+
+	<h2>Farming Simulator 17 Tools And Scripts</h2>
+
+<p>
+FS17 Tools, scripts etc.
+</p>
+
+<ul>
+<li>Giants_Editor_Scripts_2018-07-11.7z - 901bytes. <a href="magnet:?xt=urn:btih:84f1d9d6e58d02854b6364f35e8ed1b9516f2153&xt=urn:btmh:1220db17593f928f903ddb9c4ae9e6d3a3ac905864abca4f0794c866ebc600af8144&dn=Giants_Editor_Scripts_2018-07-11.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent magnet</a></li>
+<li>grleConverter_7.0.1_win32.7z - 158kb. <a href="magnet:?xt=urn:btih:33ae96dfa3956c396a8a17a392e45395d36a480b&xt=urn:btmh:122076b01077acf5ddbf72a572bc75b78cf62daa6e485eea2ca61439ac08f0f44793&dn=grleConverter_7.0.1_win32.7z&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce">Torrent magnet</a></li>
+</ul>
+
+<p>
+Remember to keep seeding, give back to the community which has given so much to you all for free.
+</p>
 
 </section>
 
