d5642489d1dfd3d8cf2cdd2bf3c29d0db746cb29
Snake Man <snakeman@phnet.fi>
2024-09-18 10:15:24 +0000

    fs25 news-pre-release terrains page created.

---
+++ 25/news-pre-release/terrains.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Terrains Farming Simulator 25 (FS25) Pre-Release News - PMC Farming</title>
+<LINK href="../../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Terrains Farming Simulator 25 (FS25) Pre-Release News - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Terrains Farming Simulator 25 Pre-Release News</h1>
+<p>
+Terrains for Farming Simulator 25 (FS25). Information is that there are three terrains, Riverbend Springs is the American terrain, South East Asian terrain name has not been released and European terrain is FS22 Zielonka, its just ported over from one of the DLC's.
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Terrains FS25</h2>
+
+<p>
+"(south) East Asian landscape, North American environment with plenty of open space and entral European location with green fields between ponds and rivers" -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=544" target="_blank">Giants websites blog</a>.
+</p>
+
+<p>
+Preview: Riverbend Springs. "A huge, winding river, lots of water, and many little creeks and streams in between. A ferry, too. If you're wondering, how to get across the river - crossing it with a ferry fit for your machine is an option!", "It's all about water and we wanted to celebrate that. piers, jetties, houseboats, restaurants floating on the river", "old grain elevator, a paper mill at the river or brick buildings how they were built a hundred years ago" <a href="https://www.farming-simulator.com/newsArticle.php?news_id=550" target="_blank">Giants websites blog</a>.
+</p>
+
+<a href="screenshots/Riverbend-Springs-Buildings-06.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-06.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<p>
+PDA map.
+</p>
+
+<a href="screenshots/Riverbend-Springs-PDA-Map.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-PDA-Map.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<p>
+Rice paddy.
+</p>
+
+<a href="screenshots/Rice-Paddy-01.jpg" target="_blank"><img src="screenshots/thumbs/Rice-Paddy-01.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<p>
+River ferry.
+</p>
+
+<a href="screenshots/River-Ferry-02.jpg" target="_blank"><img src="screenshots/thumbs/River-Ferry-02.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/River-Ferry-03.jpg" target="_blank"><img src="screenshots/thumbs/River-Ferry-03.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<p>
+River creek.
+</p>
+
+<a href="screenshots/Riverbend-Springs-Landscape-03.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Landscape-03.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Landscape-02.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Landscape-02.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-05.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-05.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-03.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-03.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-01.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-01.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-11.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-11.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-08.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-08.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-10.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-10.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-04.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-04.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Landscape-01.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Landscape-01.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-13.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-13.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-09.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-09.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-02.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-02.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-07.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-07.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Buildings-12.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Buildings-12.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Riverbend-Springs-Landscape-04.jpg" target="_blank"><img src="screenshots/thumbs/Riverbend-Springs-Landscape-04.jpg" class="terrainimg" loading="lazy" alt="Riverbend Springs Terrain Farming Simulator 25 Screenshot" title="Riverbend Springs Terrain Farming Simulator 25 Screenshot"></a>
+
+<p>
+There is no information about FS25 Riverbend Springs terrain size, so its safe to assume it will be 2 kilometers x 2 kilometers, same as previous Giants terrains.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>
+<a href="../../index.php" class="button">PMC Farming Simulator index page</a>
+<a href="index.php" class="button">FS25 Pre-Release News</a>
+</p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
