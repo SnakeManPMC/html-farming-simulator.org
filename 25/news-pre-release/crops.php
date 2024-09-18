eb061ca54a03b6bdde2da96951e89c50158e448d
Snake Man <snakeman@phnet.fi>
2024-09-18 07:18:14 +0000

    fs25 news-pre-release crops page created.

---
+++ 25/news-pre-release/crops.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Crops Farming Simulator 25 (FS25) Pre-Release News - PMC Farming</title>
+<LINK href="../../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Crops Farming Simulator 25 (FS25) Pre-Release News - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Crops Farming Simulator 25 Pre-Release News</h1>
+<p>
+New Crops coming in Farming Simulator 25 (FS25) game release on November 12th 2024. This page focuses on the new crops or any changes how old existing crops work in the new FS25 engine.
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Crops FS25</h2>
+
+<p>
+"flood the fields with fresh water to grow rice", "Grow two types of rice, spinach, and other new crops choosing from well over 20 overall." -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=544" target="_blank">Giants website blog</a>.
+</p>
+
+<p>
+Spinach. "Spring and Summer are spinach seasons. In March or April, you take your seeder and put out about 10 liters of seed per hectare to look forward to a yield of about 23100 liters in return. With an average selling price of $660 per thousand liters, you need to be ready to harvest in June. Mind, that you need a specialized harvester here - the OXBO MKB-4TR.", "Sow Once, Harvest Twice. To do that, you got to sow it right in March, wait for it to mature, harvest it right away in June and watch it grow again. Doing everything right and in time, you'll see the those growth stages that slightly differ in the second cycle. As it is common among crops like spinach, the first growth stage after harvesting it the first time, appears a bit more bushy. Helps to easier keep track of all your spinach fields visually." -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=551" target="_blank">Giants website blog</a>.
+</p>
+
+<p>
+Spinach Growth Stages
+</p>
+
+<a href="screenshots/Growth-Stages-Spinach.jpg" target="_blank"><img src="screenshots/thumbs/Growth-Stages-Spinach.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Agro-Masz-Aquila-Drive-400-01.jpg" target="_blank"><img src="screenshots/thumbs/Agro-Masz-Aquila-Drive-400-01.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<p>
+Harvesting Spinach?
+</p>
+
+<a href="screenshots/Oxbo-MKB-4TR-06.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-06.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<p>
+Oxbo MKB-4TR harvesting spinach
+</p>
+
+<a href="screenshots/Oxbo-MKB-4TR-03.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-03.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-MKB-4TR-02.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-02.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-MKB-4TR-01.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-01.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-MKB-4TR-04.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-04.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-MKB-4TR-05.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-05.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-01.jpg" target="_blank"><img src="screenshots/thumbs/Crops-01.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-02.jpg" target="_blank"><img src="screenshots/thumbs/Crops-02.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-06.jpg" target="_blank"><img src="screenshots/thumbs/Crops-06.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-07.jpg" target="_blank"><img src="screenshots/thumbs/Crops-07.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-03.jpg" target="_blank"><img src="screenshots/thumbs/Crops-03.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-05.jpg" target="_blank"><img src="screenshots/thumbs/Crops-05.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-04.jpg" target="_blank"><img src="screenshots/thumbs/Crops-04.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-08.jpg" target="_blank"><img src="screenshots/thumbs/Crops-08.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<p>
+Peas. "Peas are among the first crops you can plant in Spring. In March or April, you take your seeder and put out about 250 liters of seed per hectare to look forward to a yield of about 9,600 liters in return. you need to be ready to harvest in July and September at the latest. Peas can be used in production chains." -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=555" target="_blank">Giants website blog</a>.
+</p>
+
+<p>
+Peas Growth States
+</p>
+
+<a href="screenshots/Growth-Stages-Peas.jpg" target="_blank"><img src="screenshots/thumbs/Growth-Stages-Peas.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-13.jpg" target="_blank"><img src="screenshots/thumbs/Crops-13.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/John-Deere-3650-Great-Plains-01.jpg" target="_blank"><img src="screenshots/thumbs/John-Deere-3650-Great-Plains-01.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/John-Deere-3650-Great-Plains-02.jpg" target="_blank"><img src="screenshots/thumbs/John-Deere-3650-Great-Plains-02.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-17.jpg" target="_blank"><img src="screenshots/thumbs/Crops-17.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-12.jpg" target="_blank"><img src="screenshots/thumbs/Crops-12.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-18.jpg" target="_blank"><img src="screenshots/thumbs/Crops-18.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-EPD540e-02.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-EPD540e-02.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-EPD540e-01.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-EPD540e-01.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-MKB-4TR-07.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-07.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-09.jpg" target="_blank"><img src="screenshots/thumbs/Crops-09.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-10.jpg" target="_blank"><img src="screenshots/thumbs/Crops-10.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-11.jpg" target="_blank"><img src="screenshots/thumbs/Crops-11.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-15.jpg" target="_blank"><img src="screenshots/thumbs/Crops-15.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-14.jpg" target="_blank"><img src="screenshots/thumbs/Crops-14.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Crops-16.jpg" target="_blank"><img src="screenshots/thumbs/Crops-16.jpg" class="terrainimg" loading="lazy" alt="Crops Farming Simulator 25 Screenshot" title="Crops Farming Simulator 25 Screenshot"></a>
+
+<p>
+2024-09-17T09:17:53Z Coming Soon on the Blog: Green Beans! ???
+</p>
+
+<p>
+You can grow your own rice saplings in a greenhouse that then is used to seed rice fields / rice paddys. -source youtube the farmsim guy <a href="https://www.youtube.com/watch?v=DSWjR-13NSU" target="_blank">"9 Minutes of Farming Simulator 25 - Raw Gameplay Footage"</a>.
+</p>
+
+<p>
+FS25 includes 25 different crops. New crop is green beans. -source djgoham <a href="https://www.youtube.com/watch?v=IBVaN3mWuJ8" target="_blank">"I PLAYED FARM SIM 25 BEFORE ANYONE ELSE!"</a>.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="../../index.php" class="button">PMC Farming Simulator index page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
