2ef44f69544de8fb2b68d2bbc8626ab324c0ea22
Snake Man <snakeman@phnet.fi>
2024-09-18 11:09:45 +0000

    fs25 news-pre-release vehicles page created.

---
+++ 25/news-pre-release/vehicles.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Vehicles Farming Simulator 25 (FS25) Pre-Release News - PMC Farming</title>
+<LINK href="../../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Vehicles Farming Simulator 25 (FS25) Pre-Release News - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Vehicles Farming Simulator 25 Pre-Release News</h1>
+<p>
+Farming Simulator (FS25) Pre-Release News for Vehicles and Equipment.
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Vehicles FS25</h2>
+
+<p>
+List of confirmed vehicles in FS25, tractors, combine harvesters, trailers, telehandlers, wheel loaders, swathers, seeders, planters, sprayers and semi-trucks.
+</p>
+
+<p>
+OXBO EPD540E self propelled pea harvester
+</p>
+
+<p>
+OXBO MKB-4TR
+</p>
+
+<p>
+Case IH AF11 combine harvester
+</p>
+
+<p>
+Case IH Quadtrac 715
+</p>
+
+<p>
+Claas Quadtrac Xerion 12.650 TT
+</p>
+
+<p>
+Fendt 512 Vario and Fendt 700 Vario
+</p>
+
+<p>
+Grimme Matrix 1800
+</p>
+
+<p>
+International semi-trucks
+</p>
+
+<p>
+John Deere 3650
+</p>
+
+<p>
+John Deere 9R 540
+</p>
+
+<p>
+John Deere S7.900 combine harvester
+</p>
+
+<p>
+Kubota SVL 97.2 skid steer
+</p>
+
+<p>
+Lode King Prestige semi-truck grain trailer
+</p>
+
+<p>
+MacDon R216 SP header and MacDon M1240 mower/swather(?)
+</p>
+
+<p>
+Massey Ferguson MF 9s.425
+</p>
+
+<p>
+Merlo MF 44.9 telehandler
+</p>
+
+<p>
+Mzuri Pro-ti(?) 4tx
+</p>
+
+<p>
+Sennebogen (spelling?) telehandler
+</p>
+
+<p>
+Valtra S 286
+</p>
+
+<p>
+Giants weekly friday Fact Sheets.
+</p>
+
+<p>
+Fendt 700 Vario + Bednar Omega 00 6000 FL
+</p>
+
+<a href="screenshots/Fendt-700-Vario-Bednar-Omega-00-6000-FL-01.jpg" target="_blank"><img src="screenshots/thumbs/Fendt-700-Vario-Bednar-Omega-00-6000-FL-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Fendt-700-Vario-Bednar-Omega-00-6000-FL-02.jpg" target="_blank"><img src="screenshots/thumbs/Fendt-700-Vario-Bednar-Omega-00-6000-FL-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Deutz Fahr 6c Rvshift + Krone Varipack V 190 XC Plus
+</p>
+
+<a href="screenshots/Deutz-Fahr-6C-Rvshift-Krone-VariPack-V190XC-Plus-01.jpg" target="_blank"><img src="screenshots/thumbs/Deutz-Fahr-6C-Rvshift-Krone-VariPack-V190XC-Plus-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Deutz-Fahr-6C-Rvshift-Krone-VariPack-V190XC-Plus-02.jpg" target="_blank"><img src="screenshots/thumbs/Deutz-Fahr-6C-Rvshift-Krone-VariPack-V190XC-Plus-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+JCB 435s wheel loader
+</p>
+
+<a href="screenshots/JCB-435S-Wheel-Loader-01.jpg" target="_blank"><img src="screenshots/thumbs/JCB-435S-Wheel-Loader-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+AGCO White 8010 Series + Anderson Mergepro 915
+</p>
+
+<a href="screenshots/AGCO-White-8010-Series-Anderson-Mergepro-915-01.jpg" target="_blank"><img src="screenshots/thumbs/AGCO-White-8010-Series-Anderson-Mergepro-915-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/AGCO-White-8010-Series-Anderson-Mergepro-915-02.jpg" target="_blank"><img src="screenshots/thumbs/AGCO-White-8010-Series-Anderson-Mergepro-915-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Case IH AF11 + 4418 N combine harvester
+</p>
+
+<a href="screenshots/Case-IH-AF11-4418-N-01.jpg" target="_blank"><img src="screenshots/thumbs/Case-IH-AF11-4418-N-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Case-IH-AF11-4418-N-02.jpg" target="_blank"><img src="screenshots/thumbs/Case-IH-AF11-4418-N-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+MacDon M1240 + D 140 XL
+</p>
+
+<a href="screenshots/MacDon-M1240-D140-XL-01.jpg" target="_blank"><img src="screenshots/thumbs/MacDon-M1240-D140-XL-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/MacDon-M1240-D140-XL-02.jpg" target="_blank"><img src="screenshots/thumbs/MacDon-M1240-D140-XL-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Agrifac Condor Endurance II
+</p>
+
+<a href="screenshots/Agrifac-Condor-Endurance-II-01.jpg" target="_blank"><img src="screenshots/thumbs/Agrifac-Condor-Endurance-II-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+John Deere 3650 + Great Plains Solid Stand 1500
+</p>
+
+<a href="screenshots/John-Deere-3650-Great-Plains-Solid-Stand-1500-01.jpg" target="_blank"><img src="screenshots/thumbs/John-Deere-3650-Great-Plains-Solid-Stand-1500-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/John-Deere-3650-Great-Plains-Solid-Stand-1500-02.jpg" target="_blank"><img src="screenshots/thumbs/John-Deere-3650-Great-Plains-Solid-Stand-1500-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Valtra S Series + Brantner TR 34090/2 PT+
+</p>
+
+<a href="screenshots/Valtra-S-Series-Brantner-TR-34090-2PT-01.jpg" target="_blank"><img src="screenshots/thumbs/Valtra-S-Series-Brantner-TR-34090-2PT-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Valtra-S-Series-Brantner-TR-34090-2PT-02.jpg" target="_blank"><img src="screenshots/thumbs/Valtra-S-Series-Brantner-TR-34090-2PT-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Merlo MF44.9CS-170-Cvtronic
+</p>
+
+<a href="screenshots/Merlo-MF44_9CS-170-Cvtronic-01.jpg" target="_blank"><img src="screenshots/thumbs/Merlo-MF44_9CS-170-Cvtronic-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Merlo-MF44_9CS-170-Cvtronic-02.jpg" target="_blank"><img src="screenshots/thumbs/Merlo-MF44_9CS-170-Cvtronic-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Oxbo MKB-4TR
+</p>
+
+<a href="screenshots/Oxbo-MKB-4TR-08.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-08.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Oxbo-MKB-4TR-09.jpg" target="_blank"><img src="screenshots/thumbs/Oxbo-MKB-4TR-09.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+Iseki TJW + Farmtech EDK 650
+</p>
+
+<a href="screenshots/Iseki-TJW-Farmtec-EDK-650-01.jpg" target="_blank"><img src="screenshots/thumbs/Iseki-TJW-Farmtec-EDK-650-01.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Iseki-TJW-Farmtec-EDK-650-02.jpg" target="_blank"><img src="screenshots/thumbs/Iseki-TJW-Farmtec-EDK-650-02.jpg" class="terrainimg" loading="lazy" alt="Vehicles Farming Simulator 25 Screenshot" title="Vehicles Farming Simulator 25 Screenshot"></a>
+
+<p>
+J&M grain cart X1112 -source youtube djgoham time code <a href="https://youtu.be/M2KFbxM2_JY?t=61" target="_blank">"FS25 Gamescom Trailer (Official) - Farming Simulator 25"</a>.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>
+<a href="../../index.php" class="button">PMC Farming Simulator index page</a>
+<a href="index.php" class="button">FS25 Pre-Release News</a>
+</p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
