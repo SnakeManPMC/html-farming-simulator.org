8db9010e34a13ed787312207ccc660d73ffabd4b
Snake Man <snakeman@phnet.fi>
2024-09-18 06:02:23 +0000

    fs25 news-pre-release dir created - animals.php page created

---
+++ 25/news-pre-release/animals.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Animals Farming Simulator 25 (FS25) Pre-Release News - PMC Farming</title>
+<LINK href="../../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Animals Farming Simulator 25 (FS25) Pre-Release News - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Animals Farming Simulator 25 Pre-Release News</h1>
+<p>
+Farming Simulator (FS25) Pre-Release News for Animals. Cows, Pigs, Water Buffalos, Goats, Horses, etc. This page focuses on the new animals or any changes how animals work in the new FS25 engine.
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Animals FS25</h2>
+
+<p>
+<b>Water Buffalos</b> -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=544" target="_blank">Giants website blog</a>.
+</p>
+
+<a href="screenshots/Water-Buffalo-Header-Wide.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-Header-Wide.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<p>
+"Water Buffaloes adapt to harsher environments: Hot and humid? No problem. That's why they feel at home in South East Asia. fields of grass will be cut by grazing livestock. Not just the water buffaloes. Water buffalo milk is especially rich in fat, protein, and minerals, making it ideal for producing dairy products. Water buffalos are mainly used to produce milk. breeding and selling for a profit. By the age of 18 months, water buffalos are matured. That's when they start producing milk. And offspring. They also start reproducing, and tiny baby buffaloes will soon appear in your pastures. you buy water buffalos as calfs or adults. The game features a system developed to ensure animals interact correctly by keeping them within a defined space where they're aware of each other's positions." -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=553" target="_blank">Giants website blog</a>.
+</p>
+
+<a href="screenshots/Water-Buffalo-02.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-02.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-03.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-03.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-04.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-04.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<p>
+Water Buffalo Milk Pallets
+</p>
+
+<a href="screenshots/Water-Buffalo-Milk-Pallets.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-Milk-Pallets.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-01.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-01.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<p>
+Water Buffalo Calfs
+</p>
+
+<a href="screenshots/Water-Buffalo-Calf-02.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-Calf-02.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-Calf-01.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-Calf-01.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-Calf-03.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-Calf-03.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<p>
+Water Buffalo 3D Model Zbrush
+</p>
+
+<a href="screenshots/Water-Buffalo-3D-Model-01.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-3D-Model-01.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-3D-Model-02.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-3D-Model-02.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-3D-Model-05.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-3D-Model-05.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<p>
+Water Buffalo Texture In Substance Painter
+</p>
+
+<a href="screenshots/Water-Buffalo-Texture-Substance-Painter.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-Texture-Substance-Painter.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-3D-Model-06.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-3D-Model-06.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-3D-Model-03.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-3D-Model-03.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Water-Buffalo-3D-Model-04.jpg" target="_blank"><img src="screenshots/thumbs/Water-Buffalo-3D-Model-04.jpg" class="terrainimg" loading="lazy" alt="Water Buffalo Farming Simulator 25 Screenshot" title="Water Buffalo Farming Simulator 25 Screenshot"></a>
+
+<p>
+Pigs have "piglets" baby animals as well. -source djgoham youtube video <a href="https://youtu.be/M2KFbxM2_JY?t=31" target="_blank">"FS25 Gamescom Trailer (Official) - Farming Simulator 25"</a>.
+</p>
+
+<p>
+Goats. There has only been a teaser alike short video clip of a baby goat.
+</p>
+
+<p>
+Cows have calfs, ie baby cows as well.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="../../index.php" class="button">PMC Farming Simulator index page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
