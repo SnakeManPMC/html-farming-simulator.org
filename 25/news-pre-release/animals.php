114ed2fcc379f6570d2761ba45fd11c08fa4977a
Snake Man <snakeman@phnet.fi>
2024-10-10 15:10:15 +0000

    fs25 goats pre-release-news updated

--- 25/news-pre-release/animals.php
+++ 25/news-pre-release/animals.php
@@ -95,6 +95,149 @@ Goats. There has only been a teaser alike short video clip of a baby goat.
 Cows have calfs, ie baby cows as well.
 </p>
 
+
+	<h2>Goats</h2>
+
+<p>
+Preview: Goats! -source <a href="https://www.farming-simulator.com/newsArticle.php?news_id=564" target="_blank">Giants website blog</a>.
+</p>
+
+<p>
+Looks like FS25 goats will use the same barn/pasture as FS22 uses for Sheeps, can be seen in this <a href="https://www.youtube.com/watch?v=pyhazjLCteY" target="_blank">youtube: Goats in 25 Seconds FS25</a> video.
+</p>
+
+<a href="screenshots/Goat-13.jpg" target="_blank"><img src="screenshots/thumbs/Goat-13.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<p>
+You want to get goats right and the most complex thing are their legs and rectangular pupils.
+</p>
+
+<a href="screenshots/Goat-Eyeball-02.jpg" target="_blank"><img src="screenshots/thumbs/Goat-Eyeball-02.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-Eyeball-03.jpg" target="_blank"><img src="screenshots/thumbs/Goat-Eyeball-03.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-Eyeball-01.jpg" target="_blank"><img src="screenshots/thumbs/Goat-Eyeball-01.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-05.jpg" target="_blank"><img src="screenshots/thumbs/Goat-05.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<p>
+Then our artists and programmers build other parts of an anatomically correct goat in different stages from newborn to adult, so you can enjoy one of the most anticipated farm animals!
+</p>
+
+<p>
+Click for a large animated <a href="https://www.farming-simulator.org/25/news-pre-release/screenshots/Goat-Closeup-Animation.gif" target="_blank">Goat-Closeup-Animation.gif</a> image.
+</p>
+
+<a href="screenshots/Goat-01.jpg" target="_blank"><img src="screenshots/thumbs/Goat-01.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-04.jpg" target="_blank"><img src="screenshots/thumbs/Goat-04.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<ul>
+<li>Goats are incredible climbers. They're known for their agility, even scaling steep cliffs and trees. If you think you're good at bouldering, look at goats!</li>
+<li>Rectangular pupils give them nearly 360-degree panoramic vision, helping them spot predators and navigate challenging terrain.</li>
+<li>They're curious and intelligent. They can learn how to open gates or manipulate objects. Good to have an eye on them.</li>
+<li>Goats are highly social and form close bonds within their herds, or even humans and other animals.</li>
+<li>Each goat has a unique voice. Just look at all those screaming goat videos. So not only mother goats could recognize individual goats go by their bleat.</li>
+</ul>
+
+<p>
+Click for a large animated <a href="https://www.farming-simulator.org/25/news-pre-release/screenshots/Goat-Young-Jumping-Around-Animation-01.gif" target="_blank">Goat-Young-Jumping-Around-Animation-01.gif</a> image.
+</p>
+
+<a href="screenshots/Goat-07.jpg" target="_blank"><img src="screenshots/thumbs/Goat-07.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-03.jpg" target="_blank"><img src="screenshots/thumbs/Goat-03.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-12.jpg" target="_blank"><img src="screenshots/thumbs/Goat-12.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-06.jpg" target="_blank"><img src="screenshots/thumbs/Goat-06.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-11.jpg" target="_blank"><img src="screenshots/thumbs/Goat-11.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<p>
+Goat milk it's so unique in taste and profitable in production!
+</p>
+
+<a href="screenshots/Goat-09.jpg" target="_blank"><img src="screenshots/thumbs/Goat-09.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-10.jpg" target="_blank"><img src="screenshots/thumbs/Goat-10.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-Cheese-01.jpg" target="_blank"><img src="screenshots/thumbs/Goat-Cheese-01.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<a href="screenshots/Goat-02.jpg" target="_blank"><img src="screenshots/thumbs/Goat-02.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<p>
+New production chains allow you to process their milk and go for fine goat cheese!
+</p>
+
+<a href="screenshots/Goat-08.jpg" target="_blank"><img src="screenshots/thumbs/Goat-08.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot"></a>
+
+<p>
+<b>Bonus Interview Manuel Leithner</b>, producer, gameplay and lead gameplay programmer.
+</p>
+
+<img src="screenshots/Interview-Manuel-Leithner.jpg" class="terrainimg" loading="lazy" alt="Animals Goats Farming Simulator 25 Screenshot" title="Animals Goats Farming Simulator 25 Screenshot">
+
+<p>
+What inspired the decision to add goats to the game?
+</p>
+
+<p>
+<i>
+Goats have been a popular request from our players, especially in certain regions where they play an important role in animal husbandry. They also work well with our production chains and new products that players can craft, making them more interesting for everyone.
+</i>
+</p>
+
+<p>
+What unique behavior will goats exhibit compared to other livestock?
+</p>
+
+<p>
+<i>
+You will notice many new animations from our Character & Animation team, who have done a great job bringing the animals to life. As goats are particularly lively and looking at our improved animation system, we thought Farming Simulator 25 would be incomplete without them. Especially the kids are jumping around like you would expect young goats to behave which is quite nice to watch.
+</i>
+</p>
+
+<p>
+How did you research real goat behavior to design their in-game actions?
+</p>
+
+<p>
+<i>
+One of the benefits of having a lot of people working on Farming Simulator who already had a lot of experience with real-life farming is that it is easier to bring those elements into the game. There's already an underlying sense of what's authentic, and that includes animals like goats when we digitize them. 
+</i>
+</p>
+
+<p>
+<i>
+But to really bring their virtual counterparts to life, we also look at a lot of videos of goats in various environments and discussed their behavior together. In the end, artists, animators, and programmers had to be in sync.
+</i>
+</p>
+
+<p>
+What challenges did you face when creating goats in general?
+</p>
+
+<p>
+<i>
+Goats are active, excited and playful creatures when you follow them around. So, the challenging part here was of course to make their movements look realistic and animate them properly. I think, our team did a good job here and goats add to the liveliness of the farms.
+</i>
+</p>
+
+<p>
+Why do you think goats are impressive farm animals?
+</p>
+
+<p>
+<i>
+Goats in particular like to let you know that they have distinct personalities. As I said before, they are playful. Because they are also incredibly curious and intelligent, always exploring their surroundings, they can be quite challenging livestock to keep. But they make up for it in providing valuable resources like milk, and they’re known for their hardiness in different environments.
+</i>
+</p>
+
+<p>
+Click for a large animated <a href="https://www.farming-simulator.org/25/news-pre-release/screenshots/Goat-Laying-Down-Chewing-Animation.gif" target="_blank">Goat-Laying-Down-Chewing-Animation.gif</a> image.
+</p>
+
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
