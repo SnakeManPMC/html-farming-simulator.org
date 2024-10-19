78c04a661577b337d8ba2a8e07187e27ef16a672
Snake Man <snakeman@phnet.fi>
2024-10-19 04:56:30 +0000

    fs25 placeholder info for mods-combine-harvester page

--- 25/mods-combine-harvester.php
+++ 25/mods-combine-harvester.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Farming Simulator 25 Mods - PMC Farming</title>
+<title>Mods Combine Harvester Farming Simulator 25 - PMC Farming Simulator 25</title>
 <LINK href="../css.css" rel=stylesheet type="text/css">
-<META name="description" content="Farming Simulator 25 Mods - PMC Farming">
+<META name="description" content="Mods Combine Harvester Farming Simulator 25 - PMC Farming Simulator 25">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,32 +15,31 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1>PMC Farming Simulator 25 Mods</h1>
+	<h1>Mods Combine Harvester Farming Simulator 25 - PMC Farming Simulator 25</h1>
 
 <p>
-Modifications for Farming Simulator 25 (FS25), aka "mods". These include tractors, obviously, cultivators, plows (digger, ripper etc names), spreaders, sprayers, trailers, grain carts, combine harvesters, balers, grain bin silos, placeables, animal sheds, vehicle sheds, ooh my, there are so many!
+Farming Simulator 25 (FS25) combine harvester mods. There are all kinds of harvesters for FS25, small ones, big ones, old and new ones. John Deere, Case IH, Claas, etc.
 </p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>FS25 Mods</h2>
-
-<ul>
-<li><a href="mods-baler.php">Baler</a> to get grass or hay into more manageable bales, you need a baler.</li>
-<li><a href="mods-combine-harvester.php">Combine Harvester</a> harvest is the king of farming activities, for that you need a good combine harvester.</li>
-<li></li>
-</ul>
+	<h2>Mods Combine Harvester Farming Simulator 25</h2>
 
 <?php include("../include/fs25-placeholder-pages.php"); ?>
+
+<div class="wrapper_mod_vehicles">
+
+<!-- category vehicles -->
+</div>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
 <?php include("/var/www/include/footer-start.php"); ?>
-<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><a href="mods.php" class="button">FS25 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
 <p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
 <?php include("/var/www/include/footer-end.php"); ?>
 <?php include("/var/www/include/support.php"); ?>
