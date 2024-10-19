009a2aaa914c9456b15ef05d5562a8c0c9f76f71
Snake Man <snakeman@phnet.fi>
2024-10-19 07:16:59 +0000

    fs25 stories-release-and-install page filled

--- 25/stories-release-and-install.php
+++ 25/stories-release-and-install.php
@@ -25,9 +25,46 @@ This is the first Farming Simulator 25 (FS25) Story by PMC, its about how I got,
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>Beginning</h2>
+	<h2>Release Date Announcement And Pre-Order</h2>
 
 <p>
+2024-06-25 Farming Simulator 25 (FS25) release date is announced for 2024-11-12 (nov 12th).
+</p>
+
+<p>
+At this time I didn't have too much free money to use for purchasing FS25 serial key, technically I did have it but Farming Simulator 22 (FS22) with its season passes which I purchased, gave me such a bad taste in mouth as those passes were completely useless to me, only thing I could use from those was Kubota tractor and one sprayer, now that is overkill if anything if this whole season pass thing only gets you one tractor and one implement, barely, I really don't need those but as they are available I do use them in early savegames.
+</p>
+
+<p>
+For this reason I was extremely reluctant to buy FS25, my enthusiasm for it was just about zero. I don't think on june 25th community even had any info about the new game engine yet so it was just best guesses and speculation, which was founded upon the previous game release ie FS22 and as I said that left my mouth with a very bad after taste with useless season pass and all.
+</p>
+
+<p>
+I had already decided year or so ago that for the next game engine release I will not buy any of the DLC or whatever season passes, I will rather buy them more expensive when I see something interesting being confirmed than just throwing money blindly to Giants Software like gambling that maybe the season pass will have some good stuff in it. Nope, not me, not this time around, FS22 burnt me pretty bad.
+</p>
+
+<p>
+Time went on, months passed by, in sep/oct was the dreaded PMC website hosting and domain registration yearly payment, renewal fees, for those I saved money for months, actually I might had already saving mode on back in jun 25th just cant remember. I wrote details about this project in <a href="https://www.pmctactical.org/forum/viewtopic.php?t=22666" target="_blank">PMC Tactical Forum "PMC websites from shared hosting to Virtual Private Server" topic</a>, but in short I finally managed to get all those website fees paid off, but now I am broke as ever.
+</p>
+
+<p>
+Early september while dealing with that website payment stuff I decided to ask community for help on getting me FS25, gifted as the term goes. Went and posted <a href="https://www.pmctactical.org/forum/viewtopic.php?t=23009" target="_blank">PMC Tactical Forum "Farming Simulator 25 Game Gift to PMC" topic</a> about it. Then again time went on and honestly I was quite surprised that nobody took upon that gifting idea, seeing how much for example people in twitch and youtube live streams just throw away serial key "give-aways" like they are nothing, I have to admit I was quite sad/disappointed that nobody offered a serial key for me, especially as I laid out what would I do "in return" for the whole community (read the topic, its all there).
+</p>
+
+<p>
+Finally one guy stepped up, PMC Tactical Forum member <a href="https://www.pmctactical.org/forum/memberlist.php?mode=viewprofile&u=12345" target="_blank">coop_63</a>, he gifted a copy of FS25 to me. Man that was awesome! Thank you <b>coop_63</b> you are so generous, thank you so much! <img src="../images/smileys/icon_biggrin.gif" alt="Smiley :D" loading="lazy">
+</p>
+
+<p>
+2024-10-08T13:59:00Z Purchased legit FS25 46,78eur (49,99 USD) from giants website.
+</p>
+
+<p>
+Tested <a href="https://eshop.giants-software.com/downloads.php" target="_blank">Giants eshop</a> right away could I already download the installer files but no, there is just notification that game is available nov 12th 2024.
+</p>
+
+<p>
+Next up was just waiting until such time the installer can be downloaded. My previous FS22 release and install story begins from hours before serial key goes active on release day, so I have no reference data for how many days/weeks prior to release day the installer becomes available for download, cant remember if giants announced it somewhere or did it just one day appear in eshop. Anyways. Now I'm waiting, perhaps check eshop every few days.
 </p>
 
 <p>
@@ -37,7 +74,6 @@ Some background information, my gaming computer is called PMC Gaming and it is r
 <!--
 	<h2>UTC FS25 Release Story and Installation</h2>
 -->
-<?php include("../include/fs25-placeholder-pages.php"); ?>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
