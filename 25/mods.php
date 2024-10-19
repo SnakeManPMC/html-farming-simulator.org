3b5d3b1cbbfe69e76b8c2e1883bfa395803b35cf
Snake Man <snakeman@phnet.fi>
2024-10-19 03:16:58 +0000

    fs25 placeholder info for mods page

---
+++ 25/mods.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Farming Simulator 25 Mods - PMC Farming</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Farming Simulator 25 Mods - PMC Farming">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>PMC Farming Simulator 25 Mods</h1>
+
+<p>
+Modifications for Farming Simulator 25 (FS25), aka "mods". These include tractors, obviously, cultivators, plows (digger, ripper etc names), spreaders, sprayers, trailers, grain carts, combine harvesters, balers, grain bin silos, placeables, animal sheds, vehicle sheds, ooh my, there are so many!
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>FS25 Mods</h2>
+
+<?php include("../include/fs25-placeholder-pages.php"); ?>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -30,6 +30,7 @@
 <a href="25/dev-diary.php" class="button">Dev Diary</a>
 <a href="25/equipment-vehicle-prices.php" class="button">Equipment Vehicle Prices</a>
 <a href="25/guide.php" class="button">Guide</a>
+<a href="25/mods.php" class="button">Mods</a>
 <a href="25/pmc-terrains.php" class="button">PMC Terrains</a>
 <a href="25/settings.php" class="button">Settings</a>
 <a href="https://www.pmctactical.org/forum/viewforum.php?f=80" target="_blank" class="button">Forum FS25</a>
