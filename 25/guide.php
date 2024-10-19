eb7993e75b21b7f3cfc42be026b4f84c71ec6d8c
Snake Man <snakeman@phnet.fi>
2024-10-19 02:34:49 +0000

    fs25 placeholder info for guide page

--- 25/guide.php
+++ 25/guide.php
@@ -14,27 +14,40 @@
 <body>
 
 <header>
-<h1>PMC Farming Simulator 25 Playing Guide</h1>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>PMC Farming Simulator 25 Playing Guide</h1>
+<p>
+Farming Simulator 25 (FS25) playing guide, tutorial, tips and other information about farming.
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
-
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
 	<h2>FS25 Playing Guide</h2>
 
 <p>
-<b>2024-07-05T11:21:26Z</b> updated.
+<b>2024-10-19T02:33:15Z</b> updated.
 </p>
 
 <p>
 Join discussion in <a href="https://www.pmctactical.org/forum/viewforum.php?f=80" target="_blank">PMC Tactical Forum FS25 General area</a>. Please read <a href="../17/guide.php">Farming Simulator 17 Guide</a>, <a href="../19/guide.php">Farming Simulator 19 (FS19) Guide</a> and <a href="../22/guide.php">Farming Simulator 22 (FS22) Guide</a> so you can get up to speed, its much of the same data.
 </p>
 
+<?php include("../include/fs25-placeholder-pages.php"); ?>
+
+<?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
+<?php include("/var/www/include/footer-start.php"); ?>
 <p><a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
 <p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
-<?php include("../include/w3-validator-logo.php"); ?>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
 </footer>
 
 </body>
---
+++ include/fs25-placeholder-pages.php
@@ -0,0 +1,11 @@
+<p>
+PMCTODO task, this page is a empty placeholder until Farming Simulator 25 (FS25) has been released and content created.
+</p>
+
+<p>
+My life will be extremely hectic because FS25 is released on nov 12th 2024 as we all know, but also <a href="https://sea-power.pmctactical.org" target="_blank">Sea Power (Naval Combat in the Missile Age)</a> gets released in <b>the same day!</b> so I am going to be completely overwhelmed how do I divide my time between these two titles, play, learn and document everything new.
+</p>
+
+<p>
+This is the reason an empty placeholder pages for many FS25 categories were created during october 2024 before the release when I have all the time in the world to edit stuff, so I don't have to spend that precious little time do I have each day after the game release to create these pages, I can just write notes and dump the text into already existing pages.
+</p>
