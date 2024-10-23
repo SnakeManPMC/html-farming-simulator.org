3cb1a5319b78ea9ee65e4c497a2f98f34917cd08
Snake Man <snakeman@phnet.fi>
2024-10-23 14:16:30 +0000

    brought links up to date

--- links.php
+++ links.php
@@ -7,37 +7,44 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- google analytics -->
 <?php include("include/google-analytics.php"); ?>
 
 </head>
 <body>
 
 <header>
-	<h1><b>PMC Farming Simulator Links</b></h1>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>PMC Farming Simulator Links</h1>
+
+<p>
+Useful links for Farming Simulator community.
+</p>
+<?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
 	<h2>PMC Web Site Links</h2>
 
-<p><a href="https://www.elitedangerous.net" target="_blank">EliteDangerous.net</a>, Elite Dangerous space exploration game.</p>
-<p><a href="https://www.pmctactical.org" target="_blank">PMC Tactical</a>, Falcon 4.0, Operation Flashpoint, ArmA 1 (Armed Assault), ArmA 2, ArmA 3, addons/mods.</p>
-<p><a href="https://pmc.editing.wiki" target="_blank">PMC Editing Wiki</a>, ofp/arma and Falcon 4.0 editing tutorials.</p>
-<p><a href="https://www.scenariohq.com" target="_blank">Scenario HeadQuarters</a>, ofp/arma missions and campaigns.</p>
-<p><a href="https://www.silenthunter.info" target="_blank">SilentHunter.info</a>, Silent Hunter 3, Silent Hunter 4, Silent Hunter 5 (SH5) world world 2 submarine simulator.</p>
-<p><a href="https://www.vtemod.com" target="_blank">Vietnam: The Experience</a>, the leading and most detailed Vietnam War Modifications for PC games.</p>
+<?php include("/var/www/include/links.php"); ?>
 
-
+<!--
 	<h2>Friends Of Ours Web Site Links</h2>
 
 <p><a href="https://www.fsasmc.com" target="_blank">fsasmc.com</a>, Farming Simulator American Style Community</p>
-
+-->
+<?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
+<?php include("/var/www/include/footer-start.php"); ?>
 <p><a href="index.php" class="button">PMC Farming Simulator root page</a></p>
 <p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
-<?php include("include/w3-validator-logo.php"); ?>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
 </footer>
 
 </body>
