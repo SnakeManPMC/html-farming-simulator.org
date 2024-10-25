aa5a33081b74f62cbc8ea4ca50da0c4a0ecc8333
Snake Man <snakeman@phnet.fi>
2024-10-25 11:41:07 +0000

    fs22 mods-tractor-trailer page created

---
+++ 22/mods-tractor-trailer.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Tractor Trailer - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Tractor Trailer - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Tractor Trailer - PMC Farming Simulator 22</h1>
+<p>Mods Tractor Trailer Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Tractor Trailer FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+
+<div class="mod_release_Vehicle">
+	<h2>Penta DB50</h2>
+
+<p>
+v1.0 by FS17 Nordvarg, FS19, 22 SB_Farms. From modDesc: Capacity: 55000 l.
+</p>
+
+<p>
+FS22_pentaDB50.zip
+</p>
+
+<p>
+Notes: 2023-08-31T15:29:00Z Tested no errors, no issues. Solid looking silage wagon.
+</p>
+
+<p>
+<a href="https://files.modhub.us/download?file=/15/2022-10/penta-db50-wagon/qktzf1666624263/1666624263-FS22_pentaDB50.zip&fileName=FS22_pentaDB50.zip" target="_blank">FS22_pentaDB50.zip</a>, 
+<a href="https://www.modhub.us/farming-simulator-22-mods/penta-db50-wagon" target="_blank">Modhub.us</a>
+</p>
+</div>
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
--- 22/mods.php
+++ 22/mods.php
@@ -64,6 +64,7 @@ Join discussion in <a href="https://www.pmctactical.org/forum/viewtopic.php?f=79
 <p><a href="mods-sprayer.php">Sprayer</a> liquid fertilizer or herbicide applicator.</p>
 <p><a href="mods-spreader.php">Spreader</a> manure spreader.</p>
 <p><a href="mods-subsoiler.php">Subsoiler</a> one kind of plow/digger/ripper.</p>
+<p><a href="mods-tractor-trailer.php">Tractor Trailer</a> trailer pulled by a tractor.</p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
