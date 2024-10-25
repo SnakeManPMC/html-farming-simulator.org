4e1eac35194a9cfa41444c8dd03e26fc30fedd59
Snake Man <snakeman@phnet.fi>
2024-10-25 06:46:05 +0000

    fs22 mods-placeable-production page created

---
+++ 22/mods-placeable-production.php
@@ -0,0 +1,406 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Placeable Production - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Placeable Production - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Placeable Production - PMC Farming Simulator 22</h1>
+<p>Mods Placeable Production Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Placeable Production FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>Thompson Factory</h2>
+
+<p>
+v1.0 by MRThompson_tv. From modDesc: Factory that will produce seeds,fertilizer,herbicide and fuel.<br>
+Production inputs: Wheat + Barley = seeds , inputs: Oat + Canola = seeds<br>
+inputs: Straw + Water = Fertilizer<br>
+inputs: Liquidmanure + Water = Herbicide<br>
+inputs: Sugarcane + Corn + Sorghum + Woodchips = Diesel Fuel
+</p>
+
+<p>
+FS22_Thompson_Factory.zip
+</p>
+
+<p>
+Notes:
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://files.modhub.us/download?file=/15/2021-12/fs22-thompson-factory/dcutg1638863640/1638863640-FS22_Thompson_Factory.zip&fileName=FS22_Thompson_Factory.zip" target="_blank">FS22_Thompson_Factory.zip</a>,
+<a href="https://www.modhub.us/farming-simulator-22-mods/fs22-thompson-factory" target="_blank">modhub.us</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Biodieselherstellung</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_Biodieselherstellung.zip
+</p>
+
+<p>
+Notes: Diesel production. This is german garbage with the authors facebook and twitter whatever crap icons and name textured on the side, complete trash, avoid this.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="" target="_blank">MISSING_LINK_SORRY</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>German saat, duenger and raffinerie production</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_Saatgutherstellung.zip, FS22_Duengerherstellung.zip, FS22_Raffinerie.zip
+</p>
+
+<p>
+Notes: sigh, german language file name obviously, but also some www .de advertisement text by the side of the building. Immediate delete.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="" target="_blank">MISSING_LINK_SORRY</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Kalkherstellung_V3</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_Kalkherstellung_V3.zip
+</p>
+
+<p>
+Notes: file name is german language but the model/textures themselves are just the rock crusher without ads (unless I missed them), however the production chains menu text is in german unlike the three other production mods from this author which were english in the menu. Immediate delete.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="" target="_blank">MISSING_LINK_SORRY</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Fert_Seed_Chemical</h2>
+
+<p>
+v by Parzival. From modDesc:
+</p>
+
+<p>
+FS22_Fert_Seed_Chemical.zip
+</p>
+
+<p>
+Notes: inputs: barley, oat, wheat and water. output: seeds. 500 grain + 50 water == 450 seeds.
+inputs: manure and water. output solid and liquid fertilizer. 500 manure == 450 solid fertilizer, 500 manure + 250 water == 750 liquid fertilizer. Capacity barley, oat, wheat 144k liters. Seeds, manure 100k liters. Solid fert, liquid fert and water 50k liters.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="" target="_blank">MISSING_LINK_SORRY</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Seeds Factory</h2>
+
+<p>
+v1.1 by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes:
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://farmingsimulator22mods.com/seeds-factory-v1-1-fs22/" target="_blank">farmingsimulator22mods.com</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Midwest Production And Feed Pack</h2>
+
+<p>
+v1.0.0.2 by . From modDesc:
+</p>
+
+<p>
+FS22_midwestProductionPack.zip
+</p>
+
+<p>
+Notes:
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://fs22.com/wp-content/uploads/2022/10/FS22_midwestProductionPack.zip" target="_blank">FS22_midwestProductionPack.zip</a>,
+<a href="https://fs22.com/farming-simulator-22-mods/packs/midwest-production-and-feed-pack-v1-0/" target="_blank">fs22.com</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Multi Production With Bale Function</h2>
+
+<p>
+v1.3.1 by . From modDesc:
+</p>
+
+<p>
+FS22_Multi_Production_Factory_1_3_1.zip, FS22_Multi_Production_Factory.zip
+</p>
+
+<p>
+Notes:
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://fs22.com/wp-content/uploads/2023/01/FS22_Multi_Production_Factory_1_3_1.zip" target="_blank">FS22_Multi_Production_Factory_1_3_1.zip</a>,
+<a href="https://fs22.com/wp-content/uploads/2022/09/FS22_Multi_Production_Factory.zip" target="_blank">FS22_Multi_Production_Factory.zip</a>,
+<a href="https://fs22.com/farming-simulator-22-mods/placeable-objects/multi-production-factory-v1-0/" target="_blank">fs22.com</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Crop Inputs Co-operative</h2>
+
+<p>
+v1.0.0.1 by Bcbuhler. From modDesc: Welcome to the Prairie Farmers Co-op. Inspired by various North American farm input sales locations mainly in the Canadian Prairies. There's a lot of different functionality for you to add to your game whether it's for roleplay or just you want a good realistic place to buy your crop inputs. The production pallet buy points function like a normal production but instead of requiring an input they have a high operating cost to simulate buying pallets. So simply enable the production until it spawns the number of pallets you want. The money will come out of your account at the end of the hour depending on how much product you have purchased. Keep in mind only full pallets will spawn. Also they can be used to turn bulk product into pallet form. For the seed shed the tipping point is the bins (you will need an auger) and for the chemical shed it is in front of the big door.
+</p>
+
+<p>
+FS22_FarmersCoop.zip
+</p>
+
+<p>
+Notes: 2024-05-26T06:19:00Z tested, no errors no issues. Hmm I'm very confused about this placeable building pack, yes you get liquid fertilizer, herbicide and seeds from these, but... umm. There are two production buildings and one just simple buy point of some sort. Production buildings have input for liquid fertilizer with ZERO (0) input requirement, it outputs liquid fertilizer, spawns tank pallets to the building yard. Same for herbicide. The other building is the same but for seeds. I am really not sure what is going on here, if you want to have a buy point, why on earth would you create this kind of complex production point 0 input to output given material, I just don't get it. A simple buying station placeable is million times better and smaller, or is the whole point of this weird production to buy stuff building setup a cosmetic one, to have large american looking farming industrial type buildings for you to place, no idea <img src="../images/smileys/icon_confused.gif" alt="Smiley :?" loading="lazy">
+</p>
+
+<p>
+Anyways. When you spawn seeds they will be placed inside a building in a corner from where they have to be picked up by telehandler or at least very small seeder/trailer which can maneuvre itself into such bad spot. The liquid production building spawns liquid fertilizer and herbicide tanks right next to each other making it impossible to start refilling specific type to an empty sprayer/tanker (once you have either one in the vehicles tank then you can just refill more of the same). This is very bad design here, game-play would be horrible. Hey maybe I just don't understand the functionality here, but I would never ever use these in my savegames or terrains, these are just cumbersome badly designed bizarre production buildings. If you know how to properly use these buildings, please post on <a href="https://www.pmctactical.org/forum/viewforum.php?f=77" target="_blank">PMC Tactical forum FS22 area</a>.
+</p>
+
+<p>
+More info from Bcbuhler: Buy point for liquid fertilizer and herbicide as well as a production for buying tanks of liquid fertilizer and herbicide. Buy point for fertilizer and lime. Buy point for seeds as well as a production for buying proboxes of seeds. Thanks to Legacy Ag for letting me use his Probox. The production pallet buy points function like a normal production but instead of requiring an input they have a high operating cost to simulate buying pallets. So simply enable the production until it spawns the number of pallets you want. The money will come out of your account at the end of the hour depending on how much product you have purchased. Keep in mind only full pallets will spawn. Also they can be used to turn bulk product into pallet form. For the seed shed the tipping point is the bins (you will need an auger) and for the chemical shed it is in front of the big door.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://cdn70.giants-software.com/modHub/storage/00249578/FS22_FarmersCoop.zip" target="_blank">FS22_FarmersCoop.zip</a>,
+<a href="https://www.farming-simulator.com/mod.php?mod_id=249578" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Grain Mill Extended</h2>
+
+<p>
+v1.0 by OmaTana. From modDesc: This grain mill can produce flour from maize in addition to the other grain types. There are also recipes that give you additional pigfood as a byproduct of the flour production. The grain mill looks slightly different than the base game grain mill.
+</p>
+
+<p>
+FS22_grainMillExtended.zip
+</p>
+
+<p>
+Notes: 2023-08-28T08:32:00Z Tested no errors, no issues. Dunno what to say as I have not much experience with the original grain mill, this extended one works as advertised I guess.
+</p>
+
+<p>
+<a href="https://cdn20.giants-software.com/modHub/storage/00241513/FS22_grainMillExtended.zip" target="_blank">FS22_grainMillExtended.zip</a>,
+<a href="https://www.farming-simulator.com/mod.php?mod_id=241513" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Seed Production</h2>
+
+<p>
+v1.0.0.1 by ZoddelZockt. From modDesc: The mod includes a seed production that produces seed from wheat, barley, oats, canola, soybeans, corn and millet. There is an individual production line for each crop. Besides the grain, herbicide and liquid fertilizer are also needed.
+Capacities: Liquid fertilizer and herbicide 10000l each. Wheat, barley, oats, canola, soybeans, corn and millet each 100000l Seeds 1000000l
+</p>
+
+<p>
+FS22_SeedProductionPlus.zip
+</p>
+
+<p>
+Notes: 2023-08-28T08:51:00Z Tested no errors, no issues. Basic seed production building placeable. Required input materials: liquid fertilizer and herbicide on top of regular grains to produce seeds. So at savegame start you have to buy few tanks of input material to get going. Model has no dump grate on the ground or even trigger indicating warning stripes, you just dump into concrete or what that light colored texture is, not a showstopper, but doesn't look / feel nice either. I brought in liquid fertilizer and herbicide tanks with a front loader into the input location and nothing happened, then from https://cdn5.giants-software.com/modHub/storage/00241966/screenshot1.jpg screenshots I saw example where tractor is pulling some small liquid tanker, that must be the trailer to use for those liquids, it says nowhere on description about that, sigh. Looks like lizard MKS 8 and MKS 32 liquid tanker trailers can be used for these liquids, base game equipment. Using Lizard MKS 8 worked OK, first refill from liquid fertilizer or herbicide tank, then overload to seed production placeable, I just don't understand why you cannot simply bring those the dealership purchased default tanks into the location and have them triggered "fill" on the placeable, oh well now I know how it works and sure 32,000 liters semi-truck pulled tanker trailer is much nicer than hauling some 2k liter tanks individually anyways. As there are several input grains you can fill and activate them all, however having them all active does not increase seed producing and therefore liquid fertilizer and herbicide use, but I guess its a nice feature to put in 100k barley and 100k wheat there, perhaps 100k sorghum too, those high yielding crops you are not really selling, then seed production building will eat away each evenly, that is nice endurance but looking at the 3d size of the building of course highly unrealistic. When you take away seeds, you refill from the building into a trailer, obviously, that is very fast, there is no animation whatsoever, no auger pipe to fill your trailer, it just magically appears from nowhere (kind of like pallets and tanks in all fairness). Seed production rate is about 1 liter per second, it feels that way. Cycles per month 600, cost $120. It takes 100 liters of grain + 10 liters of liquid fertilizer + 5 liters of herbicide to produce 100 liters of seeds, I like the grain == seeds 1:1 ratio, it makes sense, this is just seed treatment. Production options for seeds says "Spawn", but it doesnt spawn anything, didn't try distribute as I have no other buildings, but selling worked as expected, the whole contents of seeds capacity will be sold at top of the hour once that option is selected, there will be no profit taking green info display text, but the money appears in your budget finances view panel. Obviously nobody is running this production chains building to sell seeds, but I just tested the feature. As said in description, seeds storage capacity is 1 million liters, now that is a bit nutty, after lets say 500k liters of seeds... what difference does another 500k make anymore, that arbituary 1 million capacity is indeed pretty crazy, sure I'll take it but yeah kind of wish it would be realistic relative to the 3d building size. So whats the verdict; do I use this rather than GtX Seed Cleaner 1200-LG? Hmm only thing that bothers me in seed cleaner is that its not production chains compatible placeable, so I am now eager for production chains placeables so yeah I currently prefer my seed making into for example this "Seed Production" placeable by ZoddelZockt.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://cdn71.giants-software.com/modHub/storage/00241966/FS22_SeedProductionPlus.zip" target="_blank">FS22_SeedProductionPlus.zip</a>,
+<a href="https://www.farming-simulator.com/mod.php?mod_id=241966" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>US Animal Food</h2>
+
+<p>
+v1.0 by FiB7. From modDesc: The animal feed system in US design for several animals on the farm. In order to get the animal feed system in operation, you also need diesel. The diesel is consumed in the animal feed plant and you don't get it back. Capacity: 500,000 liters. animal feed plant: - Inputs: diesel. Total mixed ration: - Input: grass, hay and silage, - Output: Total mixed ration.
+</p>
+
+<p>
+FS22_AnimalFood.zip
+</p>
+
+<p>
+Notes: 2023-08-29T02:50:00Z Tested no errors, no issues. This is brown brick building factory looking big placeable. Production chains placeable. Has 500k liter capacity on everything input and output. Incoming materials: corn, sunflowers, canola, potatoes, diesel, sugar beet, hay, straw and silage. Output materials: pig food and total mixed ration. Has four different setups for pig food, for 60 and 120 pigs. Has three different setups for TMR, 85, 170 and 255 cows.
+</p>
+
+<p>
+<a href="https://cdn20.giants-software.com/modHub/storage/00241005/FS22_AnimalFood.zip" target="_blank">FS22_AnimalFood.zip</a>,
+<a href="https://www.farming-simulator.com/mod.php?mod_id=241005" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Cow Feed Mixer</h2>
+
+<p>
+v1.0.0.1 by ZoddelZockt. From modDesc: The mod includes a feed mixing plant for TMR. Silage, hay, straw and mineral feed can be delivered as bulk material. The plant must be made of mixed rations for cows. Construction cost: 150000 €, Cycles per month: 600, Cost per cycle: 1 €. Capacity: Hay and straw: 1000000 l, Silage: 1800000 l, Mineral feed: 200000 l, Mixed ration: 4000000 l.
+</p>
+
+<p>
+FS22_CowFoodMixer.zip
+</p>
+
+<p>
+Notes: 2023-09-11T11:39:00Z Tested no errors, no issues. Production rate seems insanely fast, when factory is running its info box numbers flicker by so fast its crazy, when time accelerating 1000 speed it only took few in-game hours to consume 100,000 liters of silage. Also mineral feed is not necessary for cows, they technically do not need it its more like role playing stuff in FS22, so buying mineral feed in 1k liter pallets from the dealership seems like an awful grind just to keep this production building running, you would have to get yet another refill station just to buy mineral feed in bulk which also adds insult to injury so to speak, "get a placeable, have to use yet another mod to run it.", or something along those lines. But yes in the end of the day, this will produce cow feed Total Mixed Ration (TMR) in big bulk scale for those big beef cattle or dairy cow operations.
+</p>
+
+<p>
+<a href="https://cdn20.giants-software.com/modHub/storage/00239557/FS22_CowFoodMixer.zip" target="_blank">FS22_CowFoodMixer.zip</a>,
+<a href="https://www.farming-simulator.com/mod.php?mod_id=239557" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
