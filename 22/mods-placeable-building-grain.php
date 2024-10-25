bc1e21dec8bb03683016ab1a808053bf937e2e88
Snake Man <snakeman@phnet.fi>
2024-10-25 05:14:39 +0000

    fs22 mods-placeable-building-grain page created

---
+++ 22/mods-placeable-building-grain.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Placeable Building Grain - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Placeable Building Grain - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Placeable Building Grain - PMC Farming Simulator 22</h1>
+<p>Mods Placeable Building Grain Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Placeable Building Grain FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>Grain storage 50x20</h2>
+
+<p>
+v1.0 by Muzhik TV. From modDesc: Grain storage 50x20m.
+</p>
+
+<p>
+AngarNG.zip
+</p>
+
+<p>
+Notes: 2024-05-18T23:54:00Z testing done in FSG Mod Assistant v4.0.4, unused files so delete.
+</p>
+
+<p>
+Includes unused files:
+</p>
+<pre>
+isxodnik/ugol.fbx
+isxodnik/ugol2.fbx
+isxodnik/untitle122d.fbx
+isxodnik/untitled.blend
+isxodnik/untitled.blend1
+isxodnik/untitled.fbx
+</pre>
+
+<p>
+<a href="https://fs22.com/wp-content/uploads/2023/01/AngarNG.zip" target="_blank">AngarNG.zip</a>,
+<a href="https://fs22.com/farming-simulator-22-mods/placeable-objects/grain-storage-50x20-v1-0/" target="_blank">fs22.com</a>
+</p>
+</div>
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
