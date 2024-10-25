4c8368b06025611c1293ba190ce0fb0f87fd3101
Snake Man <snakeman@phnet.fi>
2024-10-25 07:17:06 +0000

    fs22 mods-placeable-warehouse page created

---
+++ 22/mods-placeable-warehouse.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Placeable Warehouse - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Placeable Warehouse - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Placeable Warehouse - PMC Farming Simulator 22</h1>
+<p>Mods Placeable Warehouse Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Placeable Warehouse FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>Pallet Warehouse</h2>
+
+<p>
+v1.1 by SidoxGamer. From modDesc: For all pallets with 375000 capacity
+</p>
+
+<p>
+FS22_palletStorage.zip
+</p>
+
+<p>
+Notes: With the placeable Pallet Warehouse you can store all the pallets you can make in the game. 375000 capacity.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://files.modhub.us/download?file=/15/2021-12/pallet-warehouse-for-all-pallets-with-375000-capacity-v1100/vgekq1638733152/1638733152-FS22_palletStorage.zip&fileName=FS22_palletStorage.zip" target="_blank">FS22_palletStorage.zip</a>
+<a href="https://www.modhub.us/farming-simulator-22-mods/pallet-warehouse-for-all-pallets-with-375000-capacity-v1100" target="_blank">modhub.us</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Pallet Warehouse</h2>
+
+<p>
+v1.0 by . From modDesc:
+</p>
+
+<p>
+FS22_palletWarehouse_ASM.zip
+</p>
+
+<p>
+Notes: 
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://fs22.com/wp-content/uploads/2023/01/FS22_palletWarehouse_ASM.zip" target="_blank">FS22_palletWarehouse_ASM.zip</a>, 
+<a href="https://fs22.com/farming-simulator-22-mods/placeable-objects/pallet-warehouse-v1-0-2/" target="_blank">fs22.com</a>
+</p>
+</div>
+
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
