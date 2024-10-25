3afa555d657f9870c025ebab63963e0a446544b7
Snake Man <snakeman@phnet.fi>
2024-10-25 06:06:38 +0000

    fs22 mods-placeable-building-silo page created

---
+++ 22/mods-placeable-building-silo.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Placeable Building Silo - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Placeable Building Silo - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Placeable Building Silo - PMC Farming Simulator 22</h1>
+<p>Mods Placeable Building Silo Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Placeable Building Silo FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>American Fermenting Silos</h2>
+
+<p>
+v1.0 by VertexDezign. From modDesc: These classic american upright silos should not be missing on any farm. With three sizes to choose from, every farmer can select the right silo for their operation. Small Fermenting Silo: Capacity: 480.000 l, Production Speed: 3.500 l/hr. Medium Fermenting Silo: Capacity: 660.000 l, Production Speed: 3.500 l/hr. Large Fermenting Silo: Capacity: 840.000 l, Production Speed: 3.500 l/hr.
+</p>
+
+<p>
+FS22_americanFermentingSilos.zip
+</p>
+
+<p>
+Notes: 2023-08-29T02:35:00Z Tested no errors, no issues. These are the tall dark blue / purple-ish tube alike silos with either none, american or canadian flag on them. These are production chains placeables, accepting chaff, hay and grass with production of silage. Nice looking fermenting silos, definitely gives that north american farming look to your farm yard. I might start to use these in my farms even as just cosmetic decoration as they look so nice (I rarely do silage).
+</p>
+
+<p>
+<a href="https://cdn20.giants-software.com/modHub/storage/00242684/FS22_americanFermentingSilos.zip" target="_blank">FS22_americanFermentingSilos.zip</a>, 
+<a href="https://www.farming-simulator.com/mod.php?mod_id=242684" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
