a6c8cb267577811a599000392a5be4e7021d8d66
Snake Man <snakeman@phnet.fi>
2024-10-25 05:03:37 +0000

    fs22 mods-placeable-building-vehicle page created

---
+++ 22/mods-placeable-building-vehicle.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Placeable Building Vehicle - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Placeable Building Vehicle - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Placeable Building Vehicle - PMC Farming Simulator 22</h1>
+<p>Mods Placeable Building Vehicle Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Placeable Building Vehicle FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>American Pack</h2>
+
+<p>
+v1.1 by Lancyboi. From modDesc: American style workshop. Open Glass Window Barn. Large Barn (Real life barn based in michigan america).
+</p>
+
+<p>
+FS22_American_pack.zip
+</p>
+
+<p>
+Notes: 2023-08-28T17:12:00Z Tested no errors, no issues. Several different color options for side walls, but not for the roof, doors and edges. Overall very nice basic american vehicle sheds. Too bad the title of this mod is too generic, I mean "american pack", really? pack of what... geez.
+</p>
+
+<p>
+<a href="https://cdn71.giants-software.com/modHub/storage/00260718/FS22_American_pack.zip" target="_blank">FS22_American_pack.zip</a>,
+<a href="https://www.farming-simulator.com/mod.php?mod_id=260718" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<!-- category vehicles -->
+</div>
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
