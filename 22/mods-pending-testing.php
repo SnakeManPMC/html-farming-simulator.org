0af46ec9f3dfd98557492eb92116a73683a115fe
Snake Man <snakeman@phnet.fi>
2024-10-25 12:32:45 +0000

    fs22 pending testing mods added

--- 22/mods-pending-testing.php
+++ 22/mods-pending-testing.php
@@ -33,6 +33,407 @@ Back to <a href="mods.php">Mods Main Page</a>. Post a message to <a href="https:
 
 <div class="wrapper_mod_vehicles">
 
+<div class="mod_release_Vehicle">
+	<h2>Production Pack 1 By Stevie</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Included mods are : Grass Dryer, Fermenting Silo, TMR mixer, Pig Food Mixer, Manure Mixer, Seed Maker, Fertilizer Maker and Lime Production.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://www.modhub.us/farming-simulator-22-mods/production-pack-1-by-stevie" target="_blank">modhub.us</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_Fed_Mods_Pack.zip
+</p>
+
+<p>
+Notes: Multisilos 1.2 FS22_Fed_Mods_Pack.zip
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://sharemods.com/z9jj88vrw1xx/FS22_Fed_Mods_Pack.zip.html" target="_blank">FS22_Fed_Mods_Pack.zip</a>,
+<a href="" target="_blank">MISSING_LINK_SORRY</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Düngerproduktion</h2>
+
+<p>
+v1.1 by . From modDesc:
+</p>
+
+<p>
+FS22_Fed_Produktions_Pack.zip
+</p>
+
+<p>
+Notes:
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://sharemods.com/2et1vtw6tzr9/FS22_Fed_Produktions_Pack.zip.html" target="_blank">FS22_Fed_Produktions_Pack.zip</a>,
+<a href="https://www.fedaction-letsplay.de/landwirtschafts-simulator/ls22/ls22-mods-download/" target="_blank">www.fedaction-letsplay.de</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Herbicide Production. This mod includes a herbicide production facility. Here, herbicide can be produced from water, acetic acid, and urea. The herbicide can be sold to wholesale or used for weed control. Acetic acid can be produced in the acetic acid production facility. Urea can be produced in the urea production facility. Placeable Shop Category: Productions -> Factories Construction Cost: $111,000 Incoming Goods: Water, Acetic Acid, Urea, Herbicide Outgoing Goods: Herbicide Production Recipes: Mineral Feed: 800 liters of water + 100 liters of acetic acid + 100 liters of urea yield 1000 liters of herbicide (48 cycles/month | $240/month) Features: - Switchable light - Production information panel displayable - Trigger marker displayable - In/Out symbols displayable. This mod is not hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_HerbicideProduction&language=en" target="_blank">agraz-mods.de</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Wholesale. At this wholesale all products from the basegame can be sold. Contract work orders will also target this point of sale. Construction cost: 100000 $. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_Wholesale&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Wholesale Maize Plus Edition. At this wholesale all products from the basegame can be sold. Contract work orders will also target this point of sale. Construction cost: 100000 $ Supported Mods/DLC: - Platinum DLC - FS22_SeedPotatoFarmBuildings - FS22_MaizePlus. This mod is not hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_Wholesale_MP_Edition&language=en" target="_blank">agraz-mods.de</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Heating Plant. The CHP plant can burn hay, straw, wood chips, bark mulch, hay pellets, straw pellets, wood pellets and manure pellets and produce heat, electricity and ash as a waste material. The pellets can be produced with a pellet mill. Electricity and heat can be sold directly or distributed directly to other productions that use electricity and/or heat using the distribute function. The ash can either be sold at wholesale or processed in productions that process ash. Combined heat and power plant: Purchase price: 237000 $ Capacity: 1000000 l per filling type Daily cost: 50 $. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_HeatingPlant&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Urea Production. With urea production can be obtained from liquid manure urea. The mod includes urea production, a silo for urea, a trailer for all liquids and a trailer for all liquids. The urea can be sold either directly through production or at wholesale. Urea production: Construction cost: 85000 $ Capacity: - Liquid manure: 1000000 l - Urea: 100000 l Urea silo: Construction cost: 10000 $ Capacity: 10000000 l Mks8: Purchase price: 25000 $ - 40000 $ Capacity: 8000 l - 50000 l Mks32: Purchase price: 68000 $ - 118000 $ Capacity: 32000 l - 150000 l. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_UreaProduction&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Lime And Beet Pulp Production. 2 different production lines offer the possibilities 1. to create lime from stone and 2. to create beet pulp from beets. Stonecrusher: Construction cost: 10000 € Filling volume: 1000000 l per filling type Production rate: 4000 stones = 2000 lime Cycles per hour: 10 Sugarbeetcrusher: Construction cost: 10000 € Filling volume: 1000000 l per filling type Production rate: 4000 sugar beets = 2000 sugar beet pulp Cycles per hour: 15. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_LimeAndSugarbeetCutProduction&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Pig Feed Mixer</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_PigFoodMixerMulti.zip
+</p>
+
+<p>
+Notes: Pig Feed Mixer. This pig feed mixer can mix feed for pigs from different ingredients. Fruits from 4 categories are needed for mixing. This mixer can process potato, sugar beet, sugar beet pulp, corn, millet, oat, barley, wheat, canola, soybean and sunflower. Construction cost: 195000 $ Capacities: Ingredients: 100000 l each Pig feed: 10000000 l. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_PigFoodMixerMulti&language=en" target="_blank">agraz-mods.de</a>
+<a href="https://www.farming-simulator.com/mod.php?mod_id=254434" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Fuel Refinery Revamp Edition. With this fuel refinery fuels can be produced. With the solar and wind power plant on the roof, energy for electric vehicles can be generated. In addition, gas and diesel can be used to produce electrical energy. Rapeseed, sunflowers, olives, soybeans, sugarcane, sugar beets, potatoes and wood chips can be processed into liquid fuel. Methane, compost and pig feed are produced in varying amounts depending on the feedstock used. Liquid fuel can be produced from vegetable oils without waste. Vehicles can be refueled directly. If the fuel is not needed, it can be sold directly. Diesel and methane can be transported with a suitable trailer to the wholesale and sold there. Revamp 1.2.0.0 or newer is required to use this mod and to enjoy the advantages of the Revamp script, e.g. the respective fill levels can be read directly from digital displays. Purchase price: 125000 € Storage capacity: Electric charge, methane: 10000 l Diesel, compost, pig feed, rape, sunflower, olives, soybeans, sugar cane, sugar beet pulp, potatoes, wood chips: 1000000 l Rapeseed oil, sunflower oil, olive oil, soybean oil: 1000000 l Cycles per month: 600 cycles Production cost per month: *Electric charge from air = 24 € *Electric charge from methane = 48 € *Electric charge from diesel = 72 € *Diesel from rapeseed = 120 € *Diesel from sunflowers = 120 € *Diesel from olives = 120 € *Diesel from soybeans = 120 € *Diesel from sugar cane = 240 € *Diesel from sugar cane = 240 € *Diesel from potatoes = 240 € *Diesel from wood chips = 360 € *Diesel from rapeseed oil = 600 € *Diesel from sunflower oil = 600 € *Diesel from olive oil = 600 € *Diesel from soybean oil = 600 €. This mod is not hosted on the Giants Modhub. This mod requires Revamp!
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_FuelRefineryRevampEdition&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Distillery. This mod includes a distillery for alcohol. It can produce brandy, grain, rum and vodka from various fruits. Water, blank plates and cartons are also needed for production. Waste products are compost, pig feed and molasses. Construction costs: 225000 € The molasses and the compost can be sold directly or, with a suitable transport vehicle, at the wholesale. The alcohol can be sold at the liquor store. Required modfs: Sawmill: FS22_SawmillWithBarkmulch.zip Production For Empty Pallets And Barrels: FS22_PalettAndBarrelProduction.zip Paper and Cardboard Production: FS22_PaperAndCardboardProduction.zip Liquor Store: FS22_LiquorStore.zip Recommended mods: Wholesale: FS22_Wholesale.zip. This mod is not hosted on the Giants Modhub. Required mods: Sawmill, Production For Empty Pallets And Barrels, Production For Paper And Cardboard, Liquor Shop
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_Distillerie&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Acetic Acid Production. The mod includes acetic acid production, silo for compost, silo for acetic acid, one trailer and one semi-trailer for all liquids. With the acetic acid production can be produced from silage - acetic acid, as a by-product is compost. Compost and acetic acid can be sold either directly at the storage silo or at the wholesale and further processed in still appearing productions. Acetic acid production: Price: 65000 $ Capacity: - Silage: 1000000 l - Compost: 1000000 l - Acetic acid: 100000 l Compost silo: Price: 10000 $ Capacity: 10000000 l Acetic acid silo: Price: 10000 $ Capacity: 10000000 l Mks8: Price: 25000 $ - 40000 $ Capacity: 8000 l - 50000 l Mks32: Price: 68000 $ - 118000 $ Capacity: 32000 l - 150000 l. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_AceticAcidProduction&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>PMCTODO NAME</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FILENAME
+</p>
+
+<p>
+Notes: Pellet Press. This mod includes a pellet mill, a silo for molasses, a silo for pellets and 2 universal liquid trailers. The pellet mill can produce hay, straw and wood pellets from hay, straw and wood chips, in combination with molasses and water. The pellets can be sold either directly through production or at wholesale. Molasses can be purchased at the molasses silo. Pellet mill: Purchase price: 125000 $ Storage capacity: - Water 100000 l - Molasses 50000 l - Hay, straw and wood chips each 1000000 l - Hay, straw, wood pellets each 5000000 l Molasses silo: Purchase price: 10000 $ Daily Up keep: 5 $ Capacity: 10000000 l Pellet silo: Purchase price: 23500 $ Daily Up keep: 10 $ Capacity: 10000000 l MKS8: Capacity: 8000 l to 50000 l Price: 25000 $ to 40000 $ MKS32: Capacity: 32000 l to 150000 l Price: 68000 $ to 118000 $. This mod is hosted on the Giants Modhub.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="" target="_blank">FILENAME</a>,
+<a href="https://agraz-mods.de/moddetails.php?modname=FS22_PelletClench&language=en" target="_blank">agraz-mods.de</a>
+<a href="" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
 <!-- category vehicles -->
 </div>
 
