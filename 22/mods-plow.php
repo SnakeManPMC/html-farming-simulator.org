2fd8200e84fd68db4ef1c7b6b504af55479352d3
Snake Man <snakeman@phnet.fi>
2024-10-25 10:37:38 +0000

    fs22 mods-plow page created

---
+++ 22/mods-plow.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Plow - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Plow - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Plow - PMC Farming Simulator 22</h1>
+<p>Mods Plow Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Plow FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>Vaederstad Nz Ultimate 2545 - Square Plow</h2>
+
+<p>
+v1.1.2 by Xinterp. From modDesc: A cultivator that has been equipped with new teeth that cuts through the soil like butter, this unit now plows instead of cultivates.<br>
+14.25m Spread Swath<br>
+16mph Operating Speed (26kmh)
+</p>
+
+<p>
+FS22_NZUltimate2545.zip
+</p>
+
+<p>
+Notes: 
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://files.modhub.us/download?file=/15/2021-12/vaederstad-nz-ultimate-2545-square-plow-v1120/zerfg1638761130/1638761130-FS22_NZUltimate2545.zip&fileName=FS22_NZUltimate2545.zip" target="_blank">FS22_NZUltimate2545.zip</a>, 
+<a href="https://www.modhub.us/farming-simulator-22-mods/vaederstad-nz-ultimate-2545-square-plow-v1120" target="_blank">modhub.us</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Rostselmash K-12200 CultiPlow</h2>
+
+<p>
+v1.0.0.3 by DancerVLT69. From modDesc:
+</p>
+
+<p>
+FS22_Rostselmash_K12200_CultiPlow.zip
+</p>
+
+<p>
+Notes: Unrealistic plow, doesn't require enough horsepower for a plow of this width! The K 12200 has been converted into a subsoiler. This means that 'plowing' and cultivating can be done in one operation. Working width: 12.2 meters, speed 18 km/h, required horsepower 325hp.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://files.modhub.us/download?file=/15/2021-12/rostselmash-k-12200-cultiplow-v1003/tgjei1639472385/1639472385-FS22_Rostselmash_K12200_CultiPlow.zip&fileName=FS22_Rostselmash_K12200_CultiPlow.zip" target="_blank">FS22_Rostselmash_K12200_CultiPlow.zip</a>, 
+<a href="https://www.modhub.us/farming-simulator-22-mods/rostselmash-k-12200-cultiplow-v1003" target="_blank">modhub.us</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>John Deere 2410</h2>
+
+<p>
+v1.2.2 by J.A. From modDesc: Power: 320hp, Working width: 16.7m.
+</p>
+
+<p>
+FS22_JohnDeere2410.zip
+</p>
+
+<p>
+Notes: 2023-10-19T06:51:00Z Tested no errors, no issues. Perhaps one issue could be dirt / dust particles this implement generates, if you have a poor GPU don't expect to zoom close to this implement because the massive amount of dust particles generated as that drags your FPS down to 30FPS or so, but this should not be a problem with normal high power video cards.
+</p>
+
+<p>
+<a href="https://cdn71.giants-software.com/modHub/storage/00244319/FS22_JohnDeere2410.zip" target="_blank">FS22_JohnDeere2410.zip</a>, 
+<a href="https://www.farming-simulator.com/mod.php?mod_id=244319" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Case Ecolo-Til 2500</h2>
+
+<p>
+v1.0 by ARM-Team. From modDesc: The Ecolo-Til 2500 is an in-line ripper to effectively manage post-harvest residue through sizing, placement and incorporation. You can modify it with a long attacher or a 3-point hitch. It's foldable with a working width of 8.5m, - Required power: 310 HP
+</p>
+
+<p>
+FS22_CaseEcoloTil2500.zip
+</p>
+
+<p>
+Notes: 2023-08-30T03:06:00Z Tested no errors, no issues. Has the dealership options to three point hitch or extension, extension is like a "draw bar" or whats its called so you tow the implement behind you instead of directly connect it to your tractor (which is three point hitch), extension includes medium size wheels to be used when implement is lifted up. Kind of nice to have that kind of different connection option, no idea whats the difference is in-game though, guess it comes to game-play farming preference. Working speed 12km/h is not the fastest, but then again this is a deep ripper so don't expect any fast speed passes. Overall very nice implement for plowing / ripping.
+</p>
+
+<p>
+<a href="https://cdn70.giants-software.com/modHub/storage/00235731/FS22_CaseEcoloTil2500.zip" target="_blank">FS22_CaseEcoloTil2500.zip</a>, 
+<a href="https://www.farming-simulator.com/mod.php?mod_id=235731" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>2623 Tandom Disc</h2>
+
+<p>
+v1.0.0.3 by Heavyd. From modDesc: 2623 5-Section 49'3" Disc.
+</p>
+
+<p>
+FS22_JohnDeere_2623.zip
+</p>
+
+<p>
+Notes: 2023-09-01T05:08:00Z Tested error spam in the log, delete. This plow actually has been on my essential mod list for as long as I remember in FS22, however just recently I spotted an error on it, no idea if this error was there since the beginning or is this some FS22 v1.12 or other recent game engine change caused error. Required horsepower 400hp, working width 15m. Was very sorry to spot this error and had to let this mod go.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+2023-09-01 05:11   Warning (C:/FS22.Mods/Quarantine/FS22_JohnDeere_2623/JD2623.xml): Missing ai agent attachment definition for attachable vehicle
+</pre>
+
+<p>
+<a href="" target="_blank">FS22_JohnDeere_2623.zip</a>, 
+<a href="" target="_blank">MISSING_LINK_SORRY</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>John Deere 2100 Ripper Modified</h2>
+
+<p>
+v1.0 by LR. From modDesc: Working width:8.6 Meter(26,3ft), Working speed:8 and 15km/h, Power requirement: 450 HP.
+</p>
+
+<p>
+FS22_JohnDeere_Ripper2100.zip
+</p>
+
+<p>
+Notes: 2023-09-05T19:24:00Z Tested no errors, no issues. Generates proper nice plowing field ground texture.
+</p>
+
+<p>
+<a href="https://cdn70.giants-software.com/modHub/storage/00259044/FS22_JohnDeere_Ripper2100.zip" target="_blank">FS22_JohnDeere_Ripper2100.zip</a>, 
+<a href="https://www.farming-simulator.com/mod.php?mod_id=259044" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
