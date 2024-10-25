5848f194a5308bf26cd5ea6719b282961602ba50
Snake Man <snakeman@phnet.fi>
2024-10-25 05:57:36 +0000

    fs22 mods-placeable-container page created

---
+++ 22/mods-placeable-container.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mods Placeable Container - PMC Farming Simulator 22</title>
+<LINK href="../css.css" rel=stylesheet type="text/css">
+<META name="description" content="Mods Placeable Container - PMC Farming Simulator 22">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("../include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Mods Placeable Container - PMC Farming Simulator 22</h1>
+<p>Mods Placeable Container Farming Simulator 22 (FS22).</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>Mods Placeable Container FS22</h2>
+
+<div class="wrapper_mod_vehicles">
+
+<div class="mod_release_Vehicle">
+	<h2>Fuel Station</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_fuelStation.zip
+</p>
+
+<p>
+Notes: Capacity 10,000 liters, daily upkeep 60.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://cdn60.giants-software.com/modHub/storage/00227627/FS22_fuelStation.zip" target="_blank">FS22_fuelStation.zip</a>, 
+<a href="https://www.farming-simulator.com/mod.php?mod_id=227627" target="_blank">Giants Modhub</a>
+</p>
+</div>
+
+
+<div class="mod_release_Vehicle">
+	<h2>Meridian Universal Liquid Silo</h2>
+
+<p>
+v by . From modDesc:
+</p>
+
+<p>
+FS22_UniversalMeridianLiquidSilo.zip
+</p>
+
+<p>
+Notes: giants meridian in-game liquid tank which now accepts all liquid types not just fertilizer. ATTENTION: To empty the silo you need a liquid trailer that supports all liquids. You also must download my "Universal Liquid Transport" mod.
+</p>
+
+<p>
+Errors in the log:
+</p>
+<pre>
+</pre>
+
+<p>
+<a href="https://cdn69.giants-software.com/modHub/storage/00257220/FS22_UniversalMeridianLiquidSilo.zip" target="_blank">FS22_UniversalMeridianLiquidSilo.zip</a>, 
+<a href="https://www.farming-simulator.com/mod.php?mod_id=257220" target="_blank">Giangs modhub</a>
+</p>
+</div>
+
+
+<!-- category vehicles -->
+</div>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="mods.php" class="button">FS22 Mods root page</a> <a href="../index.php" class="button">PMC Farming Simulator root page</a></p>
+<p><i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
