5e9e95848a94dd9649eedd80ba653d395090d755
Snake Man <snakeman@phnet.fi>
2024-10-27 11:50:18 +0000

    fs19 mods-footer include file created

---
+++ include/fs19-mods-footer.php
@@ -0,0 +1,7 @@
+<p>
+<a href="Mods.php" class="button">Mods FS19</a>
+<a href="../index.php" class="button">PMC Farming Simulator root page</a>
+</p>
+<p>
+<i>PMC Farming Simulator 2017 - <?php print(date("Y")); ?>.</i>
+</p>
