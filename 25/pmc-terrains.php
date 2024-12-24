9ebcb919bd135280b309deb41dbae2c2f65568fc
Snake Man <snakeman@phnet.fi>
2024-12-24 17:02:34 +0000

    fs25 PMC terrains update

--- 25/pmc-terrains.php
+++ 25/pmc-terrains.php
@@ -17,49 +17,63 @@
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
 	<h1>PMC Terrains Farming Simulator 25</h1>
+
+<p>
+PMC created terrains for Farming Simulator 25 (FS25).
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>PMC Terrain News</h2>
+	<h2>PMC Terrain News FS25</h2>
 
 <p>
-Possible PMC terrains to be ported for Farming Simulator 25 (FS25) include:
+Currently there are no terrains created for FS25 by PMC nor are there any planned for. Possible FS19 PMC terrains to be ported for FS25 include:
 </p>
-<ul>
-<li><a href="../19/pmc-iowa-garden-city-8km.php">PMC Iowa Garden City 8km (FS19)</a></li>
-<li><a href="../19/pmc-iowa-ringsted-8km.php">PMC Iowa Ringsted 8km (FS19)</a></li>
-<li><a href="../19/pmc-kentucky-middleton-8km.php">PMC Kentucky Middleton 8km (FS19)</a></li>
-<li><a href="../19/pmc-minnesota-elrosa-8km.php">PMC Minnesota Elrosa 8km (FS19)</a></li>
-<li><a href="../19/pmc-minnesota-lowry-8km.php">PMC Minnesota Lowry 8km (FS19)</a> "Millennial Farmer Map"</li>
-<li><a href="../19/pmc-montana-shelby-8km.php">PMC Montana Shelby 8km (FS19)</a> "Welker Farms Map"</li>
-<li><a href="../19/pmc-north-dakota-fairmount-8km.php">PMC North Dakota Fairmount 8km (FS19)</a></li>
-<li><a href="../19/pmc-north-dakota-greendale-4km.php">PMC North Dakota Greendale 4km (FS19)</a></li>
-<li><a href="../19/pmc-north-dakota-sonora-8km.php">PMC North Dakota Sonora 8km (FS19)</a></li>
-<li><a href="../19/pmc-north-dakota-wahpeton-16km.php">PMC North Dakota Wahpeton 16km (FS19)</a></li>
-<li><a href="../19/pmc-ohio-arcanum-8km.php">PMC Ohio Arcanum 8km (FS19)</a></li>
-<li><a href="../19/pmc-ohio-westhope-8km.php">PMC Ohio Westhope 8km (FS19)</a></li>
-<li><a href="../19/pmc-ohio-wheatville-8km.php">PMC Ohio Wheatville 8km (FS19)</a></li>
-<li><a href="../19/pmc-texas-hereford-16km.php">PMC Texas Hereford 16km (FS19)</a></li>
-<li><a href="../19/pmc-texas-red-springs-8km.php">PMC Texas Red Springs 8km (FS19)</a></li>
-<li><a href="../19/pmc-texas-rockwood-8km.php">PMC Texas Rockwood 8km (FS19)</a></li>
-<li><a href="../19/pmc-texas-rowena-8km.php">PMC Texas Rowena 8km (FS19)</a></li>
-<li><a href="../19/pmc-eternal-sugar-beet-damnation-32km.php">PMC Eternal Sugar Beet Damnation 32km (FS19)</a></li>
-<li><a href="../19/pmc-farm-lab.php">PMC Farm Lab (FS19)</a></li>
-<li><a href="../19/pmc-grande-gardens-16km.php">PMC Grande Gardens 16km (FS19)</a></li>
-<li><a href="../22/pmc-cereal-region-32km.php">PMC Cereal Region 32km (FS22)</a></li>
-<li><a href="../22/pmc-king-corn-45km.php">PMC King Corn 45km (FS22)</a></li>
-<li><a href="../22/pmc-korkscrew-40km.php">PMC Korkscrew 40km (FS22)</a></li>
-<li><a href="../22/pmc-ohio-alger-10km.php">PMC Ohio Alger 10km (FS22)</a></li>
-<li><a href="../22/pmc-redwhiteblue-10km.php">PMC RedWhiteBlue 10km (FS22)</a></li>
-<li><a href="../22/pmc-super-six-6km.php">PMC Super Six 6km (FS22)</a></li>
-<li><a href="../22/pmc-undefined-farms-20km.php">PMC Undefined Farms 20km (FS22)</a></li>
-<li><a href="https://www.pmctactical.org/forum/viewtopic.php?f=78&t=22967">PMC Project Orange 20240609 (FS22)</a></li>
-</ul>
 
 <p>
-Read more news from <a href="../news.php">PMC Farming News page</a>.
+<a href="../19/pmc-north-dakota-greendale-4km.php">PMC North Dakota Greendale 4km (FS19)</a>
+</p>
+
+<p>
+Following terrains cannot be ported from FS19 and FS22 because <a href="terrain-giants-broke-large.php">giants broke large terrains in FS25</a>:
+</p>
+
+<p><a href="../19/pmc-iowa-garden-city-8km.php">PMC Iowa Garden City 8km (FS19)</a></p>
+<p><a href="../19/pmc-iowa-ringsted-8km.php">PMC Iowa Ringsted 8km (FS19)</a></p>
+<p><a href="../19/pmc-kentucky-middleton-8km.php">PMC Kentucky Middleton 8km (FS19)</a></p>
+<p><a href="../19/pmc-minnesota-elrosa-8km.php">PMC Minnesota Elrosa 8km (FS19)</a></p>
+<p><a href="../19/pmc-minnesota-lowry-8km.php">PMC Minnesota Lowry 8km (FS19)</a> "Millennial Farmer Map"</p>
+<p><a href="../19/pmc-montana-shelby-8km.php">PMC Montana Shelby 8km (FS19)</a> "Welker Farms Map"</p>
+<p><a href="../19/pmc-north-dakota-fairmount-8km.php">PMC North Dakota Fairmount 8km (FS19)</a></p>
+<p><a href="../19/pmc-north-dakota-sonora-8km.php">PMC North Dakota Sonora 8km (FS19)</a></p>
+<p><a href="../19/pmc-north-dakota-wahpeton-16km.php">PMC North Dakota Wahpeton 16km (FS19)</a></p>
+<p><a href="../19/pmc-ohio-arcanum-8km.php">PMC Ohio Arcanum 8km (FS19)</a></p>
+<p><a href="../19/pmc-ohio-westhope-8km.php">PMC Ohio Westhope 8km (FS19)</a></p>
+<p><a href="../19/pmc-ohio-wheatville-8km.php">PMC Ohio Wheatville 8km (FS19)</a></p>
+<p><a href="../19/pmc-texas-hereford-16km.php">PMC Texas Hereford 16km (FS19)</a></p>
+<p><a href="../19/pmc-texas-red-springs-8km.php">PMC Texas Red Springs 8km (FS19)</a></p>
+<p><a href="../19/pmc-texas-rockwood-8km.php">PMC Texas Rockwood 8km (FS19)</a></p>
+<p><a href="../19/pmc-texas-rowena-8km.php">PMC Texas Rowena 8km (FS19)</a></p>
+<p><a href="../19/pmc-eternal-sugar-beet-damnation-32km.php">PMC Eternal Sugar Beet Damnation 32km (FS19)</a></p>
+<p><a href="../19/pmc-farm-lab.php">PMC Farm Lab (FS19)</a></p>
+<p><a href="../19/pmc-grande-gardens-16km.php">PMC Grande Gardens 16km (FS19)</a></p>
+<p><a href="../22/pmc-cereal-region-32km.php">PMC Cereal Region 32km (FS22)</a></p>
+<p><a href="../22/pmc-king-corn-45km.php">PMC King Corn 45km (FS22)</a></p>
+<p><a href="../22/pmc-korkscrew-40km.php">PMC Korkscrew 40km (FS22)</a></p>
+<p><a href="../22/pmc-ohio-alger-10km.php">PMC Ohio Alger 10km (FS22)</a></p>
+<p><a href="../22/pmc-redwhiteblue-10km.php">PMC RedWhiteBlue 10km (FS22)</a></p>
+<p><a href="../22/pmc-super-six-6km.php">PMC Super Six 6km (FS22)</a></p>
+<p><a href="../22/pmc-undefined-farms-20km.php">PMC Undefined Farms 20km (FS22)</a></p>
+<p><a href="https://www.pmctactical.org/forum/viewtopic.php?f=78&t=22967">PMC Project Orange 20240609 (FS22)</a></p>
+
+<p>
+Long time ago in FS19 editing era I made a statement that I will not create new 2km, 4km, 8.1km or 16.3km terrains anymore because that would be detrimental to the community, it would enforce the belief most players have that you can only do those size terrains which is NOT true, you can do any sizes you want (in FS19 and FS22). Now in FS25 engine you can only do 2km and 4km terrains so... I cant do any new terrains. The only option left for me is to port over PMC North Dakota Greendale 4km terrain but because FS25 is so depressing game engine with all those bugs I have no desire to port my one and only 4km FS19 terrain to it.
+</p>
+
+<p>
+If this situation changes in FS25 life time, I am happy to get back to editing FS25.
 </p>
 
 <?php include("/var/www/include/section-end.php"); ?>
