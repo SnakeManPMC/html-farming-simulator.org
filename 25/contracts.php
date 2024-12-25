c5c04a4fc97abea3ff5c3e454e7e41d3e2d9a258
Snake Man <snakeman@phnet.fi>
2024-12-25 05:19:06 +0000

    fs25 contracts updated

--- 25/contracts.php
+++ 25/contracts.php
@@ -17,7 +17,7 @@
 <?php include("/var/www/include/header-start.php"); ?>
 	<h1>Contracts Farming Simulator 25 - PMC Farming</h1>
 <p>
-Contracting, making money in Farming Simulator 25 (FS25) by doing odd jobs for AI farmers. Excellent way to grow your farm.
+Contracting, making money in Farming Simulator 25 (FS25) by doing odd jobs for AI farmers. Excellent way to grow your farm. Back in FS17 these were called "farmsim Missions".
 </p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
@@ -25,13 +25,30 @@ Contracting, making money in Farming Simulator 25 (FS25) by doing odd jobs for A
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
 <?php include("/var/www/include/support.php"); ?>
-	<h2>Contracts Farming Simulator 25</h2>
+	<h2>Contracts FS25</h2>
 
 <p>
-For background reference see also <a href="../19/contracts.php">FS19 Contracts</a> and <a href="../22/contracts.php">FS22 Contracts</a> pages. Assumption is that FS25 contracts work more or less the same as in FS19 and FS22 games.
+For background reference see also <a href="../19/contracts.php">FS19 Contracts</a> and <a href="../22/contracts.php">FS22 Contracts</a> pages. FS25 contracts work more or less the same as in FS19 and FS22 games.
 </p>
 
-<?php include("../include/fs25-placeholder-pages.php"); ?>
+<p>
+Giants Software (developers of farmsim games) took many good features of <b>FS19 Better Contracts Mod</b> and <b>FS22 Better Contracts Mod</b>, added them into base game FS25. If you know how to play with contracts on FS19 or FS22, then you know how to do it in FS25 already, its the same. If you are a new user who never played farmsim games before, read the above FS19 and FS22 Contracts playing guide pages.
+</p>
+
+<p>
+In FS25 baling contracts had changed a bit, now you are unable to sell bales anymore, you can bale straw or grass and wrap grass bales for silage, but there is no more delivering bales to a selling point or animal dealer.
+</p>
+
+<p>
+When playing on 2km terrains such as Riverbend Springs, Hutan Pantai or Zielonka, all contract rewards are extremely low amount of money, you have to do a lot of contracts for the money rewards to mount up to anything useful even a cheap implement not to mention a tractor. This is because the contract reward system by itself is fine, its just that the terrain is too small with tiny field sizes. Once you download user made custom terrains which are larger, or at least 2km terrain with larger fields than mentioned three Giants default terrains, then FS25 Contract rewards become more viable.
+</p>
+
+<p>
+See also <a href="guide.php">FS25 Playing Guide</a> if you need any more help how to play farmsim game.
+</p>
+
+<p>
+</p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
